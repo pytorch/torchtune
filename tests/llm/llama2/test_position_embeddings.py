4bb68cda240e2085ac41c793ff0e38489843c33d
Kartikay Khandelwal <kartikayk@fb.com>
2023-10-29 11:13:42 -0700

    Add type hints to test

--- tests/llm/llama2/test_position_embeddings.py
+++ tests/llm/llama2/test_position_embeddings.py
@@ -4,10 +4,13 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import pytest
+from typing import Tuple
 
+import pytest
 import torch
+
 from llm.llama2.position_embeddings import RotaryPositionalEmbeddings
+from torch import tensor
 
 from tests.test_utils import assert_expected, set_rng_seed
 
@@ -26,7 +29,7 @@ class TestRotaryPositionEmbedding:
     """
 
     @pytest.fixture
-    def input_params(self):
+    def input_params(self) -> Tuple[int, int, int, int]:
         bsz = 4
         num_heads = 32
         embed_dim = 4096
@@ -35,22 +38,24 @@ class TestRotaryPositionEmbedding:
         return bsz, num_heads, head_dim, seq_len
 
     @pytest.fixture
-    def input(self, input_params):
+    def input(self, input_params: Tuple[int, int, int, int]) -> tensor:
         bsz, num_heads, head_dim, seq_len = input_params
         return torch.randn(bsz, seq_len, num_heads, head_dim)
 
     @pytest.fixture
-    def rope(self, input_params):
+    def rope(
+        self, input_params: Tuple[int, int, int, int]
+    ) -> RotaryPositionalEmbeddings:
         _, _, head_dim, seq_len = input_params
         return RotaryPositionalEmbeddings(dim=head_dim, max_seq_len=seq_len)
 
-    def test_forward(self, input, rope):
+    def test_forward(self, input: tensor, rope: RotaryPositionalEmbeddings) -> None:
         x_out = rope(input)
 
         # check the numerics of the computed tensor
-        assert_expected(x_out.mean(), torch.tensor(-4.3060e-05))
-        assert_expected(x_out.sum(), torch.tensor(-2889.6804))
-        assert_expected(x_out.max(), torch.tensor(5.6446))
+        assert_expected(x_out.mean(), tensor(-4.3060e-05))
+        assert_expected(x_out.sum(), tensor(-2889.6804))
+        assert_expected(x_out.max(), tensor(5.6446))
 
         # check shapes
         assert_expected(x_out.shape, input.shape)
