c3cdc7fa02adaaed32362772165860a7be8bd220
Kartikay Khandelwal <kartikayk@fb.com>
2023-10-30 09:12:01 -0700

    Address comments

--- llm/llama2/position_embeddings.py
+++ llm/llama2/position_embeddings.py
@@ -22,7 +22,7 @@ class RotaryPositionalEmbeddings(nn.Module):
     ```max_seq_len``` by computing this during init.
 
     Args:
-        dim (int): Embedding dimension. This is ussually set to the dim of each
+        dim (int): Embedding dimension. This is usually set to the dim of each
             head in the attention module computed as ```embed_dim``` // ```num_heads```
         max_seq_len (int): Maximum expected sequence length for the
             model, if exceeded the cached freqs will be recomputed
