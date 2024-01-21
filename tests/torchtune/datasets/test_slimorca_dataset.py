fc0f49ebbc3b062e09450c74dc0ca1f001bcc865
Gokul Gunasekaran <gokulg@meta.com>
2024-01-21 12:48:41 -0800

    Add truncation logic and unit tests

--- tests/torchtune/datasets/test_slimorca_dataset.py
+++ tests/torchtune/datasets/test_slimorca_dataset.py
@@ -23,3 +23,53 @@ class TestSlimOrcaDataset:
     def test_slim_orca_dataset(self, tokenizer):
         dataset = datasets.get_dataset("slimorca", tokenizer=tokenizer)
         assert dataset
+
+    def test_prompt_label_generation(self, tokenizer):
+        dataset = datasets.get_dataset("slimorca", tokenizer=tokenizer)
+        sample = [
+            {
+                "from": "system",
+                "value": "hi",
+            },
+            {
+                "from": "human",
+                "value": "mid",
+            },
+            {
+                "from": "gpt",
+                "value": "lo",
+            },
+        ]
+        prompt, label = dataset.generate_prompt_label(sample)
+        assert prompt == "[INST] <<SYS>>\nhi\n<</SYS>>\n\nmid [/INST]"
+        assert label == " lo "
+
+    def test_token_generation(self, tokenizer):
+        dataset = datasets.get_dataset(
+            "slimorca", tokenizer=tokenizer, max_token_length=4096
+        )
+        input, label = dataset.generate_tokens("Hello ", "world!")
+        assert input == [tokenizer.bos_id, 12, 1803, 1024, 103, tokenizer.eos_id]
+        assert label == ([-100] * 3 + [1024, 103, tokenizer.eos_id])
+
+    def test_truncated_token_generation(self, tokenizer):
+        dataset = datasets.get_dataset(
+            "slimorca", tokenizer=tokenizer, max_token_length=5
+        )
+        # 5 is enough for full prompt, but not for label
+        input, label = dataset.generate_tokens("Hello ", "world!")
+        assert input == [tokenizer.bos_id, 12, 1803, 1024, tokenizer.eos_id]
+        assert label == ([-100] * 3 + [1024, tokenizer.eos_id])
+
+        # 4 is not enough for full prompt nor response but truncation
+        # is still feasible
+        dataset = datasets.get_dataset(
+            "slimorca", tokenizer=tokenizer, max_token_length=4
+        )
+        input, label = dataset.generate_tokens("Hello ", "world!")
+        assert input == [tokenizer.bos_id, 12, 1024, tokenizer.eos_id]
+        assert label == ([-100] * 2 + [1024, tokenizer.eos_id])
+
+    def test_value_error(self, tokenizer):
+        with pytest.raises(ValueError):
+            datasets.get_dataset("slimorca", tokenizer=tokenizer, max_token_length=3)
--- torchtune/datasets/slimorca.py
+++ torchtune/datasets/slimorca.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 from datasets import load_dataset
 from torch.utils.data import Dataset
@@ -23,8 +23,20 @@ class SlimOrcaDataset(Dataset):
     The data is formatted to adhere to Llama2 Chat Format.
     This format is required if the base model is Llama2 Chat Model.
 
+    The returned data is a tuple of input token id list and label token id
+    list. If `max_token_length` keyword argument is provided, the returned
+    input token id list is ensured (by truncation if necssary) to be within
+    that length.
+
     Args:
         tokenizer (Tokenizer): Tokenizer used to encode data. Tokenize must implement an `encode` and `decode` method.
+        **kwargs: Additional keyword arguments to pass to the SlimOrca Dataset.
+        This value needs to be at least 4 though it is generally set it to
+        max sequence length accepted by the model.
+
+    Keyword Arguments:
+        max_token_length (int): Maximum number of tokens in the returned.
+        Default is 512.
 
     Data input format:
         [ { "from": "system", "value": "You are an AI assistant. You will be
@@ -51,6 +63,11 @@ class SlimOrcaDataset(Dataset):
     def __init__(self, tokenizer: Tokenizer, **kwargs) -> None:
         self._data = load_dataset("Open-Orca/SlimOrca-Dedup", split="train")
         self._tokenizer = tokenizer
+        self._max_token_length = kwargs.get("max_token_length", 512)
+        if self._max_token_length < 4:
+            # Input token needs to have 1 bos, 1 eos,
+            # and 1 token from prompt, 1 from label
+            raise ValueError("max_token_length must be at least 4")
 
     def __len__(self):
         return len(self._data)
@@ -63,6 +80,31 @@ class SlimOrcaDataset(Dataset):
 
     def __getitem__(self, index: int) -> Tuple[List[int], List[int]]:
         data = self._data[index]["conversations"]
+        prompt, label = generate_prompt_label(data)
+        return generate_tokens(prompt, label)
+
+    def generate_tokens(self, prompt: str, label: str) -> Tuple[List[int], List[int]]:
+        prompt_tokens = self._tokenizer.encode(prompt, add_bos=True, add_eos=False)
+        # Truncate to max token length - 2 (so that there is at least one label token)
+        prompt_tokens = prompt_tokens[: self._max_token_length - 2]
+
+        # Calculate space left for label tokens
+        label_tokens_length = self._max_token_length - len(prompt_tokens)
+        label_tokens = self._tokenizer.encode(label, add_bos=False, add_eos=True)
+
+        # Truncate label tokens
+        label_tokens = label_tokens[: label_tokens_length - 1]
+        if label_tokens[-1] != self._tokenizer.eos_id:
+            label_tokens.append(self._tokenizer.eos_id)
+
+        input = prompt_tokens + label_tokens
+        label = [
+            _CROSS_ENTROPY_IGNORE_IDX for _ in range(len(prompt_tokens))
+        ] + label_tokens
+        assert len(input) == len(label)
+        return input, label
+
+    def generate_prompt_label(self, data: List[Dict[str, str]]) -> Tuple[str, str]:
         agent_text_dict = {}
         # agents can be {system, human, gpt}
         for conversation in data:
@@ -77,13 +119,4 @@ class SlimOrcaDataset(Dataset):
             prompt = f"{self.B_INST} {agent_text_dict['human']} {self.E_INST}"
 
         response = f" {agent_text_dict['gpt']} "
-
-        prompt_tokens = self._tokenizer.encode(prompt, add_bos=True, add_eos=False)
-        label_tokens = self._tokenizer.encode(response, add_bos=False, add_eos=True)
-        input = prompt_tokens + label_tokens
-
-        label = [
-            _CROSS_ENTROPY_IGNORE_IDX for _ in range(len(prompt_tokens))
-        ] + label_tokens
-        assert len(input) == len(label)
-        return input, label
+        return prompt, response
