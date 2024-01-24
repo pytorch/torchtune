0df9f9fae6c65857f0f2ba83275be46ab999b538
Gokul Gunasekaran <gokulg@meta.com>
2024-01-24 07:28:51 -0800

    convert to sphnix note

--- torchtune/utils/checkpointable_dataloader.py
+++ torchtune/utils/checkpointable_dataloader.py
@@ -64,8 +64,8 @@ class CheckpointableDataLoader(DataLoader):
 
     ``state_dict`` returns the current state of the DataLaoder as a dict.
 
-    Note: As this works only with :class:`~torch.utils.data.distributed.DistributedSampler`, the ``set_epoch`` method of that sampler should be
-    invoked before creating an iterator of this class.
+    .. note::
+        As this works only with :class:`~torch.utils.data.distributed.DistributedSampler`, the ``set_epoch`` method of that sampler should be invoked before creating an iterator of this class.
 
     .. note::
         CheckpointableDataLoader doesn't save/restore RNG state of the trainer process/dataloader workers. This implies on restore from a checkpoint if the same RNG seed is used, the RNG state will be the same as the beginning of the previous run. If no random transformations of data are performed in the :class:`~torch.utils.data.Dataset` passed to this DataLoader, this should not matter.
