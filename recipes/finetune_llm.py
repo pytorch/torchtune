b12dde0094377bf9a5da65b49b5dd55c2be90f9b
RdoubleA <rafiayub@fb.com>
2024-01-26 11:19:06 -0800

    Add config validation utility
    [ghstack-poisoned]

--- recipes/finetune_llm.py
+++ recipes/finetune_llm.py
@@ -16,6 +16,7 @@ from torch.utils.data import DataLoader, DistributedSampler
 
 from torchtune import datasets, losses, models, modules, optim, utils
 from torchtune.utils.checkpoint import load_checkpoint, save_checkpoint
+from torchtune.utils.config import validate_recipe_args
 from torchtune.utils.generation import generate_from_prompt
 from tqdm import tqdm
 
