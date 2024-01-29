e72bce143d1649de1f895b9d41d16a14828d523b
Philip Bontrager <pbontrager@gmail.com>
2024-01-29 16:54:34 -0500

    Added Recipes to docs (#256)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.duration",
+    "sphinx_tabs.tabs",
     "sphinx_design",
     "sphinx_gallery.gen_gallery",
     "sphinx_copybutton",
