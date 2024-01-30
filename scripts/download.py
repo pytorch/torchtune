c628a322b4031a4d7a2d4aedbbd41bad0f254ab9
joecummings <jrcummings27@gmail.com>
2024-01-29 18:59:47 -0800

    Update requirements and add to README

--- scripts/download.py
+++ scripts/download.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 """This script downloads a model from the HuggingFace hub."""
 
 import argparse
