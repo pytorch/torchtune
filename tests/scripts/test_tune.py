c694bd48825a7d28c13a72189a13edd47b6d6a26
RdoubleA <rafiayub@fb.com>
2024-01-29 16:01:57 -0800

    Update base for Update on "Add config validation in FullFinetuneParams"
    Add checks for sensible args passed into `FullFinetuneParams` using `__post_init__`. Needed to modify a few constants so that they weren't private to make the checks easier.
    
    Welcome any suggestions for any further validation that needs to be done. The next PR tests any config in the library as a whole and adds it to CI.
    
    `pytest tests`
    `recipes/tests/run_test.sh`
    
    [ghstack-poisoned]

--- tests/scripts/test_tune.py
+++ tests/scripts/test_tune.py
@@ -123,7 +123,7 @@ class TestTuneCLI:
                 )
                 assert os.path.exists(config_path), f"{config_path} must exist"
 
-    def test_run(self, capsys):
+    def test_run(self):
         recipe = "finetune_llm"
         testargs = f"\
             tune {recipe} --config alpaca_llama2_finetune --override tokenizer=fake \
