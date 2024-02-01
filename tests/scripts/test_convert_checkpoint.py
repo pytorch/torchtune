640ca1883ea8b37a5e601e16457c80cf6a7fd3f3
joecummings <jrcummings27@gmail.com>
2024-02-01 12:16:55 -0800

    Add large scale test on daily schedule

--- tests/scripts/test_convert_checkpoint.py
+++ tests/scripts/test_convert_checkpoint.py
@@ -52,10 +52,82 @@ class TestTuneCLIWithConvertCheckpointScript:
             ) as e:
                 runpy.run_path(TUNE_PATH, run_name="__main__")
 
-    def test_convert_checkpoint_matches_fair_model(self, capsys):
-        tiny_fair_ckpt = ASSETS / "tiny_fair_checkpoint.pt"
+    def _tiny_fair_transformer(self, ckpt):
+        tiny_fair_transfomer = Transformer(
+            vocab_size=500,
+            n_layers=2,
+            n_heads=4,
+            dim=32,
+            max_seq_len=64,
+            n_kv_heads=4,
+        )
+        tiny_fair_state_dict = torch.load(ckpt, weights_only=True)
+        tiny_fair_transfomer.load_state_dict(tiny_fair_state_dict, strict=True)
+        return tiny_fair_transfomer
+
+    def _tiny_native_transformer(self, ckpt):
+        tiny_native_transfomer = llama2(
+            vocab_size=500,
+            num_layers=2,
+            num_heads=4,
+            embed_dim=32,
+            max_seq_len=64,
+            num_kv_heads=4,
+        )
+        tiny_native_state_dict = torch.load(ckpt, weights_only=True)
+        tiny_native_transfomer.load_state_dict(tiny_native_state_dict, strict=False)
+        return tiny_native_transfomer
+
+    def _llama2_7b_fair_transformer(self, ckpt):
+        llama2_7b_fair_transformer = Transformer(
+            vocab_size=32_000,
+            n_layers=32,
+            n_heads=32,
+            dim=4096,
+            max_seq_len=2048,
+            n_kv_heads=32,
+        )
+        llama2_7b_fair_state_dict = torch.load(ckpt, weights_only=True)
+        llama2_7b_fair_transformer.load_state_dict(
+            llama2_7b_fair_state_dict, strict=False
+        )
+        llama2_7b_fair_transformer.eval()
+        return llama2_7b_fair_transformer
+
+    def _llama2_7b_native_transformer(self, ckpt):
+        llama2_7b_native_transformer = llama2(
+            vocab_size=32_000,
+            num_layers=32,
+            num_heads=32,
+            embed_dim=4096,
+            max_seq_len=2048,
+            num_kv_heads=32,
+        )
+        llama2_7b_native_state_dict = torch.load(ckpt, weights_only=True)
+        llama2_7b_native_transformer.load_state_dict(
+            llama2_7b_native_state_dict, strict=True
+        )
+        llama2_7b_native_transformer.eval()
+        return llama2_7b_native_transformer
+
+    def _generate_toks_for_tiny(self):
+        return torch.randint(low=0, high=500, size=(16, 64))
+
+    def _generate_toks_for_llama2_7b(self):
+        return torch.randint(low=0, high=32_000, size=(16, 128))
+
+    def test_convert_checkpoint_matches_fair_model(self, capsys, pytestconfig):
+        is_large_scale_test = pytestconfig.getoption("--large-scale")
+
+        if is_large_scale_test:
+            ckpt = "/tmp/test-artifacts/llama2-7b-fair"
+            fair_transformer = self._llama2_7b_fair_transformer(ckpt)
+        else:
+            ckpt = ASSETS / "tiny_fair_checkpoint.pt"
+            fair_transformer = self._tiny_fair_transformer(ckpt)
+
         output_path = tempfile.NamedTemporaryFile(delete=True).name
-        testargs = f"tune convert_checkpoint --checkpoint-path {tiny_fair_ckpt} --output-path {output_path}".split()
+        testargs = f"tune convert_checkpoint --checkpoint-path {ckpt} --output-path {output_path}".split()
         with patch.object(sys, "argv", testargs):
             runpy.run_path(TUNE_PATH, run_name="__main__")
 
@@ -65,30 +137,18 @@ class TestTuneCLIWithConvertCheckpointScript:
             in output.out.rstrip("\n").split("\n")[-1]
         )
 
-        tiny_fair_transformer = Transformer(
-            vocab_size=500,
-            n_layers=2,
-            n_heads=4,
-            dim=32,
-            max_seq_len=64,
-            n_kv_heads=4,
+        native_transformer = (
+            self._llama2_7b_native_transformer(output_path)
+            if is_large_scale_test
+            else self._tiny_native_transformer(output_path)
         )
-        tiny_fair_state_dict = torch.load(tiny_fair_ckpt, weights_only=True)
-        tiny_fair_transformer.load_state_dict(tiny_fair_state_dict, strict=True)
-
-        tiny_llama2 = llama2(
-            vocab_size=500,
-            num_layers=2,
-            num_heads=4,
-            embed_dim=32,
-            max_seq_len=64,
-            num_kv_heads=4,
-        )
-        tiny_llama2_state_dict = torch.load(output_path, weights_only=True)
-        tiny_llama2.load_state_dict(tiny_llama2_state_dict, strict=True)
 
         for i in range(10):
-            toks = torch.randint(low=0, high=500, size=(16, 64))
-            tiny_fair_transformer_out = tiny_fair_transformer(toks)
-            tiny_llama2_out = tiny_llama2(toks)
-            assert_expected(tiny_fair_transformer_out.sum(), tiny_llama2_out.sum())
+            toks = (
+                self._generate_toks_for_llama2_7b()
+                if is_large_scale_test
+                else self._generate_toks_for_tiny()
+            )
+            fair_out = fair_transformer(toks)
+            native_out = native_transformer(toks)
+            assert_expected(fair_out.sum(), native_out.sum())
