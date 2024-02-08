4c50109eb7612c27c708b280370e80a9a1b0aa91
joecummings <jrcummings27@gmail.com>
2024-02-08 07:35:29 -0800

    Remove extra copyright

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 from setuptools import find_packages, setup
 
 
