b6a48315c3ff5a3082d97f43f15d6fcdb2a31be9
Rohan Varma <rvarm1@fb.com>
2024-02-13 14:51:44 -0800

    Initial LoRA CI (#359)

--- recipes/tests/test_full_finetune.py
+++ recipes/tests/test_full_finetune.py
@@ -7,49 +7,29 @@
 import logging
 import os
 import tempfile
-from typing import Dict, Optional
+from typing import Dict
 
 import pytest
 
 from recipes.full_finetune import FullFinetuneRecipe
 from recipes.params import FullFinetuneParams
+from recipes.tests.utils import (
+    default_recipe_kwargs,
+    fetch_ckpt_model_path,
+    fetch_loss_values,
+    llama2_small_test_ckpt,
+    validate_loss_values,
+)
 
 from tests.test_utils import assert_expected
 from torchtune import models
-from torchtune.models.llama2 import llama2
 
-from torchtune.modules import TransformerDecoder
-
-
-def small_test_ckpt(max_batch_size: Optional[int] = None) -> TransformerDecoder:
-    return llama2(
-        vocab_size=32_000,
-        num_layers=4,
-        num_heads=16,
-        embed_dim=256,
-        max_seq_len=2048,
-        norm_eps=1e-5,
-        num_kv_heads=8,
-        max_batch_size=max_batch_size,
-    )
-
-
-models.ALL_MODELS["small_test_ckpt"] = small_test_ckpt
+models.ALL_MODELS["small_test_ckpt"] = llama2_small_test_ckpt
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class TestFullFinetuneRecipe:
-    def _fetch_loss_values(self, output) -> Dict[str, float]:
-        lines = output.splitlines()
-        loss_values = {}
-        for line in lines:
-            if "Loss:" in line:
-                splits = line.split("Loss:")
-                loss_value = float(splits[1].split(":")[0])
-                loss_values[splits[0]] = loss_value
-        return loss_values
-
     def _fetch_expected_loss_values(self, ckpt) -> Dict[str, float]:
         small_test_ckpt_loss_values = {
             "1|1|": 10.5074,
@@ -69,41 +49,12 @@ class TestFullFinetuneRecipe:
             return llama2_7b_ckpt_loss_values
         raise ValueError(f"Unknown ckpt {ckpt}")
 
-    def _fetch_ckpt_model_path(self, ckpt) -> str:
-        if ckpt == "small_test_ckpt":
-            return "/tmp/test-artifacts/small-ckpt-01242024"
-        if ckpt == "llama2_7b":
-            return "/tmp/test-artifacts/llama2-7b-01242024"
-        raise ValueError(f"Unknown ckpt {ckpt}")
-
     def test_loss(self, capsys, pytestconfig):
         large_scale = pytestconfig.getoption("--large-scale")
         ckpt = "llama2_7b" if large_scale else "small_test_ckpt"
         expected_loss_values = self._fetch_expected_loss_values(ckpt)
 
-        kwargs_values = {
-            "dataset": "alpaca",
-            "train_on_input": False,
-            "seed": 9,
-            "shuffle": True,
-            "model": ckpt,
-            "model_checkpoint": self._fetch_ckpt_model_path(ckpt),
-            "tokenizer": "llama2_tokenizer",
-            "tokenizer_checkpoint": "/tmp/test-artifacts/tokenizer.model",
-            "batch_size": 8,
-            "lr": 2e-5,
-            "epochs": 2,
-            "max_steps_per_epoch": 2,
-            "optimizer": "AdamW",
-            "loss": "CrossEntropyLoss",
-            "output_dir": "/tmp",
-            "device": "cpu",
-            "dtype": "fp32",
-            "resume_from_checkpoint": False,
-            "enable_fsdp": False,
-            "enable_activation_checkpointing": False,
-            "metric_logger_type": "disk",
-        }
+        kwargs_values = default_recipe_kwargs(ckpt)
 
         recipe_params = FullFinetuneParams(**kwargs_values)
 
@@ -111,14 +62,8 @@ class TestFullFinetuneRecipe:
         recipe.setup(params=recipe_params)
         recipe.train()
 
-        loss_values = self._fetch_loss_values(capsys.readouterr().err)
-        logger.info("Expected loss values : ", expected_loss_values)
-        logger.info("Loss values from Finetune : ", loss_values)
-        assert len(loss_values) == len(expected_loss_values)
-        for key, value in loss_values.items():
-            assert key in expected_loss_values
-            expected_loss_value = expected_loss_values[key]
-            assert value == pytest.approx(expected_loss_value, abs=0.001)
+        loss_values = fetch_loss_values(capsys.readouterr().err)
+        validate_loss_values(loss_values, expected_loss_values)
 
     def test_training_state_on_resume(self):
         """
@@ -139,7 +84,7 @@ class TestFullFinetuneRecipe:
                 "seed": 9,
                 "shuffle": True,
                 "model": model_ckpt,
-                "model_checkpoint": self._fetch_ckpt_model_path(model_ckpt),
+                "model_checkpoint": fetch_ckpt_model_path(model_ckpt),
                 "tokenizer": "llama2_tokenizer",
                 "tokenizer_checkpoint": "/tmp/test-artifacts/tokenizer.model",
                 "epochs": 4,
@@ -206,7 +151,7 @@ class TestFullFinetuneRecipe:
                 "seed": 9,
                 "shuffle": True,
                 "model": model_ckpt,
-                "model_checkpoint": self._fetch_ckpt_model_path(model_ckpt),
+                "model_checkpoint": fetch_ckpt_model_path(model_ckpt),
                 "tokenizer": "llama2_tokenizer",
                 "tokenizer_checkpoint": "/tmp/test-artifacts/tokenizer.model",
                 "batch_size": batch_size[0],  # parametrized in the test
@@ -237,9 +182,7 @@ class TestFullFinetuneRecipe:
             loss_value = float(
                 [
                     value
-                    for key, value in self._fetch_loss_values(
-                        capsys.readouterr().err
-                    ).items()
+                    for key, value in fetch_loss_values(capsys.readouterr().err).items()
                 ][0]
             )
 
@@ -259,9 +202,7 @@ class TestFullFinetuneRecipe:
             acc_loss_value = sum(
                 [
                     float(value) / (gradient_accumulation_steps[1])
-                    for key, value in self._fetch_loss_values(
-                        capsys.readouterr().err
-                    ).items()
+                    for key, value in fetch_loss_values(capsys.readouterr().err).items()
                 ]
             )
             assert_expected(loss_value, acc_loss_value, rtol=1e-1)
