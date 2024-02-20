fc3219d7bac9bdc65f27dbd42f40f203d01498ae
joecummings <jrcummings27@gmail.com>
2024-02-20 11:39:20 -0800

    Add file deletions and modify setup

--- tests/_scripts/common.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-TUNE_PATH = "_scripts/cli_utils/tune"
