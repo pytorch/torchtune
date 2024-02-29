a8715f9d6d5406d70ccbaffa906bbb1d7e8f72c9
Kartikay Khandelwal <47255723+kartikayk@users.noreply.github.com>
2024-02-29 12:41:28 -0800

    Make Checkpoint Conversion Extensible (#432)

--- tests/torchtune/_cli/test_convert_checkpoint.py
+++ tests/torchtune/_cli/test_convert_checkpoint.py
@@ -45,11 +45,22 @@ ASSETS = Path(__file__).parent.parent.parent / "assets"
 class TestTuneCLIWithConvertCheckpointScript:
     def test_convert_checkpoint_errors_on_bad_conversion(self, capsys):
         incorrect_state_dict_loc = ASSETS / "tiny_state_dict_with_one_key.pt"
-        testargs = f"tune convert_checkpoint --checkpoint-path {incorrect_state_dict_loc}".split()
+        testargs = (
+            f"tune convert_checkpoint --checkpoint-path {incorrect_state_dict_loc} --model llama2"
+        ).split()
         with patch.object(sys, "argv", testargs):
             with pytest.raises(
-                Exception, match=r".*Are you sure this is a FAIR Llama2 checkpoint.*"
+                Exception, match=r".*Error converting the original Llama2.*"
             ) as e:
+                runpy.run_path(TUNE_PATH, run_name="__main__")
+
+    def test_convert_checkpoint_errors_on_unsupported_model(self, capsys):
+        incorrect_state_dict_loc = ASSETS / "tiny_state_dict_with_one_key.pt"
+        testargs = (
+            f"tune convert_checkpoint --checkpoint-path {incorrect_state_dict_loc} --model gemma"
+        ).split()
+        with patch.object(sys, "argv", testargs):
+            with pytest.raises(Exception, match=r".*not supported in TorchTune.*") as e:
                 runpy.run_path(TUNE_PATH, run_name="__main__")
 
     def _tiny_fair_transformer(self, ckpt):
@@ -129,7 +140,9 @@ class TestTuneCLIWithConvertCheckpointScript:
             fair_transformer = self._tiny_fair_transformer(ckpt)
 
         output_path = tempfile.NamedTemporaryFile(delete=True).name
-        testargs = f"tune convert_checkpoint --checkpoint-path {ckpt} --output-path {output_path}".split()
+        testargs = (
+            f"tune convert_checkpoint --checkpoint-path {ckpt} --output-path {output_path} --model llama2"
+        ).split()
         with patch.object(sys, "argv", testargs):
             runpy.run_path(TUNE_PATH, run_name="__main__")
 
