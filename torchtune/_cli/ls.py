526d86f4704bd3335506d7267d03c75d88fb5aee
joecummings <jrcummings27@gmail.com>
2024-02-29 13:47:57 -0800

    Fix help example

--- torchtune/_cli/ls.py
+++ torchtune/_cli/ls.py
@@ -44,9 +44,9 @@ if __name__ == "__main__":
         examples:
             $ tune ls
             RECIPE               CONFIG
-            full_finetune        alpaca_llama2_full_finetune.yaml
-            lora_finetune        alpaca_llama2_lora_finetune.yaml
-            alpaca_generate      <>
+            full_finetune.py     alpaca_llama2_full_finetune.yaml
+            lora_finetune.py     alpaca_llama2_lora_finetune.yaml
+            alpaca_generate.py   <>
 
         To run one of these recipes:
             $ tune full_finetune --config alpaca_llama2_full_finetune.yaml
