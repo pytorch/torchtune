6e2f2945d89c331d0670bf7848b0b63ed753bd88
joecummings <jrcummings27@gmail.com>
2024-03-04 14:25:34 -0800

    Add alpaca generate updates

--- tests/recipes/test_alpaca_generate.py
+++ tests/recipes/test_alpaca_generate.py
@@ -10,21 +10,8 @@ from typing import Optional
 import recipes.alpaca_generate as alpaca_generate
 from omegaconf import OmegaConf
 from torchtune import models
-from torchtune.models.llama2 import llama2
 from torchtune.modules import TransformerDecoder
-
-
-def small_test_ckpt(max_batch_size: Optional[int] = None) -> TransformerDecoder:
-    return llama2(
-        vocab_size=32_000,
-        num_layers=4,
-        num_heads=16,
-        embed_dim=256,
-        max_seq_len=2048,
-        norm_eps=1e-5,
-        num_kv_heads=8,
-        max_batch_size=max_batch_size,
-    )
+from tests.recipes.utils import llama2_small_test_ckpt
 
 
 models.small_test_ckpt = small_test_ckpt
@@ -40,20 +27,19 @@ class TestAlpacaGenerateRecipe:
             return "/tmp/test-artifacts/llama2-7b-01242024"
         raise ValueError(f"Unknown ckpt {ckpt}")
 
-    def test_alpaca_generate(self, capsys, pytestconfig):
+    def test_alpaca_generate(self, capsys, pytestconfig, tmpdir, monkeypatch):
         large_scale = pytestconfig.getoption("--large-scale")
         ckpt = "llama2.llama2_7b" if large_scale else "small_test_ckpt"
 
-        kwargs_values = {
-            "model": {"_component_": f"torchtune.models.{ckpt}"},
-            "model_checkpoint": self._fetch_ckpt_model_path(ckpt),
-            "tokenizer": {
-                "_component_": "torchtune.models.llama2.llama2_tokenizer",
-                "path": "/tmp/test-artifacts/tokenizer.model",
-            },
-            "instruction": "Answer the question.",
-            "input": "What is some cool music from the 1920s?",
-            "max_gen_len": 64,
-        }
-        cfg = OmegaConf.create(kwargs_values)
-        alpaca_generate.recipe(cfg)
+        cmd = f"""
+        tune alpaca_generate
+            --config {_CONFIG_PATH} \
+            --override \
+            model._component_=torchtune.models.{ckpt} \
+            model_checkpoint={self._fetch_ckpt_model_path(ckpt)} \
+            output_dir={tmpdir} \
+        """.split()
+
+        monkeypatch.setattr(sys, "argv", cmd)
+        with pytest.raises(SystemExit):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
