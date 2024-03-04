54926e4c98cba5fa49040caec7ad52ed9a4f06ff
joecummings <jrcummings27@gmail.com>
2024-03-04 13:57:39 -0800

    Testing changes

--- tests/recipes/common.py
+++ tests/recipes/common.py
@@ -1,4 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from pathlib import Path
 
-RECIPE_DIR = Path(__file__).parent.parent.parent / "recipes"
 RECIPE_TESTS_DIR = Path(__file__).parent
