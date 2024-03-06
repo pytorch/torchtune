ed67ac83b2b2ac3d565ae55f85c8fe54e79d71e0
joecummings <jrcummings27@gmail.com>
2024-03-06 11:51:41 -0800

    Remove bad commits

--- tests/recipes/test_lora_finetune.py
+++ tests/recipes/test_lora_finetune.py
@@ -110,6 +110,13 @@ class TestLoRAFinetuneRecipe:
             cmd += [
                 'model.lora_attn_modules=["q_proj", "k_proj", "v_proj", "output_proj"]'
             ]
+        if enable_fsdp:
+            cmd.append("--enable-fsdp")
+            context_manager = contextlib.nullcontext
+        else:
+            context_manager = single_box_init
+
+        with context_manager():
             monkeypatch.setattr(sys, "argv", cmd)
             with pytest.raises(SystemExit):
                 with (
