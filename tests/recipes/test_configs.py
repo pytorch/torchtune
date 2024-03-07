3be5e8c9d749744877cf0dfec28b43071293d3ef
RdoubleA <rafiayub@fb.com>
2024-03-07 10:15:31 -0800

    move test_configs

---
+++ tests/recipes/test_configs.py
@@ -0,0 +1,23 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+import os
+
+from omegaconf import OmegaConf
+from torchtune import config
+
+ROOT_DIR: str = os.path.join(os.path.abspath(__file__), "../../../configs")
+
+
+class TestConfigs:
+    def test_instantiate(self) -> None:
+        all_configs = [
+            os.path.join(ROOT_DIR, f)
+            for f in os.listdir(ROOT_DIR)
+            if f.endswith(".yaml")
+        ]
+        for config_path in all_configs:
+            cfg = OmegaConf.load(config_path)
+            config.validate(cfg)
