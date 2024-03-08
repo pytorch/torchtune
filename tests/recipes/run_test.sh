d04b0a8d6a39e7cd085db606e5329591f932ba21
Rohan Varma <rvarm1@fb.com>
2024-03-08 12:22:55 -0800

    upd

--- tests/recipes/run_test.sh
+++ tests/recipes/run_test.sh
@@ -11,7 +11,7 @@
 
 LOCAL_DIR="/tmp/test-artifacts"
 S3_URLS=("s3://pytorch-multimodal/llama2-7b/tokenizer.model" "s3://pytorch-multimodal/small-ckpt-01242024")
-PYTEST_COMMAND="pytest tests/recipes/test_lora_finetune.py -s"
+PYTEST_COMMAND="pytest tests/recipes -s"
 
 if [[ $# -gt 0 ]]; then
     if [ "$1" = "--large-scale" ]; then
