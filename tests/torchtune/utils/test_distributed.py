b4d0528bdc38ed4e1b43d4a9db318b7b0b09ed30
Evan Smothers <ebs@fb.com>
2024-03-11 21:12:06 -0700

    better fix

--- tests/torchtune/utils/test_distributed.py
+++ tests/torchtune/utils/test_distributed.py
@@ -30,6 +30,13 @@ from torchtune.utils.distributed import (
 
 
 class TestDistributed:
+    def test_init_distributed(self) -> None:
+        """Integration test to confirm consistency across device initialization utilities."""
+        distributed = init_distributed()
+        assert (
+            not distributed
+        ), "Should return False as there are no distributed environment variables"
+
     @staticmethod
     def _test_worker_fn(init_pg_explicit: bool) -> None:
         """
