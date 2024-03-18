fix precision test

torchtune/utils/precision.py
@@ -54,7 +54,7 @@ def list_dtypes() -> List[str]:
     return list(PRECISION_STR_TO_DTYPE.keys())
 
 
-def _verify_bf16_support():
+def verify_bf16_support():
     return (
         torch.cuda.is_available()
         and torch.version.cuda
@@ -90,7 +90,7 @@ def get_dtype(dtype: Optional[str] = None) -> torch.dtype:
             f"Dtype {torch_dtype} must be one of {', '.join(list_dtypes())} for finetuning."
         )
 
-    if torch_dtype == torch.bfloat16 and not _verify_bf16_support():
+    if torch_dtype == torch.bfloat16 and not verify_bf16_support():
         log.info("BF16 not supported on this hardware. Setting dtype to float32")
         torch_dtype = torch.float32
 