15e41a3739e7a670fcdf543f51f4fec6b280c805
RdoubleA <rafiayub@fb.com>
2024-03-18 12:35:29 -0700

    add init.py

--- tests/torchtune/data/test_templates.py
+++ tests/torchtune/data/test_templates.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchtune.data.templates import AlpacaInstructTemplate
+from torchtune.data import AlpacaInstructTemplate
 
 
 class TestAlpacaInstructTemplate:
---
+++ torchtune/data/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torchtune.data.templates import AlpacaInstructTemplate
+
+__all__ = [
+    "AlpacaInstructTemplate",
+]
