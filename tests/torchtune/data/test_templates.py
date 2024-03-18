67d08f001b14a2a18dea18fe4367f96d5e120b47
RdoubleA <rafiayub@fb.com>
2024-03-18 14:00:19 -0700

    include real data in tests

--- tests/torchtune/data/test_templates.py
+++ tests/torchtune/data/test_templates.py
@@ -8,34 +8,55 @@ from torchtune.data import AlpacaInstructTemplate
 
 
 class TestAlpacaInstructTemplate:
+    samples = [
+        {
+            "instruction": "Give three tips for staying healthy.",
+            "input": "",
+            "output": (
+                "1.Eat a balanced diet and make sure to include plenty of fruits and vegetables."
+                "2. Exercise regularly to keep your body active and strong."
+                "3. Get enough sleep and maintain a consistent sleep schedule."
+            ),
+        },
+        {
+            "instruction": "Evaluate this sentence for spelling and grammar mistakes",
+            "input": "He finnished his meal and left the resturant",
+            "output": "He finished his meal and left the restaurant.",
+        },
+    ]
+    expected_prompts = [
+        (
+            "Below is an instruction that describes a task. Write a response that appropriately "
+            "completes the request.\n\n"
+            "### Instruction:\nGive three tips for staying healthy.\n\n"
+            "### Response:\n"
+        ),
+        (
+            "Below is an instruction that describes a task, paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\nEvaluate this sentence for spelling and grammar mistakes\n\n"
+            "### Input:\nHe finnished his meal and left the resturant\n\n"
+            "### Response:\n"
+        ),
+    ]
+
+    template = AlpacaInstructTemplate()
+
     def test_format(self):
-        template = AlpacaInstructTemplate()
-        sample = {
-            "instruction": "A man, a plan, a canal, Panama!",
-            "input": "The fox jumped over the lazy dog.",
-        }
-        # Test with instruction and input
-        actual = template.format(sample)
-        expected = template.system["prompt_input"].format(**sample)
-        assert actual == expected
-        # Test with instruction only
-        sample = {
-            "instruction": "A man, a plan, a canal, Panama!",
-        }
-        actual = template.format(sample)
-        expected = template.system["prompt_no_input"].format(**sample)
-        assert actual == expected
+        for sample, expected_prompt in zip(self.samples, self.expected_prompts):
+            actual = self.template.format(sample)
+            assert actual == expected_prompt
 
     def test_format_with_column_map(self):
-        template = AlpacaInstructTemplate()
-        sample = {
-            "not_an_instruction": "A man, a plan, a canal, Panama!",
-            "not_an_input": "The fox jumped over the lazy dog.",
-        }
         column_map = {"instruction": "not_an_instruction", "input": "not_an_input"}
-        # Test with instruction and input
-        actual = template.format(sample, column_map=column_map)
-        expected = template.system["prompt_input"].format(
-            instruction=sample["not_an_instruction"], input=sample["not_an_input"]
-        )
-        assert actual == expected
+        for sample, expected_prompt in zip(self.samples, self.expected_prompts):
+            modified_sample = sample.copy()
+            modified_sample["not_an_instruction"], modified_sample["not_an_input"] = (
+                modified_sample["instruction"],
+                modified_sample["input"],
+            )
+            del modified_sample["instruction"], modified_sample["input"]
+
+            actual = self.template.format(modified_sample, column_map=column_map)
+
+            assert actual == expected_prompt
