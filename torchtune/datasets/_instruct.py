e27928ac3018ab0c492900c67f37d06d7e0befea
RdoubleA <rafiayub@fb.com>
2024-03-18 13:49:08 -0700

    add train_on_input

--- torchtune/datasets/_instruct.py
+++ torchtune/datasets/_instruct.py
@@ -16,38 +16,33 @@ from torchtune.modules import Tokenizer
 
 class InstructDataset(Dataset):
     """
-    Support for the Alpaca dataset and its variants from HuggingFace Datasets.
-    https://huggingface.co/datasets/tatsu-lab/alpaca
+    Class that supports any custom dataset with instruction-based prompts and a
+    configurable template.
 
-    Data input format: https://huggingface.co/datasets/tatsu-lab/alpaca#data-instances
+    The general flow from loading a sample to tokenized prompt is:
+    load sample -> apply transform -> format into template -> tokenize
 
-    The input is created using the prompt template from the original alpaca codebase:
-    https://github.com/tatsu-lab/stanford_alpaca/blob/761dc5bfbdeeffa89b8bff5d038781a4055f796a/train.py#L31
-
-    where `instruction`, `input`, and `output` are fields from the dataset.
+    If the column/key names differ from the expected names in the `PromptTemplate`,
+    then the `column_map` argument can be used to provide this mapping.
 
     Masking of the prompt during training is controlled by the `train_on_input` flag, which is
-    set to `True` by default (ref: https://github.com/tloen/alpaca-lora/blob/main/finetune.py#L49)
+    set to `False` by default.
     - If `train_on_input` is True, the prompt is used during training and
     contributes to the loss.
     - If `train_on_input` is False, the prompt is masked out (tokens replaced with -100)
 
-    The version of the dataset used is controlled by the `use_clean` flag which set to False by default.
-    - If `use_clean` is True, then https://huggingface.co/datasets/yahma/alpaca-cleaned is used
-    - If `use_clean` is False, then https://huggingface.co/datasets/tatsu-lab/alpaca is used
-
     Args:
         tokenizer (Tokenizer): Tokenizer used to encode data. Tokenize must implement an `encode` and `decode` method.
-        train_on_input (bool): Whether the model is trained on the prompt or not. Default is True.
-        use_clean (bool): Whether to use the cleaned version of the dataset or not. Default is False.
-        **kwargs: Additional keyword arguments to pass to the Alpaca Dataset.
-
-
-    Example:
-        >>> alpaca_ds = AlpacaDataset(tokenizer=tokenizer)
-        >>> for batch in Dataloader(alpaca_ds, batch_size=8):
-        >>>     print(f"Batch size: {len(batch)}")
-        >>> Batch size: 8
+        source (str): path string of dataset, anything supported by HuggingFace's `load_dataset`
+            (https://huggingface.co/docs/datasets/en/package_reference/loading_methods#datasets.load_dataset.path)
+        template (Union[PromptTemplate, str]): template used to format the prompt. It should be either a `PromptTemplate` or
+            an f-string with placeholders for the appropriate data fields. If the placeholder
+            names do not match the column/key names in the dataset, use `column_map` to map them.
+        transform (Optional[Callable]): transform to apply to the sample before formatting to the template.
+            Default is None.
+        column_map (Optional[Dict[str, str]]): a mapping from the expected placeholder names in the template
+            to the column/key names in the sample. If None, assume these are identical.
+        train_on_input (bool): Whether the model is trained on the prompt or not. Default is False.
     """
 
     def __init__(
@@ -55,28 +50,34 @@ class InstructDataset(Dataset):
         tokenizer: Tokenizer,
         source: str,
         template: Union[PromptTemplate, str],
-        text_transform: Optional[Callable] = None,
-        token_transform: Optional[Callable] = None,
-        column_map: Optional[Dict[str, str]] = None,  # by default, assume column names == what template expects
+        transform: Optional[Callable] = None,
+        column_map: Optional[Dict[str, str]] = None,
+        train_on_input: bool = False,
     ) -> None:
         self._tokenizer = tokenizer
         self._data = load_dataset(source)
-        self._template = template
-        self._text_transform = text_transform
-        self._token_transform = token_transform
+        self.template = template
+        self._transform = transform
         self._column_map = column_map
+        self.train_on_input = train_on_input
 
     def __len__(self):
         return len(self._data)
 
     def __getitem__(self, index: int) -> Tuple[List[int], List[int]]:
         sample = self._data[index]
-        return self._transform(sample)
+        return self._process(sample)
 
-    def _transform(self, sample: Mapping[str, Any]) -> Tuple[List[int], List[int]]:
-        transformed_sample = self._text_transform(sample) if self._text_transform else sample
+    def _process(self, sample: Mapping[str, Any]) -> Tuple[List[int], List[int]]:
+        transformed_sample = self._transform(sample) if self._transform else sample
         prompt = self._template.format(transformed_sample, self._column_map)
         encoded_prompt = self._tokenizer.encode(prompt)
-        labels = self._tokenizer.encode(sample[self._column_map["output"]])
-        transformed_encoded_prompt, transformed_labels = self._token_transform(encoded_prompt, labels)
-        return transformed_encoded_prompt, transformed_labels
+        response = self._tokenizer.encode(sample[self._column_map["output"]])
+        encoded_prompt_with_response = encoded_prompt + response
+
+        if self.train_on_input:
+            labels = encoded_prompt_with_response.copy()
+        else:
+            labels = [CROSS_ENTROPY_IGNORE_IDX] * len(encoded_prompt) + response
+
+        return encoded_prompt_with_response, labels
