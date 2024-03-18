43f512712da1c547762327f8fa82b78bcd0be553
RdoubleA <rafiayub@fb.com>
2024-03-18 12:49:16 -0700

    add underscore to file

--- torchtune/data/__init__.py
+++ torchtune/data/__init__.py
@@ -4,8 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchtune.data.templates import AlpacaInstructTemplate
+from torchtune.data._templates import AlpacaInstructTemplate, PromptTemplate
 
 __all__ = [
     "AlpacaInstructTemplate",
+    "PromptTemplate",
 ]
