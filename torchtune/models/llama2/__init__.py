638bad32a6af47fea3d0c7975c1d450ef116e7ee
Kartikay Khandelwal <kartikayk@fb.com>
2024-03-19 19:34:38 -0700

    lint

--- torchtune/models/llama2/__init__.py
+++ torchtune/models/llama2/__init__.py
@@ -5,15 +5,15 @@
 # LICENSE file in the root directory of this source tree.
 
 from ._checkpoint_utils import convert_llama2_fair_format
+from ._component_builders import llama2, lora_llama2
 from ._convert_weights import (  # noqa
     hf_to_tune_llama2_7b,
     meta_to_tune_llama2_7b,
     tune_to_hf_llama2_7b,
     tune_to_meta_llama2_7b,
 )
+from ._model_builders import llama2_7b, llama2_tokenizer, lora_llama2_7b
 from ._model_utils import scale_hidden_dim_for_mlp
-from ._component_builders import llama2, lora_llama2
-from ._model_builders import llama2_7b, lora_llama2_7b, llama2_tokenizer
 
 __all__ = [
     "convert_llama2_fair_format",
