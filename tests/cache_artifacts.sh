65ebb629962e0db4fac77b5401c81c9070753021
Evan Smothers <ebs@fb.com>
2024-03-21 21:12:26 -0700

    Get artifact caching + pytest flags working properly

--- tests/cache_artifacts.sh
+++ tests/cache_artifacts.sh
@@ -5,17 +5,61 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Script to cache artifacts for all tests
+# This script will handle caching of all artifacts needed for a given test run.
+# ./cache_artifacts.sh alone is a no-op
+# ./cache_artifacts --run-recipe-tests will fetch tokenizer and small model checkpoints.
+# ./cache_artifacts --run-regression-tests will fetch tokenizer and 7B model checkpoint.
+# ./cache_artifacts --run-recipe-tests --run-regression-tests will fetch all of the above.
+# In all cases, if the files already exist locally they will not be downloaded from S3.
 
-LOCAL_DIR="/tmp/test-artifacts"
-S3_URLS=(
-    "s3://pytorch-multimodal/llama2-7b/tokenizer.model"
+SMALL_MODEL_URLS=(
+    "s3://pytorch-multimodal/small-ckpt-01242024"
     "s3://pytorch-multimodal/small-ckpt-tune-03082024.pt"
     "s3://pytorch-multimodal/small-ckpt-meta-03082024.pt"
     "s3://pytorch-multimodal/small-ckpt-hf-03082024.pt"
-    "s3://pytorch-multimodal/llama2-7b-torchtune.pt"
 )
+FULL_MODEL_URL=("s3://pytorch-multimodal/llama2-7b-torchtune.pt")
+TOKENIZER_URL=("s3://pytorch-multimodal/llama2-7b/tokenizer.model")
 
+LOCAL_DIR="/tmp/test-artifacts"
+S3_URLS=()
+
+# Iterate over command-line args
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    case $arg in
+        "--run-recipe-tests")
+            # Add URLs for small models
+            for url in "${SMALL_MODEL_URLS[@]}"; do
+                S3_URLS+=( "$url" )
+            done
+            shift # Next argument
+            ;;
+        "--run-regression-tests")
+            # Add URL for large model
+            S3_URLS+=(
+                $FULL_MODEL_URL
+            )
+            shift # Next argument
+            ;;
+    esac
+done
+
+# If either recipe or regression tests are running,
+# fetch the tokenizer
+if ! [ -z "$S3_URLS" ]; then
+    S3_URLS+=(
+        $TOKENIZER_URL
+    )
+fi
+
+# Sanity check debug log
+echo "Expected artifacts for test run are:"
+for url in "${S3_URLS[@]}"; do
+    echo "$(basename "$url")"
+done
+
+# Download relevant files from S3 to local
 mkdir -p $LOCAL_DIR
 for S3_URL in "${S3_URLS[@]}"; do
     FILE_NAME=$(basename "$S3_URL")
--- tests/conftest.py
+++ tests/conftest.py
@@ -14,12 +14,48 @@ import torch.distributed.launcher as pet
 
 import torchtune
 
-root = Path(torchtune.__file__).parent.parent.absolute()
-CACHE_ARTIFACTS_SCRIPT_PATH = Path.joinpath(root, "tests", "cache_artifacts.sh")
+root = str(Path(torchtune.__file__).parent.parent.absolute())
+CACHE_ARTIFACTS_SCRIPT_PATH = root + "/tests/cache_artifacts.sh"
 
 
-def pytest_sessionstart(session):
-    os.system(CACHE_ARTIFACTS_SCRIPT_PATH)
+def pytest_configure(config):
+    """
+    This hook runs before each pytest invocation. Its purpose is to handle optional fetching
+    of remote artifacts needed for the test run. For testing, you should run one of the following:
+
+    - `pytest tests --without-integration --without-slow-integration`: run unit tests only
+    - `pytest tests --without-slow-integration`: run unit tests and recipe tests
+    - `pytest tests`: run all tests
+    - `pytest tests -m integration_test`: run recipe tests only
+    - `pytest tests -m slow_integration_test`: run regression tests only
+
+    This hook ensures that the appropriate artifacts are available locally for each of these cases.
+    """
+    # Default is to run both integration and slow integration tests (i.e. both are None)
+    run_recipe_tests = (
+        config.option.run_integration is None or config.option.run_integration is True
+    )
+    run_regression_tests = (
+        config.option.run_slow_integration is None
+        or config.option.run_slow_integration is True
+    )
+
+    # For -m flags, we run only those tests and so disable the others here
+    if config.option.markexpr == "integration_test":
+        run_regression_tests = False
+    if config.option.markexpr == "slow_integration_test":
+        run_recipe_tests = False
+
+    cmd = str(CACHE_ARTIFACTS_SCRIPT_PATH)
+
+    if run_recipe_tests:
+        cmd += " --run-recipe-tests"
+    if run_regression_tests:
+        cmd += " --run-regression-tests"
+
+    # Only need to handle artifacts for recipe and regression tests
+    if run_recipe_tests or run_regression_tests:
+        os.system(cmd)
 
 
 @pytest.fixture(scope="session")
