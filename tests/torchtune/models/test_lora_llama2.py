653a5013824f34bc5f958ce734eaeeaeed7c8aad
Evan Smothers <ebs@fb.com>
2024-03-22 20:05:54 -0700

    Skip QLoRA unit tests broken by latest ao nightly

--- tests/torchtune/models/test_lora_llama2.py
+++ tests/torchtune/models/test_lora_llama2.py
@@ -181,6 +181,7 @@ class TestLoRALlama2:
         assert not unexpected
         assert all(["lora" in key for key in missing])
 
+    @pytest.mark.skip(reason="broken by ao nightly")
     def test_lora_linear_quantize_base(self):
         model = self.get_lora_llama2(
             lora_modules=["q_proj", "v_proj", "k_proj", "output_proj"],
@@ -195,6 +196,7 @@ class TestLoRALlama2:
             if isinstance(module, LoRALinear):
                 assert module._quantize_base
 
+    @pytest.mark.skip(reason="broken by ao nightly")
     def test_qlora_llama2_parity(self, inputs):
         with utils.set_default_dtype(torch.bfloat16):
             model_ref = self.get_lora_llama2(
@@ -222,6 +224,7 @@ class TestLoRALlama2:
         output = qlora(inputs)
         torch.testing.assert_close(ref_output, output)
 
+    @pytest.mark.skip(reason="broken by ao nightly")
     def test_qlora_llama2_state_dict(self):
         with utils.set_default_dtype(torch.bfloat16):
             model_ref = self.get_lora_llama2(
@@ -255,6 +258,7 @@ class TestLoRALlama2:
             for v in qlora_sd.values():
                 assert v.dtype == torch.bfloat16
 
+    @pytest.mark.skip(reason="broken by ao nightly")
     def test_qlora_llama2_merged_state_dict(self):
         with utils.set_default_dtype(torch.bfloat16):
             qlora = self.get_lora_llama2(
