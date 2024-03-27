ef54a339a156cab9ec56ea51d7a65a64c2c13ccd
Kartikay Khandelwal <kartikayk@fb.com>
2024-03-26 18:11:09 -0700

    lint

--- torchtune/models/mistral/__init__.py
+++ torchtune/models/mistral/__init__.py
@@ -4,5 +4,5 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ._component_builders import mistral, lora_mistral  # noqa
+from ._component_builders import lora_mistral, mistral  # noqa
 from ._model_builders import lora_mistral_7b, mistral_7b, mistral_tokenizer  # noqa
