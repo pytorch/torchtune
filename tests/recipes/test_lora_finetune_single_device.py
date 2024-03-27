adfcb6890db8a1c716ce68c51e2d06eb6ffe636a
joecummings <jrcummings27@gmail.com>
2024-03-26 18:13:42 -0700

    Lora finetune single device recipe should pass

--- tests/recipes/test_lora_finetune_single_device.py
+++ tests/recipes/test_lora_finetune_single_device.py
@@ -57,10 +57,10 @@ class TestLoRAFinetuneSingleDeviceRecipe:
         log_file = gen_log_file_name(tmpdir)
 
         cmd = f"""
-        tune run lora_finetune_single_device.py
-            --config llama2/7B_lora_single_device.yaml \
+        tune run lora_finetune_single_device \
+            --config llama2/7B_lora_single_device \
             output_dir={tmpdir} \
-            checkpointer=torchtune.utils.FullModelMetaCheckpointer
+            checkpointer=torchtune.utils.FullModelMetaCheckpointer \
             checkpointer.checkpoint_dir='{ckpt_dir}' \
             checkpointer.checkpoint_files=[{ckpt_path}]\
             checkpointer.output_dir={tmpdir} \
@@ -78,7 +78,8 @@ class TestLoRAFinetuneSingleDeviceRecipe:
 
         cmd = cmd + self._get_test_config_overrides(dtype_str="fp32") + model_config
         monkeypatch.setattr(sys, "argv", cmd)
-        runpy.run_path(TUNE_PATH, run_name="__main__")
+        with pytest.raises(SystemExit, match=""):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
 
         loss_values = get_loss_values_from_metric_logger(log_file)
         expected_loss_values = self._fetch_expected_loss_values(run_qlora=False)
@@ -147,8 +148,8 @@ class TestLoRAFinetuneSingleDeviceRecipe:
 
         # Train for two epochs
         cmd_1 = f"""
-        tune run lora_finetune_single_device.py
-            --config llama2/7B_lora_single_device.yaml \
+        tune run lora_finetune_single_device \
+            --config llama2/7B_lora_single_device \
             output_dir={tmpdir} \
             checkpointer=torchtune.utils.FullModelHFCheckpointer \
             checkpointer.checkpoint_dir='{ckpt_dir}' \
@@ -167,12 +168,13 @@ class TestLoRAFinetuneSingleDeviceRecipe:
 
         cmd_1 = cmd_1 + self._get_test_config_overrides() + model_config
         monkeypatch.setattr(sys, "argv", cmd_1)
-        runpy.run_path(TUNE_PATH, run_name="__main__")
+        with pytest.raises(SystemExit, match=""):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
 
         # Resume training
         cmd_2 = f"""
-        tune run lora_finetune_single_device.py
-            --config llama2/7B_lora_single_device.yaml \
+        tune run lora_finetune_single_device \
+            --config llama2/7B_lora_single_device \
             output_dir={tmpdir} \
             checkpointer=torchtune.utils.FullModelHFCheckpointer \
             checkpointer.checkpoint_dir={tmpdir} \
@@ -186,7 +188,8 @@ class TestLoRAFinetuneSingleDeviceRecipe:
         """.split()
         cmd_2 = cmd_2 + self._get_test_config_overrides() + model_config
         monkeypatch.setattr(sys, "argv", cmd_2)
-        runpy.run_path(TUNE_PATH, run_name="__main__")
+        with pytest.raises(SystemExit, match=""):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
 
         # Second epoch only
         expected_loss_values = self._fetch_expected_loss_values()[2:]
@@ -203,10 +206,10 @@ class TestLoRAFinetuneSingleDeviceRecipe:
         ckpt_dir = ckpt_path.parent
 
         cmd = f"""
-        tune run lora_finetune_single_device.py
-            --config llama2/7B_lora_single_device.yaml \
+        tune run lora_finetune_single_device \
+            --config llama2/7B_lora_single_device \
             output_dir={tmpdir} \
-            checkpointer=torchtune.utils.FullModelTorchTuneCheckpointer
+            checkpointer=torchtune.utils.FullModelTorchTuneCheckpointer \
             checkpointer.checkpoint_dir='{ckpt_dir}' \
             checkpointer.checkpoint_files=[{ckpt_path}]\
             checkpointer.output_dir={tmpdir} \
@@ -223,7 +226,8 @@ class TestLoRAFinetuneSingleDeviceRecipe:
 
         cmd = cmd + self._get_test_config_overrides() + model_config
         monkeypatch.setattr(sys, "argv", cmd)
-        runpy.run_path(TUNE_PATH, run_name="__main__")
+        with pytest.raises(SystemExit, match=""):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
 
         # Next load both the merged weights in a Llama2 base model
         # and the base model weights + trained adapter weights in the LoRA Llama 2 model
