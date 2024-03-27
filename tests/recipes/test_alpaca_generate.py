c68fd1d33ea38e4accb197e7f52eb04ce9aa29ec
joecummings <jrcummings27@gmail.com>
2024-03-26 18:00:44 -0700

    Alpaca recipe should pass

--- tests/recipes/test_alpaca_generate.py
+++ tests/recipes/test_alpaca_generate.py
@@ -4,22 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import logging
-
 import runpy
 import sys
 
 import pytest
 
 from tests.common import TUNE_PATH
-from tests.recipes.common import RECIPE_TESTS_DIR
 from tests.recipes.utils import llama2_test_config
 from tests.test_utils import CKPT_MODEL_PATHS
-
-_CONFIG_PATH = RECIPE_TESTS_DIR / "alpaca_generate_test_config.yaml"
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 class TestAlpacaGenerateRecipe:
@@ -28,8 +20,8 @@ class TestAlpacaGenerateRecipe:
         ckpt = "small_test_ckpt_tune"
         model_checkpoint = CKPT_MODEL_PATHS[ckpt]
         cmd = f"""
-        tune run alpaca_generate.py
-            --config alpaca_generate.yaml \
+        tune run alpaca_generate \
+            --config alpaca_generate \
             model_checkpoint={model_checkpoint} \
             tokenizer.path=/tmp/test-artifacts/tokenizer.model \
             output_dir={tmpdir} \
@@ -39,4 +31,5 @@ class TestAlpacaGenerateRecipe:
         cmd += model_config
 
         monkeypatch.setattr(sys, "argv", cmd)
-        runpy.run_path(TUNE_PATH, run_name="__main__")
+        with pytest.raises(SystemExit, match=""):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
