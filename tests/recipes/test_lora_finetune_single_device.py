183169d8664a3ccd8b79dfcc7b3cd5f43ceafc71
Rohan Varma <rvarm1@fb.com>
2024-03-28 11:40:26 -0700

    Add test

--- tests/recipes/test_lora_finetune_single_device.py
+++ tests/recipes/test_lora_finetune_single_device.py
@@ -43,11 +43,13 @@ class TestLoRAFinetuneSingleDeviceRecipe:
             "log_every_n_steps=1",
         ]
 
-    def _fetch_expected_loss_values(self, run_qlora: bool = False):
-        if run_qlora:
+    def _fetch_expected_loss_values(self):
+        return [10.5074, 10.5614, 10.5205, 10.4918]
+
+    def _fetch_qlora_expected_loss_values(self, dtype):
+        if dtype == "bf16":
             return [10.5057, 10.5575, 10.5179, 10.4898]
-        else:
-            return [10.5074, 10.5614, 10.5205, 10.4918]
+        return [10.5059, 10.5571, 10.5181, 10.4897]
 
     @pytest.mark.integration_test
     def test_loss(self, tmpdir, monkeypatch):
@@ -82,13 +84,14 @@ class TestLoRAFinetuneSingleDeviceRecipe:
             runpy.run_path(TUNE_PATH, run_name="__main__")
 
         loss_values = get_loss_values_from_metric_logger(log_file)
-        expected_loss_values = self._fetch_expected_loss_values(run_qlora=False)
+        expected_loss_values = self._fetch_expected_loss_values()
         torch.testing.assert_close(
             loss_values, expected_loss_values, rtol=1e-5, atol=1e-5
         )
 
     @pytest.mark.integration_test
-    def test_loss_qlora(self, tmpdir, monkeypatch):
+    @pytest.mark.parametrize("dtype", ["fp32", "bf16"])
+    def test_loss_qlora(self, dtype, tmpdir, monkeypatch):
         ckpt = "small_test_ckpt_meta"
         ckpt_path = Path(CKPT_MODEL_PATHS[ckpt])
         ckpt_dir = ckpt_path.parent
@@ -114,14 +117,13 @@ class TestLoRAFinetuneSingleDeviceRecipe:
             lora_alpha=16,
         )
 
-        # TODO (rohan-varma): QLoRA only supported with bf16 for now
-        cmd = cmd + self._get_test_config_overrides(dtype_str="bf16") + model_config
+        cmd = cmd + self._get_test_config_overrides(dtype_str=dtype) + model_config
         monkeypatch.setattr(sys, "argv", cmd)
         with pytest.raises(SystemExit):
             runpy.run_path(TUNE_PATH, run_name="__main__")
 
         loss_values = get_loss_values_from_metric_logger(log_file)
-        expected_loss_values = self._fetch_expected_loss_values(run_qlora=True)
+        expected_loss_values = self._fetch_qlora_expected_loss_values(dtype=dtype)
         torch.testing.assert_close(
             loss_values, expected_loss_values, rtol=1e-5, atol=1e-5
         )
