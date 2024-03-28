88611e3603efc5b8252b832cef5d0517ff174de6
joecummings <jrcummings27@gmail.com>
2024-03-28 10:08:39 -0700

    Update

--- tests/torchtune/_cli/test_download.py
+++ tests/torchtune/_cli/test_download.py
@@ -2,6 +2,7 @@ import runpy
 import sys
 
 import pytest
+import huggingface_hub
 from huggingface_hub.utils import GatedRepoError, RepositoryNotFoundError
 
 from tests.common import TUNE_PATH
@@ -10,18 +11,20 @@ from tests.common import TUNE_PATH
 class TestTuneDownloadCommand:
     """This class tests the `tune download` command."""
 
-    def test_download_calls_model_info_error(self, capsys, monkeypatch, mocker):
+    @pytest.fixture
+    def model_info(self, mocker):
+        return mocker.patch.object(
+            huggingface_hub,
+            "model_info",
+            return_value=None,
+            # Side effects are iterated through on each call
+            side_effect=[GatedRepoError("test"), RepositoryNotFoundError("test"), mocker.DEFAULT],
+        )
+
+    def test_download_calls_model_info_error(self, capsys, monkeypatch, model_info):
         model = "meta-llama/Llama-2-7b"
         testargs = f"tune download {model}".split()
         monkeypatch.setattr(sys, "argv", testargs)
-
-        # Side effects are iterated through on each call
-        model_info = mocker.patch.object(
-            huggingface_hub,
-            "model_info",
-            side_effect=[GatedRepoError("test"), RepositoryNotFoundError("test")],
-            autospec=True,
-        )
 
         # Call the first time and get GatedRepoError
         with pytest.raises(SystemExit, match="2"):
@@ -41,14 +44,17 @@ class TestTuneDownloadCommand:
         # Make sure it was called twice
         assert model_info.call_count == 2
 
-    def test_download_calls_snapshot(self, capsys, tmpdir, monkeypatch, mocker):
+    def test_download_calls_snapshot(self, capsys, tmpdir, monkeypatch, model_info, mocker):
         model = "mistralai/Mistral-7b-v0.1"
         testargs = f"tune download {model} --output-dir {tmpdir}".split()
 
         snapshot_download = mocker.patch.object(
-            huggingface_hub, "snapshot_download", return_value=tmpdir, autospec=True
+            huggingface_hub,
+            "snapshot_download",
+            return_value=tmpdir,
         )
-        monkeypatch.setattr(sys, "argv", testargs)
 
+        monkeypatch.setattr(sys, "argv", testargs)
         runpy.run_path(TUNE_PATH, run_name="__main__")
+
         snapshot_download.assert_called_once()
