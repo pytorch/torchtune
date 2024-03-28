177adf7442536dec636bc63eb141e7c5c33362fe
joecummings <jrcummings27@gmail.com>
2024-03-28 13:13:56 -0700

    lint

--- torchtune/_cli/run.py
+++ torchtune/_cli/run.py
@@ -5,9 +5,9 @@
 # LICENSE file in the root directory of this source tree.
 
 import argparse
+import runpy
 import sys
 import textwrap
-import runpy
 
 from pathlib import Path
 from typing import Optional
