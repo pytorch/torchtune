1928a473c1c51b00f7af7e674260f2dc6038b9a2
solitude-alive <thisyzg@gmail.com>
2024-03-31 22:04:26 +0000

    Create Gemma models.

---
+++ torchtune/models/gemma/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ._component_builders import gemma, lora_gemma  # noqa
+from ._model_builders import gemma_2b, gemma_tokenizer, lora_gemma_2b  # noqa
