c4cf3eb6991b600f35fd82d13d3b95ea88e90dbb
joecummings <jrcummings27@gmail.com>
2024-04-02 16:47:29 -0700

    Properly remove file names

--- tests/recipes/test_full_finetune_single_device.py
+++ tests/recipes/test_full_finetune_single_device.py
@@ -102,7 +102,7 @@ class TestFullFinetuneSingleDeviceRecipe:
         # Train for two epochs
         cmd_1 = f"""
         tune run full_finetune_single_device \
-            --config llama2/7B_full_single_device \
+            --config llama2/7B_full \
             output_dir={tmpdir} \
             checkpointer._component_=torchtune.utils.FullModelHFCheckpointer \
             checkpointer.checkpoint_dir='{ckpt_dir}' \
@@ -121,7 +121,7 @@ class TestFullFinetuneSingleDeviceRecipe:
         # Resume training
         cmd_2 = f"""
         tune run full_finetune_single_device \
-            --config llama2/7B_full_single_device \
+            --config llama2/7B_full \
             output_dir={tmpdir} \
             checkpointer._component_=torchtune.utils.FullModelHFCheckpointer \
             checkpointer.checkpoint_dir={tmpdir} \
@@ -184,7 +184,7 @@ class TestFullFinetuneSingleDeviceGradientAccumulation:
 
         cmd_1 = f"""
         tune run full_finetune_single_device \
-            --config llama2/7B_full_single_device \
+            --config llama2/7B_full \
             checkpointer._component_=torchtune.utils.FullModelTorchTuneCheckpointer \
             checkpointer.checkpoint_dir={ckpt_dir} \
             checkpointer.checkpoint_files=[{ckpt_path}]\
@@ -210,7 +210,7 @@ class TestFullFinetuneSingleDeviceGradientAccumulation:
         # Update the cmd with new values for gradient accumulation
         cmd_2 = f"""
         tune run full_finetune_single_device \
-            --config llama2/7B_full_single_device \
+            --config llama2/7B_full \
             checkpointer._component_=torchtune.utils.FullModelTorchTuneCheckpointer \
             checkpointer.checkpoint_dir={ckpt_dir} \
             checkpointer.checkpoint_files=[{ckpt_path}]\
