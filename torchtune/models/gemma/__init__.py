640925d0f61b28911fdf9acd8d0bd15e1bf8bf22
solitude-alive <thisyzg@gmail.com>
2024-04-02 21:26:51 +0000

    Remove the LoRA code.

--- torchtune/models/gemma/__init__.py
+++ torchtune/models/gemma/__init__.py
@@ -4,5 +4,5 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ._component_builders import gemma, lora_gemma  # noqa
-from ._model_builders import gemma_2b, gemma_tokenizer, lora_gemma_2b  # noqa
+from ._component_builders import gemma  # noqa
+from ._model_builders import gemma_2b, gemma_tokenizer  # noqa
