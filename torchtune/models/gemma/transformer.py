9889a59d258865e65a10a648961fe0b0be7b4101
solitude-alive <thisyzg@gmail.com>
2024-04-04 19:21:34 +0000

    Update GemmaTransformerDecoder.

---
+++ torchtune/models/gemma/transformer.py
@@ -0,0 +1,112 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Optional
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from torchtune.modules.transformer import _get_clones, TransformerDecoderLayer
+
+
+class GemmaTransformerDecoder(nn.Module):
+    """
+    Transformer Decoder derived from the Llama2 architecture.
+
+    Args:
+        tok_embeddings (nn.Embedding): PyTorch embedding layer, to be used to move tokens to an embedding space.
+        layer (TransformerDecoderLayer): Transformer Decoder layer.
+        num_layers (int): Number of Transformer Decoder layers.
+        max_seq_len (int): maximum sequence length the model will be run with, as used
+            by :func:`~torchtune.modules.KVCache`
+        num_heads (int): number of query heads. For MHA this is also the
+            number of heads for key and value. This is used to setup the
+            :func:`~torchtune.modules.KVCache`
+        head_dim (int): embedding dimension for each head in self-attention. This is used
+            to setup the :func:`~torchtune.modules.KVCache`
+        norm (nn.Module): Callable that applies normalization to the output of the decoder,
+            before final MLP.
+        output (nn.Linear): Callable that applies a linear transformation to the output of
+            the decoder.
+        norm_embeddings (bool): Whether to apply normalization before the self-attention layer, defaults to False.
+
+    Note:
+        Arg values are checked for correctness (eg: ``attn_dropout`` belongs to [0,1])
+        in the module where they are used. This helps reduces the number of raise
+        statements in code and improves readability.
+    """
+
+    def __init__(
+        self,
+        tok_embeddings: nn.Embedding,
+        layer: TransformerDecoderLayer,
+        num_layers: int,
+        max_seq_len: int,
+        num_heads: int,
+        head_dim: int,
+        norm: nn.Module,
+        output: nn.Linear,
+        norm_embeddings: bool = False,
+    ) -> None:
+        super().__init__()
+        self.tok_embeddings = tok_embeddings
+        self.layers = _get_clones(layer, num_layers)
+        self.norm = norm
+        self.output = output
+        self.max_seq_len = max_seq_len
+        self.num_heads = num_heads
+        self.head_dim = head_dim
+        self.causal_mask = None
+        self.norm_embeddings = norm_embeddings
+
+    def forward(
+        self, tokens: Tensor, mask: Optional[Tensor] = None, curr_pos: int = 0
+    ) -> Tensor:
+        """
+        Args:
+            tokens (Tensor): input tensor with shape [b x s]
+            mask (Optional[Tensor]): attention mask tensor, defaults to None.
+            curr_pos (int): current position in the seq, defaults to 0.
+                Only relevant when incrementally decoding.
+
+        Returns:
+            Tensor: output tensor with shape [b x s x v]
+
+        Notation used for tensor shapes:
+            - b: batch size
+            - s: sequence length
+            - v: vocab size
+            - d: embed dim
+        """
+        # input tensor of shape [b, s]
+        bsz, seq_len = tokens.shape
+
+        # shape: [b, s, d]
+        h = self.tok_embeddings(tokens)
+
+        # TODO: Fix the masking logic to not rely on checking kv_cache
+        if seq_len > 1 and self.layers[0].attn.kv_cache is not None:
+            mask = torch.full(
+                (1, 1, seq_len, seq_len), float("-inf"), device=tokens.device
+            )
+            mask = torch.triu(mask, diagonal=curr_pos + 1)
+
+        if self.norm_embeddings:
+            hidden_size = h.size(-1)
+            normalizer = torch.tensor(hidden_size**0.5, dtype=h.dtype)
+            h = h * normalizer
+
+        for layer in self.layers:
+            # shape: [b, s, d]
+            h = layer(h, mask, curr_pos)
+
+        # shape: [b, s, d]
+        h = self.norm(h)
+
+        # shape: [b, s, v]
+        output = self.output(h).float()
+        return output
