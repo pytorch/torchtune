cba056085161385110944dc5a6fc6a6a2c7da394
Rafi Ayub <33648637+RdoubleA@users.noreply.github.com>
2024-04-04 12:02:46 -0700

    Validate messages (#647)

--- torchtune/data/_utils.py
+++ torchtune/data/_utils.py
@@ -6,6 +6,8 @@
 
 from typing import Any, List
 
+from torchtune.data._types import Message
+
 
 def truncate(
     tokens: List[Any],
@@ -16,3 +18,49 @@ def truncate(
     if tokens_truncated[-1] != eos_id:
         tokens_truncated[-1] = eos_id
     return tokens_truncated
+
+
+def validate_messages(
+    messages: List[Message],
+) -> None:
+    """
+    Given a list of messages, ensure that messages form a valid
+    back-and-forth conversation. An error will be raised if:
+        - There is a system message that's not the first message
+        - There are two consecutive user messages
+        - An assistant message comes before the first user message
+        - The message is empty
+        - Messages are shorter than length of 2 (min. one user-assistant turn)
+
+    Args:
+        messages (List[Message]): the messages to validate.
+
+    Raises:
+        ValueError: If the messages are invalid.
+    """
+    if len(messages) < 2:
+        raise ValueError(
+            f"Messages must be at least length 2, but got {len(messages)} messages"
+        )
+
+    last_turn = "assistant"
+    for i, message in enumerate(messages):
+        if message.role == "assistant" and last_turn != "user":
+            raise ValueError(
+                f"Assistant message before expected user message at index {i} in messages"
+            )
+        if message.role == "user" and last_turn == "user":
+            raise ValueError(
+                f"Two consecutive user messages at index {i} and {i - 1} in messages"
+            )
+        if message.role == "system" and i > 0:
+            raise ValueError(
+                f"System message at index {i} in messages, but system messages must come first"
+            )
+        # Assistant messages can be empty because they will not be tokenized and
+        # will not contribute to the loss, assuming the entire batch is not full
+        # of empty assistant messages. The alpaca dataset is an example of the
+        # output assistant message being empty sometimes.
+        if not message.content and message.role != "assistant":
+            raise ValueError(f"Message at index {i} in messages is empty")
+        last_turn = message.role
