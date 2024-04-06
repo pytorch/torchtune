8d72e0116b41342f3bae0d83bad2e9c0706bcd1a
joecummings <jrcummings27@gmail.com>
2024-04-05 17:10:36 -0700

    Hardcode version into __init__

--- src/torchtune/__init__.py
+++ src/torchtune/__init__.py
@@ -4,10 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+__version__ = "0.0.1"
+
 from torchtune import datasets, models, modules, utils
 
 __all__ = [datasets, models, modules, utils]
-
-# Import version from version.txt
-with open("../../version.txt", "r") as f:
-    __version__ = f.read().strip()
