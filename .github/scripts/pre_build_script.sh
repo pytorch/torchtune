76b47151644d1ec8ee47629d4eb6df3a2402f5c3
joecummings <jrcummings27@gmail.com>
2024-04-10 14:00:33 -0700

    Only replace __version__ variable

--- .github/scripts/pre_build_script.sh
+++ .github/scripts/pre_build_script.sh
@@ -3,13 +3,7 @@
 # Update __version__ in __init__.py using BUILD_VERSION environment variable
 python_file="torchtune/__init__.py"
 if [[ -n "$BUILD_VERSION" ]]; then
-    version_line=$(grep "^__version__ = " "$python_file")
-    if [[ -n "$version_line" ]]; then
-        echo "$version_line" | sed "s/='.*'$/'='$BUILD_VERSION'/" > tmp && mv tmp "$python_file"
-    else
-        echo "Error: __version__ line not found in $python_file."
-        exit 1
-    fi
+    sed "s/^__version__ = .*/__version__ = '$BUILD_VERSION'/" "$python_file" > tmp && mv tmp "$python_file"
 else
     echo "Error: BUILD_VERSION environment variable is not set or empty."
     exit 1
