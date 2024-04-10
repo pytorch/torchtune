3ba4625a0281b37867992391430faa0b758b2102
pytorchbot <pytorchbot@pytorch.com>
2024-04-10 21:19:48 +0000

    2024-04-10 nightly release (a740761f188d43c0b0f454f3bf59f6b510d37868)

--- torchtune/__init__.py
+++ torchtune/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "0.0.1"
+__version__ = ""
 
 from torchtune import datasets, models, modules, utils
 
