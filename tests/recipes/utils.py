28b12a15bc7f29c0663bb4298e3b7f28146c769d
Evan Smothers <ebs@fb.com>
2024-04-11 19:34:30 -0700

    Local Alpaca for recipe tests

--- tests/recipes/utils.py
+++ tests/recipes/utils.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import json
+import os
 from pathlib import Path
 from typing import List, Optional
 
@@ -36,6 +37,65 @@ class DummyDataset(Dataset):
 
     def __len__(self):
         return len(self._data)
+
+
+def get_assets_path():
+    return Path(__file__).parent.parent / "assets"
+
+
+# class DummyInstructDataset(InstructDataset):
+#     def __init__(
+#         self,
+#         tokenizer: Tokenizer,
+#         source: str,
+#         template: InstructTemplate,
+#         transform: Optional[Callable] = None,
+#         column_map: Optional[Dict[str, str]] = None,
+#         train_on_input: bool = False,
+#         max_seq_len: Optional[int] = None,
+#         **load_dataset_kwargs: Dict[str, Any],
+#     ):
+#         source = "text"
+#         load_dataset_kwargs = {
+#             "data_files": {"train": [get_assets_path() / "alpaca_tiny.txt]}
+#         }
+#         super().__init__(
+#         tokenizer: Tokenizer,
+#         source: str,
+#         template: InstructTemplate,
+#         transform: Optional[Callable] = None,
+#         column_map: Optional[Dict[str, str]] = None,
+#         train_on_input: bool = False,
+#         max_seq_len: Optional[int] = None,
+#         **load_dataset_kwargs: Dict[str, Any],
+#     )
+
+# def dummy_alpaca_dataset(
+#     tokenizer: Tokenizer,
+#     train_on_input: bool = True,
+#     max_seq_len: int = 512,
+# ):
+#     return InstructDataset(
+#         tokenizer=tokenizer,
+#         source="text",
+#         template=AlpacaInstructTemplate,
+#         train_on_input=train_on_input,
+#         max_seq_len=max_seq_len,
+#         split="train",
+#         data_files=,
+#     )
+
+
+def dummy_alpaca_dataset_config():
+    data_files = os.path.join(get_assets_path(), "alpaca_tiny.json")
+    out = [
+        "dataset._component_=torchtune.datasets.instruct_dataset",
+        "dataset.source='json'",
+        f"dataset.data_files={data_files}",
+        "dataset.template=AlpacaInstructTemplate",
+        "dataset.split='train'",
+    ]
+    return out
 
 
 def llama2_test_config(max_batch_size: Optional[int] = None) -> List[str]:
