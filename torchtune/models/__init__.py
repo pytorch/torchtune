0f1337a543695200fa5a632a5fcb2433530f34fc
Rohan Varma <rvarm1@fb.com>
2024-04-12 12:25:30 -0700

    upd

--- torchtune/models/__init__.py
+++ torchtune/models/__init__.py
@@ -4,5 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .common_utils import reparametrize_as_dtype_state_dict_post_hook  # noqa
 from torchtune.models import convert_weights, gemma, llama2, mistral  # noqa
+
+from .common_utils import reparametrize_as_dtype_state_dict_post_hook  # noqa
