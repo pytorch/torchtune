7f92869486dea4f0dd8e17068eb16b232bdc583c
joecummings <jrcummings27@gmail.com>
2024-04-15 09:56:07 -0700

    Linting

--- torchtune/models/llama2/__init__.py
+++ torchtune/models/llama2/__init__.py
@@ -13,8 +13,8 @@ from ._model_builders import (  # noqa
     llama2_tokenizer,
     lora_llama2_13b,
     lora_llama2_7b,
-    qlora_llama2_7b,
     qlora_llama2_13b,
+    qlora_llama2_7b,
 )
 from ._model_utils import scale_hidden_dim_for_mlp
 
