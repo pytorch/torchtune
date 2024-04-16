3574e99c6632d45120b48a70f1ed24a6ca5b8807
efenocchi <emanuele.fenocchi@gmail.com>
2024-04-16 18:31:01 +0200

    introduce RAFT settings

--- torchtune/datasets/__init__.py
+++ torchtune/datasets/__init__.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from torchtune.datasets._alpaca import alpaca_cleaned_dataset, alpaca_dataset
+from torchtune.datasets._raft import raft_dataset
 from torchtune.datasets._chat import chat_dataset, ChatDataset
 from torchtune.datasets._grammar import grammar_dataset
 from torchtune.datasets._instruct import instruct_dataset, InstructDataset
@@ -15,6 +16,7 @@ from torchtune.datasets._stack_exchanged_paired import stack_exchanged_paired_da
 __all__ = [
     "alpaca_dataset",
     "alpaca_cleaned_dataset",
+    "raft_dataset",
     "grammar_dataset",
     "samsum_dataset",
     "stack_exchanged_paired_dataset",
