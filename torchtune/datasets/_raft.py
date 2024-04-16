d2563044333b5f8658e4ebf15cfd4c8771a0e80d
efenocchi <emanuele.fenocchi@gmail.com>
2024-04-16 18:31:26 +0200

    raft dataset setting

---
+++ torchtune/datasets/_raft.py
@@ -0,0 +1,31 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from functools import partial
+
+from torchtune.data import RAFTInstructTemplate
+from torchtune.datasets._instruct_deeplake import InstructDatasetDeepLakeRAFT
+from torchtune.modules import Tokenizer
+import os
+
+dataset_source_env = os.getenv("dataset_activeloop")
+
+
+def raft_dataset(
+    tokenizer: Tokenizer,
+    source: str = dataset_source_env,
+    train_on_input: bool = True,
+    max_seq_len: int = 512,
+) -> InstructDatasetDeepLakeRAFT:
+
+    return InstructDatasetDeepLakeRAFT(
+        tokenizer=tokenizer,
+        source=source,
+        template=RAFTInstructTemplate,
+        train_on_input=train_on_input,
+        max_seq_len=max_seq_len,
+        split="train",
+    )
