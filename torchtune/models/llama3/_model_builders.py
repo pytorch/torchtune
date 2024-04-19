4f7b1a6a06d30d83f40381ad815ac068e2f669ed
Musab Gültekin <87330355+musab-mk@users.noreply.github.com>
2024-04-19 21:25:54 +0300

    Fix max_seq_len for llama 3 70b

--- torchtune/models/llama3/_model_builders.py
+++ torchtune/models/llama3/_model_builders.py
@@ -57,7 +57,7 @@ def llama3_70b() -> TransformerDecoder:
         num_heads=64,
         num_kv_heads=8,
         embed_dim=8192,
-        max_seq_len=4096,
+        max_seq_len=8192,
         intermediate_dim=28672,
         attn_dropout=0.0,
         norm_eps=1e-5,
