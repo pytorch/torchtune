36e1754f66852ef9a095d7302c47bbb193241bda
Salman Mohammadi <salman.mohammadi@outlook.com>
2024-04-25 12:33:06 +0100

    adding missing import in torchtune/models/__init__.py

--- torchtune/models/__init__.py
+++ torchtune/models/__init__.py
@@ -4,4 +4,10 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchtune.models import convert_weights, gemma, llama2, mistral  # noqa
+from torchtune.models import (  # noqa
+    code_llama2,
+    convert_weights,
+    gemma,
+    llama2,
+    mistral,
+)
