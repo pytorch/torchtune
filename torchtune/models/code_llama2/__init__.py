b88baded6a540236ace802ce3877b78efe3f8ea9
Salman Mohammadi <salman.mohammadi@outlook.com>
2024-04-25 12:21:51 +0100

    Updating README.md, fixing mis-copied files

--- torchtune/models/code_llama2/__init__.py
+++ torchtune/models/code_llama2/__init__.py
@@ -3,3 +3,27 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+from ._model_builders import (  # noqa
+    code_llama2_13b,
+    code_llama2_70b,
+    code_llama2_7b,
+    lora_code_llama2_13b,
+    lora_code_llama2_70b,
+    lora_code_llama2_7b,
+    qlora_code_llama2_13b,
+    qlora_code_llama2_70b,
+    qlora_code_llama2_7b,
+)
+
+__all__ = [
+    "code_llama2_13b",
+    "code_llama2_70b",
+    "code_llama2_7b",
+    "lora_code_llama2_13b",
+    "lora_code_llama2_70b",
+    "lora_code_llama2_7b",
+    "qlora_code_llama2_13b",
+    "qlora_code_llama2_70b",
+    "qlora_code_llama2_7b",
+]
