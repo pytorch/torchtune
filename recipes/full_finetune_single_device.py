53e63eee88a54c951cd64a5bf6f4934bcbdf0fdb
Rohan Varma <rvarm1@fb.com>
2024-04-26 11:20:45 -0700

    upd

--- recipes/full_finetune_single_device.py
+++ recipes/full_finetune_single_device.py
@@ -248,11 +248,7 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
 
         if enable_activation_checkpointing:
             utils.set_activation_checkpointing(
-                model,
-                auto_wrap_policy=utils.get_ac_policy(
-                    model_type=str(self._checkpointer._model_type),
-                    modules_to_wrap={modules.TransformerDecoderLayer},
-                ),
+                model, auto_wrap_policy={modules.TransformerDecoderLayer}
             )
 
         model.load_state_dict(model_state_dict)
