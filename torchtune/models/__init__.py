389020019bb2d512bc915a5a16d0cf268121097f
ebsmothers <ebs@meta.com>
2024-04-26 16:51:24 -0700

    Remove unused imports in models/__init__.py (#886)

--- torchtune/models/__init__.py
+++ torchtune/models/__init__.py
@@ -3,11 +3,3 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-from torchtune.models import (  # noqa
-    code_llama2,
-    convert_weights,
-    gemma,
-    llama2,
-    mistral,
-)
