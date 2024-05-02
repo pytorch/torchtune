707fe015c31283c03d34a73cea1ffe5581c6183c
pytorchbot <pytorchbot@pytorch.com>
2024-05-02 11:34:48 +0000

    2024-05-02 nightly release (f5b8eafb296e8293627640fe07995c941cb4e819)

--- torchtune/models/phi3/_model_builders.py
+++ torchtune/models/phi3/_model_builders.py
@@ -1,9 +1,9 @@
 from typing import List
 
 from torchtune.models.phi3._component_builders import phi3
+from torchtune.models.phi3._sentencepiece import Phi3MiniSentencePieceTokenizer
 
 from torchtune.modules import TransformerDecoder
-from torchtune.modules.tokenizers import SentencePieceTokenizer
 from torchtune.modules.peft import LORA_ATTN_MODULES
 from functools import partial
 
@@ -21,6 +21,10 @@ def phi3_mini() -> TransformerDecoder:
     Builder for creating the Phi3 Mini 4K Instruct Model.
     Ref: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
 
+    Note:
+        This model does not currently support 128K context length nor optimizations
+        such as sliding window attention.
+
     Returns:
         TransformerDecoder: Instantiation of Phi3 Mini 4K Instruct Model
     """
@@ -36,7 +40,25 @@ def phi3_mini() -> TransformerDecoder:
         norm_eps=1e-5,
     )
 
-def phi3_tokenizer(path: str) -> SentencePieceTokenizer:
-    tokenizer = SentencePieceTokenizer(path)
-    tokenizer.pad_id = 0
+def phi3_mini_tokenizer(path: str) -> Phi3MiniSentencePieceTokenizer:
+    """Phi-3 Mini tokenizer.
+    Ref: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct/blob/main/tokenizer_config.json
+
+    Args:
+        path (str): Path to the SPM tokenizer model.
+
+    Note:
+        This tokenizer includes typical LM EOS and BOS tokens like
+        <s>, </s>, and <unk>. However, to support chat completion,
+        it is also augmented with special tokens like <|endoftext|>
+        and <|assistant|>.
+
+    Warning:
+        Microsoft currently opts to ignore system messages citing better performance.
+        See https://huggingface.co/microsoft/Phi-3-mini-4k-instruct/discussions/51 for more details.
+
+    Returns:
+        Phi3MiniSentencePieceTokenizer: Instantiation of the SPM tokenizer.
+    """
+    tokenizer = Phi3MiniSentencePieceTokenizer(path)
     return tokenizer
