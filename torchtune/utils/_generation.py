80d8184b0e41bf918f4a94a439f82dca5ccd86ab
joecummings <jrcummings27@gmail.com>
2024-05-07 14:51:21 -0700

    Remove extra license header

--- torchtune/utils/_generation.py
+++ torchtune/utils/_generation.py
@@ -4,12 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# # Copyright (c) Meta Platforms, Inc. and affiliates.
-# # All rights reserved.
-# #
-# # This source code is licensed under the BSD-style license found in the
-# # LICENSE file in the root directory of this source tree.
-
 from typing import Callable, List, Optional
 
 import torch
