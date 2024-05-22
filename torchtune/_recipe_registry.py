f0ea7501026fd2dda39e0b1b2210a4f2adee77df
Jeff <jeff.yang@cinnamon.is>
2024-05-22 12:49:18 +0000

    update recipe for llama3 custom instruct

--- torchtune/_recipe_registry.py
+++ torchtune/_recipe_registry.py
@@ -92,6 +92,10 @@ _ALL_RECIPES = [
                 file_path="llama3/8B_qlora_single_device.yaml",
             ),
             Config(
+                name="llama3/8B_qlora_single_device_custom_instruct",
+                file_path="llama3/8B_qlora_single_device_custom_instruct.yaml",
+            ),
+            Config(
                 name="llama2/13B_qlora_single_device",
                 file_path="llama2/13B_qlora_single_device.yaml",
             ),
