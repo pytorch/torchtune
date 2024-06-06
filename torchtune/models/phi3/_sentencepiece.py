a6fd5481a6c225a513114cd2781b330c90d756da
Hugo Sousa <hmosousa@gmail.com>
2024-06-06 17:27:56 +0100

    Fix extra start token in phi3 tokenizer. Address #1063

--- torchtune/models/phi3/_sentencepiece.py
+++ torchtune/models/phi3/_sentencepiece.py
@@ -171,6 +171,10 @@ class Phi3MiniSentencePieceTokenizer:
         new_line_token_id = self.encode("\n", add_bos=False, add_eos=False)
 
         for message in messages:
+            # Skip system prompt
+            if ignore_system_prompts and message.role == "system":
+                continue
+
             # Prepend BOS on start of new turns
             if start_of_turn:
                 tokenized_messages.append(self.bos_id)
@@ -186,11 +190,8 @@ class Phi3MiniSentencePieceTokenizer:
                 end_of_turn = True
                 mask.append(message.masked)
             elif message.role == "system":
-                if ignore_system_prompts:
-                    continue
-                else:
-                    tokenized_messages.append(self.special_tokens["<|system|>"])
-                    mask.append(message.masked)
+                tokenized_messages.append(self.special_tokens["<|system|>"])
+                mask.append(message.masked)
             else:
                 raise ValueError(
                     f"Unknown role '{message.role}' for message: '{message.content}'"
