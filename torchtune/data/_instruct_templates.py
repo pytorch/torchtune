e00e1ee2cd81351f905a9e2cb763b6baabfaf160
joecummings <jrcummings27@gmail.com>
2024-06-14 12:21:07 -0700

    Try to make this work

--- torchtune/data/_instruct_templates.py
+++ torchtune/data/_instruct_templates.py
@@ -100,28 +100,18 @@ class AlpacaInstructTemplate(InstructTemplate):
             >>> # Simple instruction
             >>> AlpacaInstructTemplate.format(sample={"instruction": "Write a poem"})
             Below is an instruction that describes a task, paired with an input that provides further context.
-            Write a response that appropriately completes the request.\n
-            ### Instruction:
-            Write a poem\n
-            ### Response:\n
+            Write a response that appropriately completes the request.\n\n### Instruction:\nWrite a poem\n\n### Response:\n
 
             >>> # Instruction with input
             >>> AlpacaInstructTemplate.format(sample={"instruction": "Write a poem", "input": "The poem should be 5 lines long"})
             Below is an instruction that describes a task, paired with an input that provides further context.
-            Write a response that appropriately completes the request.\n
-            ### Instruction:
-            Write a poem\n
-            ### Input:
-            The poem should be 5 lines long\n
-            ### Response:\n
+            Write a response that appropriately completes the request.\n\n### Instruction:\nWrite a poem\n\n### Input:
+            The poem should be 5 lines long\n\n### Response:\n
 
             >>> # Instruction with column map where the 'instruction' key is actually named 'prompt' in the given sample
             >>> AlpacaInstructTemplate.format(sample={"prompt": "Write me a poem"}, column_map={"instruction": "prompt"})
             Below is an instruction that describes a task, paired with an input that provides further context.
-            Write a response that appropriately completes the request.\n
-            ### Instruction:
-            Write a poem\n
-            ### Response:\n
+            Write a response that appropriately completes the request.\n\n### Instruction:\nWrite a poem\n\n### Response:\n
 
 
         Returns:
@@ -280,16 +270,14 @@ class StackExchangedPairedTemplate(InstructTemplate):
         Examples:
             >>> # Simple question
             >>> StackExchangedPairedTemplate.format(sample={"question": "What is the capital of France?"})
-            Question: What is the capital of France?\n
-            Answer:
+            Question: What is the capital of France?\n\nAnswer:
 
             >>> # Question with column map where the 'question' key is actually named 'prompt' in the given sample
             >>> StackExchangedPairedTemplate.format(
             ...     sample={"prompt": "What is the capital of France?"},
             ...     column_map={"question": "prompt"}
             ... )
-            Question: What is the capital of France?\n
-            Answer:
+            Question: What is the capital of France?\n\nAnswer:
 
         Returns:
             The formatted prompt
