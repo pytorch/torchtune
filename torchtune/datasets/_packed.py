d6da196268f4850fba7940629e3232a4c91ea064
joecummings <jrcummings27@gmail.com>
2024-06-21 12:52:56 -0700

    Add 'on-the-fly' packing

--- torchtune/datasets/_packed.py
+++ torchtune/datasets/_packed.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Union
 
 import torch
 from torch.nn import functional as F
@@ -13,6 +13,8 @@ from torch.utils.data import Dataset
 from torchtune.data import CROSS_ENTROPY_IGNORE_IDX
 from torchtune.utils import get_world_size_and_rank
 from tqdm import tqdm
+
+PACK_TYPE: Dict[str, Union[torch.Tensor, List[int]]]
 
 
 class PackedDataset(Dataset):
@@ -71,7 +73,7 @@ class PackedDataset(Dataset):
         ds (Dataset): dataset to sample pack. This should return a dictionary with field
             "tokens" and "labels" containing the tokenized and label samples.
         max_seq_len (int): Maximum number of tokens to pack
-        max_packs (Optional[int]): maximum number of packs. Default is None, which will create as many
+        max_packs (int): maximum number of packs. Default is -1, which will create as many
             packs as possible.
         split_across_pack (bool): if the last sample in a pack does not fit in ``max_seq_len``,
             split the sample into the next pack, or move it entirely to the beginning of the next pack.
@@ -84,33 +86,29 @@ class PackedDataset(Dataset):
         self,
         ds: Dataset,
         max_seq_len: int,
-        max_packs: Optional[int] = None,
+        max_packs: int = -1,
         split_across_pack: bool = False,
     ) -> None:
         self.ds = ds
         self.max_seq_len = max_seq_len
         self.max_packs = max_packs
         self.split_across_pack = split_across_pack
-        # where final samples will be held
-        self.packs: List[Dict[str, List[int]]] = []
+        # Where final samples will be held
+        self.packs: List[PACK_TYPE] = []
+        self.previous_sample_boundary: int = 0
         self._pack()
 
     def _pack(self) -> None:
-        """
-        Iterate through the dataset. Use a buffer to hold samples until max_seq_len,
+        """Iterate through the dataset. Use a buffer to hold samples until max_seq_len,
         then append the buffer to self.packs as a single "packed" sample. Continue
-        until max_packs or end of dataset.
-        """
-        # buffer to hold samples until they are long enough to be added to self.packs
+        until max_packs or end of dataset."""
+        # Buffer to hold samples until they are long enough to be added to self.packs
         current_pack = {
             "tokens": [],
             "labels": [],
-            "mask": [],
             "input_pos": [],
+            "seq_lens": [],
         }
-        # Keep track of what index the previous sample ends in case we need
-        # to end a pack early
-        previous_sample_boundary = 0
 
         # Only show progress bar on rank 0
         _, rank = get_world_size_and_rank()
@@ -119,156 +117,178 @@ class PackedDataset(Dataset):
 
         for sample in self.ds:
             tokens, labels = sample["tokens"], sample["labels"]
-            # If the dataset outputs samples that are larger than the specified
-            # max_seq_len and we're unable to split it, user needs to modify
-            # one of the two parameters
-            seq_len = len(tokens)
-            if seq_len > self.max_seq_len and not self.split_across_pack:
-                raise ValueError(
-                    f"Dataset sample is too long ({len(tokens)} > {self.max_seq_len}). "
-                    "Please set `split_across_pack=True` or increase `max_seq_len`."
-                )
 
-            # Create integer mask and position ids for current sample and extend
-            # current pack
-            current_sample = {
-                "tokens": tokens,
-                "labels": labels,
-                # Mask is simply a causal mask within this sample length
-                "mask": [torch.tril(torch.ones(seq_len, seq_len, dtype=torch.bool))],
-                "input_pos": list(range(seq_len)),
-            }
-            current_pack = {k: v + current_sample[k] for k, v in current_pack.items()}
+            # Update the current pack
+            current_pack["tokens"] += tokens
+            current_pack["labels"] += labels
+            current_pack["input_pos"] += list(range(len(tokens)))
+            current_pack["seq_lens"] += [len(tokens)]
 
             # If the current pack is long enough, add it to self.packs and retain
-            # any truncated samples for next pack, if splitting samples
-            if len(current_pack["tokens"]) > self.max_seq_len:
-                current_pack = self._add_pack(
-                    current_pack=current_pack,
-                    boundary=self.max_seq_len
-                    if self.split_across_pack
-                    else previous_sample_boundary,
-                )
+            # any truncated or bumped samples for next pack
+            if len(current_pack["tokens"]) >= self.max_seq_len:
+                current_pack = self._add_pack(current_pack)
 
             if rank == 0:
                 pbar.update()
-            previous_sample_boundary = len(current_pack["tokens"])
-            if self.max_packs is not None and len(self.packs) >= self.max_packs:
+
+            # Keep track of previous sample boundary
+            self.previous_sample_boundary = len(current_pack["tokens"])
+
+            # If max packs is set, stop packing when we reach that number
+            if len(self.packs) == self.max_packs:
                 break
 
-        # Add the last pack with remaining samples that did not fit in previous
-        if len(current_pack["tokens"]) > 0 and (
-            self.max_packs is None or len(self.packs) < self.max_packs
-        ):
-            current_pack = self._add_pack(
-                current_pack=current_pack, boundary=len(current_pack["tokens"])
-            )
-            assert len(current_pack["tokens"]) == 0
+        # Handle the last pack if there's leftover and we haven't filled up the max packs
+        if len(current_pack["tokens"]) > 0 and len(self.packs) < self.max_packs:
+            current_pack = self._add_pack(current_pack)
 
-    def _add_pack(
-        self, current_pack: Dict[str, List[int]], boundary: int
-    ) -> Dict[str, List[int]]:
+    def _convert_to_tensors(self, pack: PACK_TYPE) -> PACK_TYPE:
+        """Converts a pack into tensors. Pack comes in as a dict of lists and is converted to tensors.
+        The only key that does not get converted is ``seq_lens``.
         """
-        Pad and add the current pack to self.packs and return what's remaining.
-        """
-        # So far we've kept a list of causal masks, one for each sample in the pack.
-        # Now we need to combine them into a single mask for the entire pack.
-        packing_mask = torch.block_diag(*current_pack["mask"])
-        pack = {
-            "tokens": current_pack["tokens"][:boundary],
-            "labels": current_pack["labels"][:boundary],
-            "mask": packing_mask[:boundary, :boundary],
-            "input_pos": current_pack["input_pos"][:boundary],
+        return {
+            "tokens": torch.tensor(pack["tokens"]),
+            "labels": torch.tensor(pack["labels"]),
+            "input_pos": torch.tensor(pack["input_pos"]),
+            "seq_lens": pack["seq_lens"],
         }
 
-        # Resultant shapes after padding
-        # tokens: [max_seq_len, ]
-        # labels: [max_seq_len, ]
-        # mask: [max_seq_len, max_seq_len]
-        # input_pos: [max_seq_len, ]
-        padded_pack = self._pad_pack(pack)
-        self.packs.append(padded_pack)
+    def _add_pack(self, current_pack: PACK_TYPE) -> None:
+        """Processes the current pack and adds it to ``self.packs``."""
 
-        # Keep sample that did not fit or got truncated for the next pack
-        updated_pack = {
-            "tokens": current_pack["tokens"][boundary:],
-            "labels": current_pack["labels"][boundary:],
-            "mask": [packing_mask[boundary:, boundary:]],
-            "input_pos": current_pack["input_pos"][boundary:],
-        }
-
-        return updated_pack
-
-    def _pad_pack(
-        self,
-        sample: Dict[str, Any],
-        padding_idx: int = 0,
-        ignore_idx: int = CROSS_ENTROPY_IGNORE_IDX,
-    ) -> Dict[str, torch.Tensor]:
-        """Pad a group of packed sequences to max sequence length in the group, and
-        convert integer lists to tensors. Account for attention mask and position
-        ids.
-
-        This is a sample-wise collator and should not be used with the dataloader.
-
-        Sample should look like::
-            {
-                "tokens": List[int],
-                "labels": List[int],
-                "mask": Tensor,
-                "input_pos": List[int],
+        # Handle the case where we want to split across packs
+        if self.split_across_pack:
+            pack = {
+                "tokens": current_pack["tokens"][: self.max_seq_len],
+                "labels": current_pack["labels"][: self.max_seq_len],
+                "input_pos": current_pack["input_pos"][: self.max_seq_len],
+                # The last elem in ``seq_lens`` ensures that ``sum(seq_lens) == self.max_seq_len``
+                "seq_lens": current_pack["seq_lens"][:-1]
+                + [self.max_seq_len - sum(current_pack["seq_lens"][:-1])],
             }
 
-        Args:
-            sample (Dict[str, Any]): A dictionary containing tokens, labels, mask,
-                and position ids
-            padding_idx (int): Padding index for input ids. Defaults to 0.
-            ignore_idx (int): Padding index for labels. Defaults to -100.
+            # Convert to tensors and add to the pack
+            pack = self._convert_to_tensors(pack)
+            self.packs.append(pack)
 
-        Returns:
-            Collated tokens, labels, mask, and position ids.
-        """
-        tokens = sample["tokens"]
-        labels = sample["labels"]
-        mask = sample["mask"]
-        input_pos = sample["input_pos"]
+            return {
+                "tokens": current_pack["tokens"][self.max_seq_len :],
+                "labels": current_pack["labels"][self.max_seq_len :],
+                "input_pos": current_pack["input_pos"][self.max_seq_len :],
+                "seq_lens": [len(current_pack["tokens"][self.max_seq_len :])],
+            }
+        # Handle the case where we DO NOT want to split across packs,
+        else:
+            # First, we check to see if the sample fits perfectly,
+            # in which case our job is easy
+            if len(current_pack["tokens"]) == self.max_seq_len:
+                pack = self._convert_to_tensors(current_pack)
+                self.packs.append(pack)
+                return {"tokens": [], "labels": [], "input_pos": [], "seq_lens": []}
 
-        # Pad to max sequence length
-        tokens = F.pad(
-            torch.tensor(tokens), (0, self.max_seq_len - len(tokens)), value=padding_idx
+            # If it doesn't fit perfectly, we have to bump the last sample into the next one
+            else:
+                pack = {
+                    "tokens": current_pack["tokens"][: self.previous_sample_boundary],
+                    "labels": current_pack["labels"][: self.previous_sample_boundary],
+                    "input_pos": current_pack["input_pos"][
+                        : self.previous_sample_boundary
+                    ],
+                    # We can guarantee that the last sample is the one that pushed the current pack over the limit
+                    # therefore we just drop the last element from seq_lens and add it to the next pack
+                    "seq_lens": current_pack["seq_lens"][:-1],
+                }
+
+                # Convert to tensors, pad, and add to the pack
+                # We have to pad b/c now the pack is lt ``self.max_seq_len``
+                pack = self._convert_to_tensors(pack)
+                pack = self._pad_pack(pack)
+                self.packs.append(pack)
+
+                return {
+                    "tokens": current_pack["tokens"][self.previous_sample_boundary :],
+                    "labels": current_pack["labels"][self.previous_sample_boundary :],
+                    "input_pos": current_pack["input_pos"][
+                        self.previous_sample_boundary :
+                    ],
+                    "seq_lens": [current_pack["seq_lens"][-1]],
+                }
+
+    def _pad_pack(self, pack, padding_idx=0, ignore_idx=CROSS_ENTROPY_IGNORE_IDX):
+        """Pads a pack to ``self.max_seq_len``."""
+        # Pad tokens
+        padded_tokens = F.pad(
+            pack["tokens"],
+            (0, self.max_seq_len - len(pack["tokens"])),
+            value=padding_idx,
         )
-        labels = F.pad(
-            torch.tensor(labels), (0, self.max_seq_len - len(labels)), value=ignore_idx
+
+        # Pad labels
+        padded_labels = F.pad(
+            pack["labels"],
+            (0, self.max_seq_len - len(pack["labels"])),
+            value=ignore_idx,
         )
-        # For attention mask, simply use identity matrix for the pad tokens
-        mask_pad = torch.eye(self.max_seq_len - mask.shape[0], dtype=torch.bool)
-        mask = torch.block_diag(mask, mask_pad)
-        # For position ids, continue to increment for pad tokens
-        next_pos = input_pos[-1] + 1
-        input_pos_pad = torch.arange(
-            next_pos, next_pos + self.max_seq_len - len(input_pos)
-        )
-        # Do not go beyond max_seq_len - 1
-        input_pos_pad = input_pos_pad.clamp(max=self.max_seq_len - 1)
-        input_pos = torch.cat(
+
+        # Pad input_pos continuing the sequence from last value
+        # in input_pos
+        # e.g. [0 1 2] -> [0 1 2 3 4 5] for self.max_seq_len = 6
+        padded_input_pos = torch.cat(
             [
-                torch.tensor(input_pos),
-                input_pos_pad,
+                pack["input_pos"],
+                torch.arange(
+                    pack["input_pos"][-1] + 1,
+                    pack["input_pos"][-1]
+                    + self.max_seq_len
+                    - len(pack["input_pos"])
+                    + 1,
+                ),
             ]
         )
 
-        assert tokens.shape == labels.shape == input_pos.shape
-        assert tokens.shape[0] == mask.shape[0]
-
         return {
-            "tokens": tokens,
-            "labels": labels,
-            "mask": mask,
-            "input_pos": input_pos,
+            "tokens": padded_tokens,
+            "labels": padded_labels,
+            "input_pos": padded_input_pos,
+            "seq_lens": pack["seq_lens"],  # seq_len is untouched
         }
 
     def __len__(self):
         return len(self.packs)
 
-    def __getitem__(self, index: int) -> Dict[str, List[int]]:
-        return self.packs[index]
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
+        """Constructs the attention mask on-the-fly and returns whole sample."""
+        current_pack = self.packs[idx]
+
+        num_samples_in_pack = len(current_pack["seq_lens"])
+        total_seq_len = 0
+
+        block_attn_masks = []
+
+        for i, seq_len in enumerate(current_pack["seq_lens"]):
+            total_seq_len += seq_len
+
+            # Append lower triangular matrix for causal mask
+            block_attn_masks.append(
+                torch.tril(torch.ones(seq_len, seq_len, dtype=torch.bool))
+            )
+
+            # If we're at the last sample and the total seq len is less than the max seq len,
+            # we need to pad with identity matrix for the remainder
+            if i == num_samples_in_pack - 1 and total_seq_len < self.max_seq_len:
+                block_attn_masks.append(
+                    torch.eye(
+                        self.max_seq_len - total_seq_len,
+                        self.max_seq_len - total_seq_len,
+                        dtype=torch.bool,
+                    )
+                )
+
+        return {
+            "tokens": current_pack["tokens"],
+            "labels": current_pack["labels"],
+            "input_pos": current_pack["input_pos"],
+            # Assemble the mask into a block causal matrix
+            "mask": torch.block_diag(*block_attn_masks),
+        }
