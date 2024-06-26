c815069a260e0a91c320173ef10d785cf576e006
RdoubleA <rafiayub@fb.com>
2024-06-26 14:09:02 -0700

    update tests

--- torchtune/models/llama3/_tokenizer.py
+++ torchtune/models/llama3/_tokenizer.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Dict, List, Optional, Any
+from typing import Any, Dict, List, Optional
 
 from PIL.Image import Image
 
