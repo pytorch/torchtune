21b3ea806903d67e6027beada375844778a886c7
RdoubleA <rafiayub@fb.com>
2024-06-26 17:43:33 -0700

    use images as attachments instead

--- torchtune/data/_types.py
+++ torchtune/data/_types.py
@@ -5,15 +5,17 @@
 # LICENSE file in the root directory of this source tree.
 
 from dataclasses import dataclass
-from typing import Literal, Union
-
-from PIL.Image import Image
+from typing import Literal
 
 Role = Literal[
     "system",  # Origin is system prompt
     "user",  # Origin is user
     "assistant",  # Origin is the model output
     "ipython",  # Origin is return from a tool call
+]
+
+Media = Literal[
+    "image",
 ]
 
 
@@ -27,19 +29,21 @@ class Message:
 
     Attributes:
         role (Role): role of the message writer. Can be "system", "user", "assistant", or "ipython".
-        content (Union[str, Image]): content of the message. Can be a string or a PIL Image.
+        content (str): content of the message.
         masked (bool): whether the message is masked in the sample. Default: False
-        ipython (bool): whether the message is an ipython call. Default: False
+        ipython (bool): whether the message is a tool call. Default: False
         eot (bool): whether the message corresponds to the end of a turn. Should be true
             except in the case of multiple consecutive assistant messages (i.e., tool calls
             by assistant). Default: True
+        media (Optional[List[Media]]): list of media attachments by type in the message. Default: None
     """
 
     role: Role
-    content: Union[str, Image]
+    content: str
     masked: bool = False
     ipython: bool = False
     eot: bool = True
+    media: Optional[List[Media]] = None
 
     @classmethod
     def from_dict(cls, d: dict) -> "Message":
