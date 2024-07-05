c96279e2ac528bb6a382b057572a3710a01500fe
Felipe Mello <felipemello@fb.com>
2024-07-05 13:17:51 -0700

    lint

--- torchtune/models/clip/__init__.py
+++ torchtune/models/clip/__init__.py
@@ -5,17 +5,18 @@
 # LICENSE file in the root directory of this source tree.
 
 from ._component_builders import clip_vision_encoder
+
+from ._model_builders import clip_vit_224_transform  # noqa
 from ._position_embeddings import (
     TiledTokenPositionalEmbedding,
     TilePositionalEmbedding,
     TokenPositionalEmbedding,
 )
-from ._model_builders import clip_vit_224_transform  # noqa
 
 __all__ = [
     "clip_vision_encoder",
     "TokenPositionalEmbedding",
     "TiledTokenPositionalEmbedding",
     "TilePositionalEmbedding",
-    "clip_vit_224_transform"
+    "clip_vit_224_transform",
 ]
