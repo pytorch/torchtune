7c25d0926bbda0e080fa3b4d02907c64e7bea9aa
Felipe Mello <felipemello@fb.com>
2024-07-08 15:39:07 -0700

    import error

--- torchtune/modules/__init__.py
+++ torchtune/modules/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .attention import CausalSelfAttentio, GroupedQueryAttention  # noqa
+from .attention import CausalSelfAttention, GroupedQueryAttention  # noqa
 from .common_utils import reparametrize_as_dtype_state_dict_post_hook
 from .feed_forward import FeedForward  # noqa
 from .kv_cache import KVCache  # noqa
