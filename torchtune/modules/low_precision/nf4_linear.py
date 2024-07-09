5fef21d1234f0c35cffa718920b49ef457081363
Wing Lian <winglian@meta.com>
2024-07-09 08:22:49 -0700

    use swap_tensors per PR feedback

--- torchtune/modules/low_precision/nf4_linear.py
+++ torchtune/modules/low_precision/nf4_linear.py
@@ -43,7 +43,7 @@ class FrozenNF4Linear(nn.Linear):
         self.nf4_weight = to_nf4(self.weight)
         # re-register self.weight as the nf4 weight, so that the nf4 weight
         # shows up as expected in .parameters, state_dict, etc.
-        self.weight = torch.nn.Parameter(self.nf4_weight, requires_grad=False)
+        torch.utils.swap_tensors(self.weight, torch.nn.Parameter(self.nf4_weight, requires_grad=False))
 
     def forward(self, input: Tensor) -> Tensor:
         """
