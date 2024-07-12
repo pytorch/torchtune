6f630c4babb25dc45d93d8c9e955b4353d5d68a7
Rafi Ayub <33648637+RdoubleA@users.noreply.github.com>
2024-07-12 12:54:46 -0700

    Update utils docs (#1170)

--- torchtune/models/gemma/__init__.py
+++ torchtune/models/gemma/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ._component_builders import gemma  # noqa
+from ._component_builders import gemma, lora_gemma  # noqa
 from ._model_builders import (  # noqa
     gemma_2b,
     gemma_7b,
@@ -22,6 +22,7 @@ __all__ = [
     "gemma_2b",
     "gemma_7b",
     "gemma_tokenizer",
+    "lora_gemma",
     "lora_gemma_2b",
     "lora_gemma_7b",
     "qlora_gemma_2b",
