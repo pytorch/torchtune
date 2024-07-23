b5eb93f2a4c96a3be4a2aece290c4b47c3a5fbcb
joecummings <jrcummings27@gmail.com>
2024-07-23 09:27:36 -0700

    Update docstrings

--- torchtune/models/llama3_1/_model_builders.py
+++ torchtune/models/llama3_1/_model_builders.py
@@ -16,20 +16,17 @@ from torchtune.modules.tokenizers import parse_hf_tokenizer_json
 
 """
 Model builders build specific instantiations using component builders. For example
-the llama3_8b model builder uses the llama3 component builder to create the
-Llama3 8B model.
+the llama3_1_8b model builder uses the llama3 component builder to create the
+Llama3.1 8B model.
 """
 
 
 def llama3_1_8b() -> TransformerDecoder:
     """
-    Builder for creating a Llama3 model initialized w/ the default 8b parameter values.
-
-    Args:
-        max_seq_len (int): Maximum sequence length. Default is 8192.
+    Builder for creating a Llama3.1 model initialized w/ the default 8b parameter values.
 
     Returns:
-        TransformerDecoder: Instantiation of Llama3 8B model
+        TransformerDecoder: Instantiation of Llama3.1 8B model
     """
     return llama3_1(
         vocab_size=128_256,
@@ -47,13 +44,10 @@ def llama3_1_8b() -> TransformerDecoder:
 
 def llama3_1_70b() -> TransformerDecoder:
     """
-    Builder for creating a Llama3 model initialized w/ the default 70B parameter values.
-
-    Args: 
-        max_seq_len (int): Maximum sequence length. Default is 8192.
+    Builder for creating a Llama3.1 model initialized w/ the default 70B parameter values.
 
     Returns:
-        TransformerDecoder: Instantiation of Llama3 70 model
+        TransformerDecoder: Instantiation of Llama3.1 70B model
     """
     return llama3_1(
         vocab_size=128_256,
@@ -71,7 +65,7 @@ def llama3_1_70b() -> TransformerDecoder:
 
 def llama3_tokenizer(path: str, special_tokens_path: Optional[str] = None) -> Llama3Tokenizer:
     """
-    Tokenizer for Llama3.
+    Tokenizer for Llama3 and Llama3.1.
 
     Args:
         path (str): path to the tokenizer
@@ -80,7 +74,7 @@ def llama3_tokenizer(path: str, special_tokens_path: Optional[str] = None) -> Ll
             structured similarly. Default is None to use the canonical Llama3 special tokens.
     
     Returns:
-        Llama3Tokenizer: Instantiation of the Llama3 tokenizer
+        Llama3Tokenizer: Instantiation of the Llama3/Llama3.1 tokenizer
     """
     special_tokens = parse_hf_tokenizer_json(special_tokens_path) if special_tokens_path is not None else None
     return Llama3Tokenizer(path=path, special_tokens=special_tokens)
@@ -95,9 +89,9 @@ def lora_llama3_1_8b(
     quantize_base: bool = False,
 ) -> TransformerDecoder:
     """
-    Builder for creating a Llama3 8B model with LoRA enabled.
+    Builder for creating a Llama3.1 8B model with LoRA enabled.
 
-    The Llama3 defaults are the same as in :func:`~torchtune.models.llama3.llama3_8b`,
+    The Llama3.1 defaults are the same as in :func:`~torchtune.models.llama3_1.llama3_1_8b`,
     while LoRA default params are based on
     https://github.com/tloen/alpaca-lora/blob/8bb8579e403dc78e37fe81ffbb253c413007323f/finetune.py#L41-L43.
 
@@ -114,7 +108,7 @@ def lora_llama3_1_8b(
         quantize_base (bool): Whether to quantize base model weights
 
     Returns:
-        TransformerDecoder: Instantiation of Llama3 8B model with LoRA applied
+        TransformerDecoder: Instantiation of Llama3.1 8B model with LoRA applied
     """
     return lora_llama3_1(
         lora_attn_modules=lora_attn_modules,
@@ -146,9 +140,9 @@ def lora_llama3_1_70b(
     quantize_base: bool = False,
 ) -> TransformerDecoder:
     """
-    Builder for creating a Llama3 70B model with LoRA enabled.
+    Builder for creating a Llama3.1 70B model with LoRA enabled.
 
-    The Llama3 defaults are the same as in :func:`~torchtune.models.llama3.llama3_70b`,
+    The Llama3.1 defaults are the same as in :func:`~torchtune.models.llama3_1.llama3_1_70b`,
     while LoRA default params are based on
     https://github.com/tloen/alpaca-lora/blob/8bb8579e403dc78e37fe81ffbb253c413007323f/finetune.py#L41-L43.
 
@@ -165,7 +159,7 @@ def lora_llama3_1_70b(
         quantize_base (bool): Whether to quantize base model weights
 
     Returns:
-        TransformerDecoder: Instantiation of Llama3 70B model with LoRA applied
+        TransformerDecoder: Instantiation of Llama3.1 70B model with LoRA applied
     """
     return lora_llama3_1(
         lora_attn_modules=lora_attn_modules,
@@ -191,15 +185,15 @@ def lora_llama3_1_70b(
 qlora_llama3_1_8b = partial(lora_llama3_1_8b, quantize_base=True)
 
 qlora_llama3_1_8b.__doc__ = """
-Builder for creating a Llama3 8B model with QLoRA enabled. Base model weights in linear layers
+Builder for creating a Llama3.1 8B model with QLoRA enabled. Base model weights in linear layers
 that LoRA is applied to are quantized per the QLoRA paper: https://arxiv.org/abs/2305.14314.
-Please see `lora_llama3_8b` for full API arguments.
+Please see `lora_llama3_1_8b` for full API arguments.
 """
 
 qlora_llama3_1_70b = partial(lora_llama3_1_70b, quantize_base=True)
 
 qlora_llama3_1_70b.__doc__ = """
-Builder for creating a Llama3 70B model with QLoRA enabled. Base model weights in linear layers
+Builder for creating a Llama3.1 70B model with QLoRA enabled. Base model weights in linear layers
 that LoRA is applied to are quantized per the QLoRA paper: https://arxiv.org/abs/2305.14314.
-Please see `lora_llama3_70b` for full API arguments.
+Please see `lora_llama3_1_70b` for full API arguments.
 """
