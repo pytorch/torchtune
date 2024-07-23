935fd0f2bc2960cce63ab814de4dd749c5b83dbc
joecummings <jrcummings27@gmail.com>
2024-07-23 08:26:57 -0700

    Fix linting

--- torchtune/models/llama3_1/_position_embeddings.py
+++ torchtune/models/llama3_1/_position_embeddings.py
@@ -1,5 +1,11 @@
-from typing import Optional
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import math
+from typing import Optional
 
 import torch
 
