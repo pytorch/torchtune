88c5936e7331362a284df01c655120e43115b265
Felipe Mello <felipemello@fb.com>
2024-08-02 11:53:19 -0700

    added max/min

--- tests/torchtune/models/clip/test_clip_image_transform.py
+++ tests/torchtune/models/clip/test_clip_image_transform.py
@@ -12,6 +12,12 @@ import torch
 from tests.test_utils import assert_expected
 
 from torchtune.models.clip._transforms import CLIPImageTransform
+from torchtune.utils.seed import set_seed
+
+
+@pytest.fixture(autouse=True)
+def random():
+    set_seed(0)
 
 
 class TestCLIPImageTransform:
@@ -22,28 +28,36 @@ class TestCLIPImageTransform:
                 "image_size": (100, 400, 3),
                 "expected_shape": torch.Size([2, 3, 224, 224]),
                 "resize_to_max_canvas": False,
-                "expected_tile_means": [0.2231, 0.1754],
+                "expected_tile_means": [0.2230, 0.1763],
+                "expected_tile_max": [1.0, 1.0],
+                "expected_tile_min": [0.0, 0.0],
                 "expected_aspect_ratio": [1, 2],
             },
             {
                 "image_size": (1000, 300, 3),
                 "expected_shape": torch.Size([4, 3, 224, 224]),
                 "resize_to_max_canvas": True,
-                "expected_tile_means": [0.4999, 0.5000, 0.5000, 0.1652],
+                "expected_tile_means": [0.5007, 0.4995, 0.5003, 0.1651],
+                "expected_tile_max": [0.9705, 0.9694, 0.9521, 0.9314],
+                "expected_tile_min": [0.0353, 0.0435, 0.0528, 0.0],
                 "expected_aspect_ratio": [4, 1],
             },
             {
                 "image_size": (200, 200, 3),
                 "expected_shape": torch.Size([4, 3, 224, 224]),
                 "resize_to_max_canvas": True,
-                "expected_tile_means": [0.4991, 0.5000, 0.4997, 0.5004],
+                "expected_tile_means": [0.5012, 0.5020, 0.5011, 0.4991],
+                "expected_tile_max": [0.9922, 0.9926, 0.9970, 0.9908],
+                "expected_tile_min": [0.0056, 0.0069, 0.0059, 0.0033],
                 "expected_aspect_ratio": [2, 2],
             },
             {
                 "image_size": (600, 200, 3),
                 "expected_shape": torch.Size([3, 3, 224, 224]),
                 "resize_to_max_canvas": False,
-                "expected_tile_means": [0.4464, 0.4464, 0.3028],
+                "expected_tile_means": [0.4473, 0.4469, 0.3032],
+                "expected_tile_max": [1.0, 1.0, 1.0],
+                "expected_tile_min": [0.0, 0.0, 0.0],
                 "expected_aspect_ratio": [3, 1],
             },
         ],
@@ -59,10 +73,13 @@ class TestCLIPImageTransform:
             resample="bilinear",
             resize_to_max_canvas=params["resize_to_max_canvas"],
         )
-
         # Generate a deterministic image using np.arange for reproducibility
         image_size = params["image_size"]
-        image = (np.arange(np.prod(image_size)).reshape(image_size)).astype(np.uint8)
+        image = (
+            np.random.randint(0, 256, np.prod(image_size))
+            .reshape(image_size)
+            .astype(np.uint8)
+        )
         image = PIL.Image.fromarray(image)
 
         # Apply the transformation
@@ -70,20 +87,26 @@ class TestCLIPImageTransform:
         output_image = output["image"]
         output_ar = output["aspect_ratio"]
 
-        # output shape matches the expected shape
+        # Check if the output shape matches the expected shape
         assert (
             output_image.shape == params["expected_shape"]
         ), f"Expected shape {params['expected_shape']} but got {output_image.shape}"
 
-        # pixel values are within the expected range [0, 1]
+        # Check if the pixel values are within the expected range [0, 1]
         assert (
             0 <= output_image.min() <= output_image.max() <= 1
         ), f"Expected pixel values to be in range [0, 1] but got {output_image.min()} and {output_image.max()}"
 
-        #  mean values of the tiles match the expected means
+        # Check if the mean, max, and min values of the tiles match the expected values
         for i, tile in enumerate(output_image):
             assert_expected(
                 tile.mean().item(), params["expected_tile_means"][i], rtol=0, atol=1e-4
+            )
+            assert_expected(
+                tile.max().item(), params["expected_tile_max"][i], rtol=0, atol=1e-4
+            )
+            assert_expected(
+                tile.min().item(), params["expected_tile_min"][i], rtol=0, atol=1e-4
             )
 
         #  aspect ratio matches the expected aspect ratio
