db44d3956893a3ccacff373c9c60c2b9e9dddd46
RdoubleA <rafiayub@fb.com>
2024-08-15 13:15:47 -0700

    update test

--- tests/torchtune/models/llama2/test_llama2_tokenizer.py
+++ tests/torchtune/models/llama2/test_llama2_tokenizer.py
@@ -8,20 +8,21 @@ from pathlib import Path
 
 import pytest
 from torchtune.data import Message
-from torchtune.models.llama2 import llama2_tokenizer
+from torchtune.models.llama2 import llama2_tokenizer, Llama2ChatTemplate
+from collections import Counter
 
 ASSETS = Path(__file__).parent.parent.parent.parent / "assets"
 
 
 class TestLlama2Tokenizer:
-    @pytest.fixture
-    def tokenizer(self):
+    def tokenizer(self, template: bool = False):
         # m.model is a pretrained Sentencepiece model using the following command:
         # spm.SentencePieceTrainer.train('--input=<TRAIN_FILE> --model_prefix=m --vocab_size=2000')
-        return llama2_tokenizer(str(ASSETS / "m.model"))
+        return llama2_tokenizer(str(ASSETS / "m.model"), prompt_template=Llama2ChatTemplate() if template else None)
 
-    def test_tokenize_messages(self, tokenizer):
-        messages = [
+    @pytest.fixture
+    def messages(self):
+        return [
             Message(
                 role="user",
                 content="Below is an instruction that describes a task. Write a response "
@@ -39,209 +40,19 @@ class TestLlama2Tokenizer:
                 "good conversation over coffee.",
             ),
         ]
+
+    def test_tokenize_messages(self, messages):
+        tokenizer = self.tokenizer(template=False)
         tokens, mask = tokenizer.tokenize_messages(messages)
-        expected_tokens = [
-            1,
-            323,
-            418,
-            202,
-            31,
-            128,
-            15,
-            120,
-            47,
-            88,
-            584,
-            23,
-            1665,
-            182,
-            9,
-            434,
-            295,
-            85,
-            4,
-            780,
-            47,
-            636,
-            9,
-            1094,
-            213,
-            23,
-            9,
-            69,
-            69,
-            164,
-            1153,
-            299,
-            35,
-            961,
-            132,
-            237,
-            7,
-            5,
-            761,
-            4,
-            12,
-            0,
-            313,
-            120,
-            47,
-            88,
-            584,
-            166,
-            493,
-            171,
-            54,
-            299,
-            9,
-            906,
-            244,
-            19,
-            186,
-            767,
-            303,
-            671,
-            92,
-            209,
-            24,
-            190,
-            52,
-            38,
-            4,
-            12,
-            0,
-            1243,
-            7,
-            69,
-            135,
-            213,
-            166,
-            6,
-            21,
-            45,
-            128,
-            71,
-            58,
-            38,
-            14,
-            10,
-            652,
-            35,
-            462,
-            101,
-            1306,
-            7,
-            341,
-            171,
-            20,
-            14,
-            127,
-            26,
-            652,
-            7,
-            10,
-            1268,
-            4,
-            6,
-            21,
-            45,
-            591,
-            9,
-            566,
-            22,
-            994,
-            913,
-            38,
-            20,
-            52,
-            24,
-            10,
-            1306,
-            734,
-            14,
-            71,
-            365,
-            1382,
-            7,
-            10,
-            801,
-            105,
-            88,
-            244,
-            985,
-            7,
-            4,
-            6,
-            21,
-            45,
-            9,
-            566,
-            126,
-            180,
-            11,
-            5,
-            1137,
-            7,
-            10,
-            1089,
-            151,
-            8,
-            1156,
-            213,
-            342,
-            7,
-            10,
-            384,
-            104,
-            54,
-            470,
-            4,
-            6,
-            21,
-            45,
-            287,
-            14,
-            33,
-            125,
-            135,
-            24,
-            101,
-            512,
-            66,
-            7,
-            28,
-            822,
-            15,
-            542,
-            69,
-            59,
-            110,
-            14,
-            365,
-            229,
-            7,
-            3,
-            36,
-            267,
-            36,
-            125,
-            135,
-            24,
-            101,
-            1503,
-            182,
-            9,
-            222,
-            1661,
-            191,
-            332,
-            92,
-            92,
-            24,
-            24,
-            4,
-            2,
-        ]
-        expected_mask = [True] * 75 + [False] * 125
-        assert expected_tokens == tokens
-        assert expected_mask == mask
+        expected_tokens = {}
+        expected_mask = {}
+        assert expected_tokens == Counter(tokens)
+        assert expected_mask == Counter(mask)
+
+    def test_tokenize_messages_chat_template(self, messages):
+        tokenizer = self.tokenizer(template=True)
+        tokens, mask = tokenizer.tokenize_messages(messages)
+        expected_tokens = {}
+        expected_mask = {}
+        assert expected_tokens == Counter(tokens)
+        assert expected_mask == Counter(mask)
