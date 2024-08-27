e7b3e417189ed40f9a4a68101f4d11a448f7aa8e
Felipe Mello <felipemello@fb.com>
2024-08-27 10:03:59 -0700

    CE docstring + fix label/logit handling

--- recipes/full_finetune_single_device.py
+++ recipes/full_finetune_single_device.py
@@ -253,6 +253,11 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
         # if cfg is missing profiler key or if `cfg.profiler.enabled = False`
         self._profiler = self._setup_profiler(cfg.get(PROFILER_KEY, None))
 
+        # Used to ignore labels for loss computation
+        self.ignore_labels_cache = torch.full(
+            (cfg.batch_size, 1), self._loss_fn.ignore_index, device=self._device
+        )
+
     def _setup_profiler(
         self, cfg_profiler: Optional[DictConfig] = None
     ) -> Union[torch.profiler.profile, DummyProfiler]:
@@ -480,10 +485,16 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
         input_pos = batch.get("input_pos", None)  # shape [b, s]
 
         logits = self._model(tokens, mask=mask, input_pos=input_pos)
-        # Shift so that tokens < n predict n
-        logits = logits[..., :-1, :].contiguous()
-        labels = labels[..., 1:].contiguous()
-        logits = logits.transpose(1, 2)
+        # Shift labels to compute loss
+        # equivalent to doing labels[..., 1:] and logits[..., :-1, :]
+        # But this way we dont need to slice the logits. We just add an ignore index to labels.
+        labels = torch.hstack(
+            (labels[..., 1:], self.ignore_labels_cache[: labels.shape[0]])
+        )
+        if not isinstance(logits, list):
+            labels = labels.reshape(-1)
+            logits = logits.reshape(-1, logits.size(-1)).float()
+
         # Compute loss
         loss = self._loss_fn(logits, labels)
         # free logits otherwise it peaks backward memory
--- torchtune/modules/loss/chunked_cross_entropy.py
+++ torchtune/modules/loss/chunked_cross_entropy.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
 from typing import List
 
 import torch
@@ -12,7 +11,18 @@ import torch
 
 class ChunkedCrossEntropyLoss(torch.nn.Module):
     """
-    TODO: explain why it saves memory
+    CE with chunked outputs saves memory by only upcasting one chunk at a time.
+
+    Since the model is trained with fp16, before running CE, we have to upcast
+    it to fp32 for better accuracy. When this happens, the memory usage doubles.
+    Models like llama3 have large vocabulary size. So doing it one chunk at a time
+    saves considerable memory. Chunking happens at the token level.
+
+    The CE and upcasting have to be compiled together for better performance.
+    Also, compiling CE always yields to great gains. Therefore, compiling
+    is the default.
+
+    For more details, please refer to: https://github.com/pytorch/torchtune/pull/1390
     """
 
     def __init__(self, num_output_chunks: int = 16, ignore_index: int = -100):
@@ -39,17 +49,32 @@ class ChunkedCrossEntropyLoss(torch.nn.Module):
 
         Returns:
             torch.Tensor: Cross entropy loss of shape (1,).
+
+        Example:
+        >>> loss_fn = ChunkedCrossEntropyLoss()
+        >>>
+        >>> h = torch.tensor([bsz, num_tokens, dim])
+        >>> output_chunks = [model.output(chunk) for chunk in h.chunk(num_chunks, dim=1)]
+        >>>
+        >>> labels = torch.tensor([bsz, num_tokens])
+        >>> loss = loss_fn(output_chunks, labels)
         """
+
         total_elements = (labels != self.ignore_index).sum()
+
+        # chunk and reshape labels (bsz, num_tokens, vocab) -> [(bsz*num_tokens/num_chunks, vocab)]
         labels = [
             target_chunk.reshape(-1)
             for target_chunk in labels.chunk(self.num_output_chunks, dim=1)
         ]
+        # reshape logits [(bsz, num_tokens/num_chunks, vocab)] -> [(bsz*num_tokens/num_chunks, vocab)]
         logits = [
             logit_chunk.reshape(-1, logit_chunk.size(-1)) for logit_chunk in logits
         ]
 
+        # compute one chunk at a time
         total_loss = 0.0
         for logits_chunk, labels_chunk in zip(logits, labels):
             total_loss += self._compute_cross_entropy(logits_chunk, labels_chunk)
+
         return total_loss / total_elements
