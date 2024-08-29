3b6af1f7f0560c831cbe0617b14a625904821ba6
RdoubleA <rafiayub@fb.com>
2024-08-29 16:51:54 -0700

    fix typo

--- torchtune/training/__init__.py
+++ torchtune/training/__init__.py
@@ -4,8 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchtune.training.quantization import get_quantizer_mode
-from trochtune.training.memory import (
+from torchtune.training.memory import (
     cleanup_before_training,
     create_optim_in_bwd_wrapper,
     get_memory_stats,
@@ -14,6 +13,7 @@ from trochtune.training.memory import (
     register_optim_in_bwd_hooks,
     set_activation_checkpointing,
 )
+from torchtune.training.quantization import get_quantizer_mode
 
 __all__ = [
     "get_quantizer_mode",
