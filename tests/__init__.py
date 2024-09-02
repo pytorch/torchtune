a322c852ddc2583a5d4aaba8da43c648c57351e4
Evan Smothers <ebs@fb.com>
2024-09-01 21:25:40 -0700

    check ao import

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,20 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+# Check at the top-level that torchao is installed.
+# This is better than doing it at every import site.
+# We have to do this because it is not currently possible to
+# properly support both nightly and stable installs of PyTorch + torchao
+# in pyproject.toml.
+try:
+    import torchao  # noqa
+except ImportError as e:
+    raise ImportError(
+        """
+    torchao not installed.
+    If you are using stable PyTorch please install via pip install torchao.
+    If you are using nightly PyTorch please install via
+    pip install --pre torchao --index-url https://download.pytorch.org/whl/nightly/cu121."
+    """
+    ) from e
--- torchtune/__init__.py
+++ torchtune/__init__.py
@@ -6,6 +6,24 @@
 
 __version__ = ""
 
+
+# Check at the top-level that torchao is installed.
+# This is better than doing it at every import site.
+# We have to do this because it is not currently possible to
+# properly support both nightly and stable installs of PyTorch + torchao
+# in pyproject.toml.
+try:
+    import torchao  # noqa
+except ImportError as e:
+    raise ImportError(
+        """
+    torchao not installed.
+    If you are using stable PyTorch please install via pip install torchao.
+    If you are using nightly PyTorch please install via
+    pip install --pre torchao --index-url https://download.pytorch.org/whl/nightly/cu121."
+    """
+    ) from e
+
 from torchtune import datasets, models, modules, utils
 
 __all__ = [datasets, models, modules, utils]
