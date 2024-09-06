8d2b46c09b6e1102bd0dfda0e3cca07f66d596a9
Rafi Ayub <33648637+RdoubleA@users.noreply.github.com>
2024-09-06 16:04:55 -0700

    utils refactor clean-up (#1515)

--- tests/torchtune/utils/test_logging.py
+++ tests/torchtune/utils/test_logging.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import pytest
-from torchtune.data._utils import deprecated
+from torchtune.utils.logging import deprecated
 
 
 def test_deprecated():
