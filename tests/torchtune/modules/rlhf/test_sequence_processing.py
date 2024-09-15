4b14c05e035889c3fac97e6ea558cd51e3a325ce
Salman Mohammadi <salman.mohammadi@outlook.com>
2024-09-15 23:49:38 +0100

    moving

--- tests/torchtune/modules/rlhf/test_sequence_processing.py
+++ tests/torchtune/modules/rlhf/test_sequence_processing.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import torch
-from torchtune.modules import rlhf
+from torchtune import rlhf
 
 
 class TestTruncateSequenceAtFirstStopToken:
