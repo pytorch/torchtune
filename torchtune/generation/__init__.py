0dc5ebce3dbc280ff018400e8de1a87b94b4b8b5
Joe Cummings <jrcummings27@gmail.com>
2024-09-16 16:08:26 -0400

    Update generation docs for v0.3.0 release (#1596)

--- torchtune/generation/__init__.py
+++ torchtune/generation/__init__.py
@@ -15,7 +15,7 @@ from ._generation import (
 __all__ = [
     "generate",
     "generate_next_token",
-    "sample",
     "get_causal_mask_from_padding_mask",
     "get_position_ids_from_padding_mask",
+    "sample",
 ]
