98985e43104dbc3c6fa67f525aac9b88c34171c1
mikaylagawarecki <mikaylagawarecki@gmail.com>
2024-09-26 19:45:33 -0400

    Add low_cpu_ram config to qlora recipes configs (excluding 2B/13B/70B configs) (#1580)

--- torchtune/models/llama3_1/_component_builders.py
+++ torchtune/models/llama3_1/_component_builders.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from functools import partial
 from typing import List, Optional
 
 from torch import nn
@@ -20,7 +19,7 @@ from torchtune.modules import (
     TransformerSelfAttentionLayer,
 )
 
-from torchtune.modules.common_utils import reparametrize_as_dtype_state_dict_post_hook
+from torchtune.modules.common_utils import _register_reparametrize_state_dict_hooks
 
 from torchtune.modules.peft import DoRALinear, LORA_ATTN_MODULES, LoRALinear
 
@@ -268,9 +267,7 @@ def lora_llama3_1(
     if quantize_base:
         # For QLoRA, we reparametrize 4-bit tensors to bf16, and offload to CPU on the fly
         # so as to not increase peak memory
-        model._register_state_dict_hook(
-            partial(reparametrize_as_dtype_state_dict_post_hook, offload_to_cpu=True)
-        )
+        _register_reparametrize_state_dict_hooks(model)
 
     return model
 
