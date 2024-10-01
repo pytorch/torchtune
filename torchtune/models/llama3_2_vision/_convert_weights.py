3c91e42a9b5886cb4137ba8c57d0d72a573e7c77
Philip Bontrager <pbontrager@gmail.com>
2024-10-01 16:38:10 -0400

    Add HF Checkpoint Format Support for Llama Vision (#1727)

--- torchtune/models/llama3_2_vision/_convert_weights.py
+++ torchtune/models/llama3_2_vision/_convert_weights.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
-from typing import Dict
+from typing import Dict, List, Optional, Tuple
 
 import torch
 
@@ -79,6 +79,72 @@ _FROM_META = {
     "vision_model.vision_encoder.global_transformer.resblocks.{}.ln_2.bias": "encoder.projection.layers.{}.mlp_norm.bias",
     "vision_model.vision_encoder.global_transformer.resblocks.{}.gate_attn": "encoder.projection.layers.{}.sa_scale.scale",
     "vision_model.vision_encoder.global_transformer.resblocks.{}.gate_ffn": "encoder.projection.layers.{}.mlp_scale.scale",
+}
+
+
+_FROM_HF = {
+    "language_model.model.embed_tokens.weight": "decoder.tok_embeddings.weight",
+    "language_model.model.layers.{}.self_attn.q_proj.weight": "decoder.layers.{}.attn.q_proj.weight",
+    "language_model.model.layers.{}.self_attn.k_proj.weight": "decoder.layers.{}.attn.k_proj.weight",
+    "language_model.model.layers.{}.self_attn.v_proj.weight": "decoder.layers.{}.attn.v_proj.weight",
+    "language_model.model.layers.{}.self_attn.o_proj.weight": "decoder.layers.{}.attn.output_proj.weight",
+    "language_model.model.layers.{}.self_attn.rotary_emb.inv_freq": None,
+    "language_model.model.layers.{}.mlp.gate_proj.weight": "decoder.layers.{}.mlp.w1.weight",
+    "language_model.model.layers.{}.mlp.up_proj.weight": "decoder.layers.{}.mlp.w3.weight",
+    "language_model.model.layers.{}.mlp.down_proj.weight": "decoder.layers.{}.mlp.w2.weight",
+    "language_model.model.layers.{}.input_layernorm.weight": "decoder.layers.{}.sa_norm.scale",
+    "language_model.model.layers.{}.post_attention_layernorm.weight": "decoder.layers.{}.mlp_norm.scale",
+    "language_model.model.norm.weight": "decoder.norm.scale",
+    "language_model.lm_head.weight": "decoder.output.weight",
+    "language_model.model.layers.{}.cross_attn_attn_gate": "decoder.layers.{}.fusion_layer.ca_scale.scale",
+    "language_model.model.layers.{}.cross_attn_mlp_gate": "decoder.layers.{}.fusion_layer.mlp_scale.scale",
+    "language_model.model.layers.{}.cross_attn.q_proj.weight": "decoder.layers.{}.fusion_layer.attn.q_proj.weight",
+    "language_model.model.layers.{}.cross_attn.k_proj.weight": "decoder.layers.{}.fusion_layer.attn.k_proj.weight",
+    "language_model.model.layers.{}.cross_attn.v_proj.weight": "decoder.layers.{}.fusion_layer.attn.v_proj.weight",
+    "language_model.model.layers.{}.cross_attn.o_proj.weight": "decoder.layers.{}.fusion_layer.attn.output_proj.weight",
+    "language_model.model.layers.{}.cross_attn.q_norm.weight": "decoder.layers.{}.fusion_layer.attn.q_norm.scale",
+    "language_model.model.layers.{}.cross_attn.k_norm.weight": "decoder.layers.{}.fusion_layer.attn.k_norm.scale",
+    "vision_model.gated_positional_embedding.embedding": "encoder.clip.token_pos_embedding.local_token_positional_embedding",
+    "vision_model.gated_positional_embedding.tile_embedding.weight": "encoder.clip.token_pos_embedding.global_token_positional_embedding",  # noqa
+    "vision_model.gated_positional_embedding.gate": "encoder.clip.token_pos_embedding.gate",
+    "vision_model.layernorm_pre.weight": "encoder.clip.ln_pre.weight",
+    "vision_model.layernorm_pre.bias": "encoder.clip.ln_pre.bias",
+    "vision_model.layernorm_post.weight": "encoder.clip.ln_post.weight",
+    "vision_model.layernorm_post.bias": "encoder.clip.ln_post.bias",
+    "vision_model.pre_tile_positional_embedding.embedding.weight": "encoder.clip.pre_tile_pos_embed.embedding",
+    "vision_model.pre_tile_positional_embedding.gate": "encoder.clip.pre_tile_pos_embed.gate",
+    "vision_model.post_tile_positional_embedding.embedding.weight": "encoder.clip.post_tile_pos_embed.embedding",
+    "vision_model.post_tile_positional_embedding.gate": "encoder.clip.post_tile_pos_embed.gate",
+    "vision_model.class_embedding": "encoder.clip.cls_token_embedding.weight",
+    "vision_model.patch_embedding.weight": "encoder.clip.conv.weight",
+    "vision_model.transformer.layers.{}.self_attn.q_proj.weight": "encoder.clip.layers.{}.attn.q_proj.weight",
+    "vision_model.transformer.layers.{}.self_attn.k_proj.weight": "encoder.clip.layers.{}.attn.k_proj.weight",
+    "vision_model.transformer.layers.{}.self_attn.v_proj.weight": "encoder.clip.layers.{}.attn.v_proj.weight",
+    "vision_model.transformer.layers.{}.self_attn.o_proj.weight": "encoder.clip.layers.{}.attn.output_proj.weight",
+    "vision_model.transformer.layers.{}.mlp.fc1.weight": "encoder.clip.layers.{}.mlp.w1.weight",
+    "vision_model.transformer.layers.{}.mlp.fc1.bias": "encoder.clip.layers.{}.mlp.w1.bias",
+    "vision_model.transformer.layers.{}.mlp.fc2.weight": "encoder.clip.layers.{}.mlp.w2.weight",
+    "vision_model.transformer.layers.{}.mlp.fc2.bias": "encoder.clip.layers.{}.mlp.w2.bias",
+    "vision_model.transformer.layers.{}.input_layernorm.weight": "encoder.clip.layers.{}.sa_norm.weight",
+    "vision_model.transformer.layers.{}.input_layernorm.bias": "encoder.clip.layers.{}.sa_norm.bias",
+    "vision_model.transformer.layers.{}.post_attention_layernorm.weight": "encoder.clip.layers.{}.mlp_norm.weight",
+    "vision_model.transformer.layers.{}.post_attention_layernorm.bias": "encoder.clip.layers.{}.mlp_norm.bias",
+    "vision_model.global_transformer.layers.{}.self_attn.q_proj.weight": "encoder.projection.layers.{}.attn.q_proj.weight",
+    "vision_model.global_transformer.layers.{}.self_attn.k_proj.weight": "encoder.projection.layers.{}.attn.k_proj.weight",
+    "vision_model.global_transformer.layers.{}.self_attn.v_proj.weight": "encoder.projection.layers.{}.attn.v_proj.weight",
+    "vision_model.global_transformer.layers.{}.self_attn.o_proj.weight": "encoder.projection.layers.{}.attn.output_proj.weight",
+    "vision_model.global_transformer.layers.{}.mlp.fc1.weight": "encoder.projection.layers.{}.mlp.w1.weight",
+    "vision_model.global_transformer.layers.{}.mlp.fc1.bias": "encoder.projection.layers.{}.mlp.w1.bias",
+    "vision_model.global_transformer.layers.{}.mlp.fc2.weight": "encoder.projection.layers.{}.mlp.w2.weight",
+    "vision_model.global_transformer.layers.{}.mlp.fc2.bias": "encoder.projection.layers.{}.mlp.w2.bias",
+    "vision_model.global_transformer.layers.{}.input_layernorm.weight": "encoder.projection.layers.{}.sa_norm.weight",
+    "vision_model.global_transformer.layers.{}.input_layernorm.bias": "encoder.projection.layers.{}.sa_norm.bias",
+    "vision_model.global_transformer.layers.{}.post_attention_layernorm.weight": "encoder.projection.layers.{}.mlp_norm.weight",
+    "vision_model.global_transformer.layers.{}.post_attention_layernorm.bias": "encoder.projection.layers.{}.mlp_norm.bias",
+    "vision_model.global_transformer.layers.{}.gate_attn": "encoder.projection.layers.{}.sa_scale.scale",
+    "vision_model.global_transformer.layers.{}.gate_ffn": "encoder.projection.layers.{}.mlp_scale.scale",
+    "multi_modal_projector.weight": "encoder.projection.output.weight",
+    "multi_modal_projector.bias": "encoder.projection.output.bias",
 }
 
 
@@ -169,5 +235,195 @@ def llama3_vision_tune_to_meta(
         elif "conv" in key:
             dim = value.shape[0]
             value = value.reshape(dim, -1)
+        converted_state_dict[new_key] = value
+    return converted_state_dict
+
+
+def llama3_vision_hf_to_tune(
+    state_dict: Dict[str, torch.Tensor],
+    num_heads: int = 32,
+    num_kv_heads: int = 32,
+    dim: int = 4096,
+    head_dim: int = None,
+    vocab_size: int = 128256,
+    cross_attention_layers: Optional[List[int]] = None,
+    # Vision Encoder Paramters
+    encoder_dim: int = 1280,
+    tile_size: int = 448,
+    num_tiles: int = 4,
+    supported_aspect_ratios: List[Tuple[int, int]] = None,
+) -> Dict[str, torch.Tensor]:
+    """
+    Convertor from HF state dict to torchtune state dict. This handles:
+    - Updating the cross attention layer numbers
+    - skip loading the rope embeddings
+    - reshaping q, k projections
+    - reversing the precomputed vision positional embeddings
+    """
+    converted_state_dict = {}
+    if head_dim is None:
+        head_dim = dim // num_heads
+    if cross_attention_layers is None:
+        cross_attention_layers = []
+
+    def _permute(t, n_heads):
+        return (
+            t.view(n_heads, 2, head_dim // 2, dim)
+            .transpose(1, 2)
+            .reshape((head_dim * n_heads), dim)
+        )
+
+    for key, value in state_dict.items():
+        if "rotary_emb.inv_freq" in key:  # Skip loading the position embeddings
+            continue
+        new_key = get_mapped_key(key, _FROM_HF)
+        if "language_model" in key:
+            if "layers" in key:  # Update layer numbers
+                layer = int(key.split(".")[3])
+                num_shifts = sum(layer > l for l in cross_attention_layers)
+                new_layer = layer - num_shifts
+                key_lst = new_key.split(".")
+                if layer in cross_attention_layers and "fusion_layer" not in new_key:
+                    # some keys are the same for sa and ca, so we need to edit them here
+                    key_lst[2] = f"{new_layer}.fusion_layer"
+                    if "sa_norm" in new_key:
+                        key_lst[3] = "ca_norm"
+                else:
+                    key_lst[2] = str(new_layer)
+                new_key = ".".join(key_lst)
+            if "q_proj" in key and "cross_attn" not in key:
+                value = _permute(value, num_heads)
+            elif "k_proj" in key and "cross_attn" not in key:
+                value = _permute(value, num_kv_heads)
+            elif new_key == "decoder.tok_embeddings.weight":
+                # Split embedding between learnable embeddings and original text embedding
+                learned_embedding = "decoder.tok_embeddings.fusion_embedding.weight"
+                converted_state_dict[learned_embedding] = value[vocab_size:]
+                value = value[:vocab_size]
+        elif "vision_model" in key:
+            if (
+                "tile_pos_embed.embedding" in new_key
+                or "global_token_positional_embedding" in new_key
+            ):
+                # WARNING: META format postional embeddings contain embeddings that
+                # the model can never use (4 tiles -> 4 x 4 embeddings -> a 4 x 4 image would be 16 tiles).
+                # HF removes these extra embeddings, for us to convert to the META format we set those
+                # unused embeddings as 0 instead of the original random (untrained) values in the original
+                # META checkpoing
+                num_embeds = value.shape[-1] // encoder_dim // num_tiles
+                pos_embedding = torch.zeros(
+                    num_tiles,
+                    num_tiles,
+                    num_embeds,
+                    encoder_dim,
+                    device=value.device,
+                    dtype=value.dtype,
+                )
+                # Loop through aspect ratios and assign precomputed embeds back to Meta Llama embeddings
+                for i, (h, w) in enumerate(supported_aspect_ratios or []):
+                    if h * w == num_tiles:  # h*w < num_tiles is redundant
+                        # i == 0 is used for padding in HF
+                        pos_embedding[:h, :w] = value[i + 1].reshape(
+                            h, w, num_embeds, encoder_dim
+                        )
+                value = pos_embedding
+
+        converted_state_dict[new_key] = value
+    return converted_state_dict
+
+
+def llama3_vision_tune_to_hf(
+    state_dict: Dict[str, torch.Tensor],
+    num_heads: int = 32,
+    num_kv_heads: int = 32,
+    dim: int = 4096,
+    head_dim: int = None,
+    vocab_size: int = 128256,
+    cross_attention_layers: Optional[List[int]] = None,
+    # Vision Encoder Paramters
+    encoder_dim: int = 1280,
+    tile_size: int = 448,
+    num_tiles: int = 4,
+    supported_aspect_ratios: List[Tuple[int, int]] = None,
+) -> Dict[str, torch.Tensor]:
+    """
+    Convertor from Tune state dict to HF state dict. This handles:
+    - Updateing the cross attention layer numbers
+    - skip loading the rope embeddings
+    - reshaping q, k projections
+    """
+    converted_state_dict = {}
+    inverted_mapping_dict = {v: k for k, v in _FROM_HF.items()}
+    # missing keys in _FROM_HF due to naming collisions
+    missing_keys = {
+        "decoder.layers.{}.fusion_layer.ca_norm.scale": "language_model.model.layers.{}.input_layernorm.weight",
+        "decoder.layers.{}.fusion_layer.mlp_norm.scale": "language_model.model.layers.{}.post_attention_layernorm.weight",
+        "decoder.layers.{}.fusion_layer.mlp.w1.weight": "language_model.model.layers.{}.mlp.gate_proj.weight",
+        "decoder.layers.{}.fusion_layer.mlp.w3.weight": "language_model.model.layers.{}.mlp.up_proj.weight",
+        "decoder.layers.{}.fusion_layer.mlp.w2.weight": "language_model.model.layers.{}.mlp.down_proj.weight",
+        "decoder.tok_embeddings.fusion_embedding.weight": None,
+    }
+    inverted_mapping_dict.update(missing_keys)
+
+    if head_dim is None:
+        head_dim = dim // num_heads
+    if cross_attention_layers is None:
+        cross_attention_layers = []
+    # convert hf layer numbers to tune numbers
+    cross_attention_layers = [
+        l - i for i, l in enumerate(sorted(cross_attention_layers))
+    ]
+
+    def _permute(t, n_heads):
+        return (
+            t.view(n_heads, head_dim // 2, 2, dim)
+            .transpose(1, 2)
+            .reshape((head_dim * n_heads), dim)
+        )
+
+    for key, value in state_dict.items():
+        new_key = get_mapped_key(key, inverted_mapping_dict)
+        if "decoder" in key:
+            if "layers" in key:  # Update layer numbers
+                layer = int(key.split(".")[2])
+                num_shifts = sum(layer > l for l in cross_attention_layers)
+                new_layer = layer + num_shifts
+                key_lst = new_key.split(".")
+                if layer in cross_attention_layers and "fusion_layer" not in key:
+                    new_layer += 1  # hf treats the fusion_layer as an additional layer
+                key_lst[3] = str(new_layer)
+                new_key = ".".join(key_lst)
+            if "q_proj" in key and "cross_attn" not in new_key:
+                value = _permute(value, num_heads)
+            elif "k_proj" in key and "cross_attn" not in new_key:
+                value = _permute(value, num_kv_heads)
+            elif key == "decoder.tok_embeddings.weight":
+                learned_embedding = state_dict[
+                    "decoder.tok_embeddings.fusion_embedding.weight"
+                ]
+                value = torch.cat([value, learned_embedding])
+            elif key == "decoder.tok_embeddings.fusion_embedding.weight":
+                continue
+        elif "encoder" in key:
+            if (
+                "tile_pos_embed.embedding" in key
+                or "global_token_positional_embedding" in key
+            ):
+                num_embeds = value.shape[-2]
+                pos_embedding = torch.zeros(
+                    len(supported_aspect_ratios) + 1,
+                    num_tiles,
+                    num_embeds,
+                    encoder_dim,
+                    device=value.device,
+                    dtype=value.dtype,
+                )
+                # Loop through aspect ratios and precompute embeds per aspect ratio
+                for i, (h, w) in enumerate(supported_aspect_ratios or []):
+                    pos_embedding[i + 1, : h * w] = value[:h, :w].reshape(
+                        h * w, num_embeds, encoder_dim
+                    )
+                value = pos_embedding.flatten(1)
+
         converted_state_dict[new_key] = value
     return converted_state_dict
