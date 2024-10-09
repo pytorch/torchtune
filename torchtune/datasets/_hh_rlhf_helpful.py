b3382afbf8f428aafea160f8c4affe2da59b48fe
Mark <75219117+krammnic@users.noreply.github.com>
2024-10-09 14:24:39 -0400

    Update _hh_rlhf_helpful.py

--- torchtune/datasets/_hh_rlhf_helpful.py
+++ torchtune/datasets/_hh_rlhf_helpful.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Any, Dict, Optional
+from typing import Any, Callable, Dict, Optional
 
 from torchtune.data import ChosenRejectedToMessages
 from torchtune.datasets._preference import PreferenceDataset
@@ -18,6 +18,7 @@ def hh_rlhf_helpful_dataset(
     column_map: Optional[Dict[str, str]] = None,
     train_on_input: bool = False,
     new_system_prompt: Optional[str] = None,
+    filter_fn: Optional[Callable] = None,
     split: str = "train",
     **load_dataset_kwargs: Dict[str, Any],
 ) -> PreferenceDataset:
@@ -42,6 +43,9 @@ def hh_rlhf_helpful_dataset(
         new_system_prompt (Optional[str]): if specified, prepend a system message to every sample for both chosen
             and rejected. This can serve as instructions to guide the model response. Setting this will OVERRIDE
             any system messages already present in the dataset. Default is None.
+        filter_fn (Optional[Callable]): callable used to filter the dataset prior to any pre-processing. See
+            the Hugging Face `docs <https://huggingface.co/docs/datasets/v2.20.0/process#select-and-filter>`_ for more
+            details.
         split (str): ``split`` argument for ``datasets.load_dataset``. You can use this argument to load a subset
             of a given split, e.g. ``split="train[:10%]"``. Default is "train".
         **load_dataset_kwargs (Dict[str, Any]): additional keyword arguments to pass to ``load_dataset``.
@@ -60,6 +64,7 @@ def hh_rlhf_helpful_dataset(
         source=source,
         message_transform=message_transform,
         tokenizer=tokenizer,
+        filter_fn=filter_fn,
         split=split,
         **load_dataset_kwargs,
     )
