664120df3bdacab02229a364bb3175bf29decd9a
Salman Mohammadi <salman.mohammadi@outlook.com>
2024-10-11 17:27:11 +0100

    slight change

--- torchtune/modules/transformer.py
+++ torchtune/modules/transformer.py
@@ -623,7 +623,7 @@ class TransformerDecoder(nn.Module):
 @deprecated(
     msg="Please use torchtune.modules.TransformerDecoder instead. \
 If you need an example, see torchtune.models.qwen2._component_builders.py \
-and how to implement torch.modules.TiedLinear for the output projection."
+on how to use torch.modules.TiedLinear for the output projection."
 )
 class TiedEmbeddingTransformerDecoder(nn.Module):
     """
