afe56519be84e13f300b9d7ff4662100f5441a91
Mark <75219117+krammnic@users.noreply.github.com>
2024-10-13 13:57:07 -0400

    fix lint __init__.py

--- torchtune/modules/__init__.py
+++ torchtune/modules/__init__.py
@@ -7,11 +7,11 @@
 from .attention import MultiHeadAttention  # noqa
 from .attention_utils import create_block_causal_mask, packed_block_causal_mask
 from .common_utils import reparametrize_as_dtype_state_dict_post_hook
-from .lr_schedulers import get_cosine_schedule_with_warmup  # noqa
 from .feed_forward import FeedForward  # noqa
 from .kv_cache import KVCache  # noqa
 from .layer_norm import Fp32LayerNorm  # noqa
 from .low_precision import FrozenNF4Linear  # noqa
+from .lr_schedulers import get_cosine_schedule_with_warmup  # noqa
 from .position_embeddings import RotaryPositionalEmbeddings  # noqa
 from .rms_norm import RMSNorm  # noqa
 from .tanh_gate import TanhGate  # noqa
