e37a3e1b256276f0af9cd484dd777b55d2761912
RdoubleA <rafiayub@fb.com>
2024-10-24 18:14:28 -0700

    first

--- torchtune/modules/peft/_utils.py
+++ torchtune/modules/peft/_utils.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import contextlib
-from typing import Any, Dict, Generator, List, Literal, Optional, Protocol, Set
+from typing import Any, Dict, Generator, List, Literal, Optional, Protocol, Set, Union
 
 import torch
 from torch import nn
@@ -62,7 +62,7 @@ def get_adapter_params(model: nn.Module) -> Dict[str, nn.Parameter]:
     return adapter_params
 
 
-def set_trainable_params(model: nn.Module, adapter_params: Dict[str, Any]) -> None:
+def set_trainable_params(model: nn.Module, adapter_params: Union[Dict[str, Any], Set]) -> None:
     """
     Set trainable parameters for an nn.Module based on a state dict of adapter parameters.
 
