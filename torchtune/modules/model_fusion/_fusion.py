a8666feb2dce08cf3fe15d5218fe7f1817c0783f
RdoubleA <rafiayub@fb.com>
2024-10-28 16:40:30 -0700

    support multiple encoders, update DeepFusion, docstrings

--- torchtune/modules/model_fusion/_fusion.py
+++ torchtune/modules/model_fusion/_fusion.py
@@ -7,7 +7,7 @@
 from typing import Any, Dict, List, Optional, Union
 
 import torch
-from torch import nn, Tensor
+from torch import nn
 from torchtune.modules import TransformerDecoder
 from torchtune.modules.model_fusion._fusion_utils import get_fusion_params
 from torchtune.modules.peft._utils import set_trainable_params
@@ -288,14 +288,19 @@ class FusionEmbedding(nn.Module):
 
 class DeepFusionModel(nn.Module):
     """DeepFusion is a type of fused model architecture where a pretrained encoder is combined
-    with a pretrained decoder (LLM). This is a popular architecture for multimodal models, with
+    with a pretrained decoder (LLM) in the internal decoder layers. This is a popular architecture for multimodal models, with
     a full overview available in `The Evolution of Multimodal Model Architectures <https://arxiv.org/abs/2405.17927>`_.
+    A common deep fusion architecture is to fuse the encoder input into the decoder with interspersed cross-attention
+    layers. This module makes no assumptions on how the encoder and decoder are fused; it simply
+    passes in the encoder embeddings to the decoder and lets the decoder handle any fusion.
 
     This module has the same methods and forward signature as :class:`~torchtune.modules.TransformerDecoder` and can be used
     interchangeably where :class:`~torchtune.modules.TransformerDecoder` is. It combines the encoder with the decoder as a
     single module for checkpointing and finetuning. It is expected that the encoder and decoder
     are already defined with any extra learnable ``fusion_params``: learnable parameters to help
     adapt the pre-trained encoder to the pre-trained decoder.
+
+    DeepFusionModel currently only supports a single encoder.
 
     Example:
         >>> # decoder is a TransformerDecoder (e.g. llama3_8b) with fused cross attention layers
@@ -309,10 +314,10 @@ class DeepFusionModel(nn.Module):
         >>> # encoder is pre-trained encoder (e.g. clip_vit_224) with an added projection head
         >>> projection_head = FeedForward(...)
         >>> register_fusion_module(projection_head))
-        >>> encoder = nn.Sequential(clip_vit_224(), projection_head)
+        >>> encoders = {"image": nn.Sequential(clip_vit_224(), projection_head)}
         >>>
         >>> # DeepFusionModel combines the encoder and decoder
-        >>> model = DeepFusionModel(decoder, encoder)
+        >>> model = DeepFusionModel(decoder, encoders)
         >>>
         >>> # Load full fused checkpoints (e.g. a Flamingo checkpoint)
         >>> model.load_state_dict(...)
@@ -322,35 +327,63 @@ class DeepFusionModel(nn.Module):
         >>> model.decoder.load_state_dict(..., strict=False)
         >>>
         >>> # Forward pass
+        >>> encoder_input = {"image": {...}}
         >>> output = model(tokens, mask, encoder_input, encoder_mask, input_pos)
 
     Args:
         decoder (TransformerDecoder): decoder module
-        encoder (nn.Module): encoder module
+        encoders (Dict[str, nn.Module]): dictionary mapping encoder name as a string to the encoder module.
         decoder_trainable (bool): whether to train or freeze the decoder. Default is False.
-        encoder_trainable (bool): whether to train or freeze the encoder. Default is False.
+        encoders_trainable (Union[bool, Dict[str, bool]]): whether to train or freeze the encoder. Use a single
+            boolean to set trainable for all encoders or a dictionary keyed by encoder names to specify trainable
+            for each encoder individually. Encoder names should match with ``encoders``. Default is False.
         fusion_trainable (bool): whether to train the fusion parameters. Default is True.
 
+    Raises:
+        ValueError: if ``encoders`` and ``encoders_trainable`` keys do not match
+        ValueError: if ``len(encoders) != 1``
     """
 
     def __init__(
         self,
         decoder: TransformerDecoder,
-        encoder: nn.Module,
+        encoders: Dict[str, nn.Module],
         *,
         decoder_trainable: bool = False,
-        encoder_trainable: bool = False,
+        encoders_trainable: Union[bool, Dict[str, bool]] = False,
         fusion_trainable: bool = True,
     ):
         super().__init__()
+        if (
+            not isinstance(encoders_trainable, bool)
+            and encoders.keys() != encoders_trainable.keys()
+        ):
+            raise ValueError(
+                f"Found mismatched keys in encoders and encoders_trainable. Got {encoders.keys()} and {encoders_trainable.keys()}."
+            )
+        # Currently, only a single encoder is supported, so user can only
+        # pass in a single key. When multiple encoders are
+        # supported, this can be removed.
+        if len(encoders.keys()) != 1:
+            raise ValueError(
+                f"DeepFusionModel only supports a single encoder. Got {len(encoders.keys())} encoders."
+            )
+
         self.decoder = decoder
-        self.encoder = encoder
+        self.encoders = nn.ModuleDict(encoders)
+        self.encoders_trainable = (
+            {k: encoders_trainable for k in self.encoders.keys()}
+            if isinstance(encoders_trainable, bool)
+            else encoders_trainable
+        )
 
         trainable_params = set()
-        if encoder_trainable:
-            trainable_params |= {
-                f"encoder.{n}" for n, p in self.encoder.named_parameters()
-            }
+        for encoder, trainable in self.encoders_trainable.items():
+            if trainable:
+                trainable_params |= {
+                    f"encoders.{encoder}.{n}"
+                    for n, p in self.encoders[encoder].named_parameters()
+                }
         if decoder_trainable:
             trainable_params |= {
                 f"decoder.{n}" for n, p in self.decoder.named_parameters()
@@ -384,8 +417,8 @@ class DeepFusionModel(nn.Module):
         Args:
             batch_size (int): batch size for the caches.
             dtype (torch.dtype): dtype for the caches.
-            encoder_max_seq_len (int): maximum encoder cache sequence length.
-            decoder_max_seq_len (int): maximum decoder cache sequence length.
+            encoder_max_seq_len (Optional[int]): maximum encoder cache sequence length.
+            decoder_max_seq_len (Optional[int]): maximum decoder cache sequence length.
         """
         self.decoder.setup_caches(
             batch_size,
@@ -434,7 +467,8 @@ class DeepFusionModel(nn.Module):
                 before the softmax. A value of True in row i and column j means token i attends
                 to token j. A value of False means token i does not attend to token j. If no
                 mask is specified, a causal mask is used by default. Default is None.
-            encoder_input (Optional[Dict[str, Any]]): Optional input for the encoder.
+            encoder_input (Optional[Dict[str, Dict[str, Any]]]): Optional input kwargs for the encoders. Must be
+                keyed by encoder name and match the keys of ``encoders``
             encoder_mask (Optional[torch.Tensor]):  Boolean tensor defining a relational matrix between
                 tokens and encoder embeddings. A True value at position i,j means token i can attend
                 to embedding j in the decoder. Mask has shape ``[b x s x s_e]``. Default is None.
@@ -454,6 +488,9 @@ class DeepFusionModel(nn.Module):
                 output tensors defined by ``output_hidden_states`` with the \
                 final output tensor appended to the list.
 
+        Raises:
+            ValueError: if ``encoder_input`` keys do not match ``encoders`` keys
+
         Notation used for tensor shapes:
             - b: batch size
             - s: token sequence length
@@ -463,17 +500,32 @@ class DeepFusionModel(nn.Module):
             - d_e: encoder embed dim
             - m_s: max seq len
         """
+        if encoder_input is not None and encoder_input.keys() != self.encoders.keys():
+            raise ValueError(
+                f"Found mismatched keys in encoder_input and instantiated encoders. "
+                f"Got {encoder_input.keys()}, expected {self.encoders.keys()}."
+            )
         # During decoding, encoder_input will only be provided
         # for new inputs. Previous encoder outputs are cached
         # in the decoder cache.
         encoder_embed = None
         if encoder_input is not None:
-            encoder_embed = self.encoder(**encoder_input)
+            encoder_embed = {
+                encoder: self.encoders[encoder](**encoder_input[encoder])
+                for encoder in encoder_input
+            }
+
+        # Currently, only a single encoder is supported, so we need
+        # to get the encoder key manually. When multiple encoders are
+        # supported, this can be removed.
+        decoder_encoder_input = (
+            list(encoder_embed.values())[0] if encoder_embed is not None else None
+        )
 
         output = self.decoder(
             tokens=tokens,
             mask=mask,
-            encoder_input=encoder_embed,
+            encoder_input=decoder_encoder_input,
             encoder_mask=encoder_mask,
             input_pos=input_pos,
         )
@@ -481,18 +533,84 @@ class DeepFusionModel(nn.Module):
 
 
 class EarlyFusionModel(nn.Module):
+    """EarlyFusion is a type of fused model architecture where pretrained encoder(s) are combined
+    with a pretrained decoder (LLM) at the model input and not in internal layers. This is a popular architecture
+    for multimodal models, with a full overview available in `The Evolution of Multimodal Model Architectures
+    <https://arxiv.org/abs/2405.17927>`_. This module assumes the decoder is trained to recognize tokens specific
+    to each encoder, which are then replaced in the sequence with the encoder embedding outputs.
+
+    This module has the same methods and forward signature as :class:`~torchtune.modules.TransformerDecoder` and can be used
+    interchangeably where :class:`~torchtune.modules.TransformerDecoder` is. It combines the encoders with the decoder as a
+    single module for checkpointing and finetuning. It is expected that the encoders and decoder
+    are already defined with any extra learnable ``fusion_params``: learnable parameters to help
+    adapt the pre-trained encoders to the pre-trained decoder.
+
+    You can pass in multiple encoders as a dictionary into ``encoders``.
+
+    Example:
+        >>> # decoder is a text-only TransformerDecoder (e.g. llama3_8b) with no modifications
+        >>> decoder = llama3_8b()
+        >>>
+        >>> # encoder is pre-trained encoder (e.g. clip_vit_224) with an added projection head
+        >>> projection_head = FeedForward(...)
+        >>> register_fusion_module(projection_head))
+        >>> encoders = {"image": nn.Sequential(clip_vit_224(), projection_head)}
+        >>>
+        >>> # EarlyFusionModel combines the encoder and decoder
+        >>> model = DeepFusionModel(decoder, encoders)
+        >>>
+        >>> # Load full fused checkpoints
+        >>> model.load_state_dict(...)
+        >>>
+        >>> # Or load pretrained individual models (fusion_params are not loaded)
+        >>> model.encoder.load_state_dict(..., strict=False)
+        >>> model.decoder.load_state_dict(..., strict=False)
+        >>>
+        >>> # Forward pass
+        >>> encoder_input = {"image": {...}}
+        >>> output = model(tokens, mask, encoder_input, encoder_mask, input_pos)
+
+    Args:
+        decoder (TransformerDecoder): decoder module
+        encoders (Dict[str, nn.Module]): dictionary mapping encoder name as a string to the encoder module.
+        encoder_tokens (Dict[str, int]): dictionary mapping encoder name to special token ID indicating where
+            in the text sequence the encoder embedding outputs should be injected.
+        decoder_trainable (bool): whether to train or freeze the decoder. Default is False.
+        encoders_trainable (Union[bool, Dict[str, bool]]): whether to train or freeze the encoder. Use a single
+            boolean to set trainable for all encoders or a dictionary keyed by encoder names to specify trainable
+            for each encoder individually. Encoder names should match with ``encoders``. Default is False.
+        fusion_trainable (bool): whether to train the fusion parameters. Default is True.
+
+    Raises:
+        ValueError: if ``encoders`` and ``encoders_trainable`` keys do not match
+    """
+
     def __init__(
         self,
         decoder: TransformerDecoder,
-        encoders: nn.ModuleDict,
+        encoders: Dict[str, nn.Module],
         encoder_tokens: Dict[str, int],
         decoder_trainable: bool,
-        encoders_trainable: Dict[str, bool],
+        encoders_trainable: Union[bool, Dict[str, bool]] = False,
+        fusion_trainable: bool = True,
     ):
         super().__init__()
+        if encoders.keys() != encoder_tokens.keys() or (
+            not isinstance(encoders_trainable, bool)
+            and encoders.keys() != encoders_trainable.keys()
+        ):
+            raise ValueError(
+                f"Found mismatched keys in encoders and encoders_trainable. Got {encoders.keys()} and {encoders_trainable.keys()}."
+            )
+
         self.decoder = decoder
-        self.encoders = encoders
+        self.encoders = nn.ModuleDict(encoders)
         self.encoder_tokens = encoder_tokens
+        self.encoders_trainable = (
+            {k: encoders_trainable for k in self.encoders.keys()}
+            if isinstance(encoders_trainable, bool)
+            else encoders_trainable
+        )
 
         # A little surgery in the decoder to give the
         # fusion module access to control the embeddings
@@ -508,7 +626,7 @@ class EarlyFusionModel(nn.Module):
         )
 
         trainable_params = set()
-        for encoder, trainable in encoders_trainable.items():
+        for encoder, trainable in self.encoders_trainable.items():
             if trainable:
                 trainable_params |= {
                     f"encoders.{encoder}.{n}"
@@ -518,6 +636,11 @@ class EarlyFusionModel(nn.Module):
             trainable_params |= {
                 f"decoder.{n}" for n, p in self.decoder.named_parameters()
             }
+        if fusion_trainable:
+            trainable_params |= set(get_fusion_params(self))
+        else:
+            trainable_params -= set(get_fusion_params(self))
+
         set_trainable_params(self, trainable_params)
 
     def _state_dict_hook(self, destination, prefix, keep_vars):
@@ -562,18 +685,59 @@ class EarlyFusionModel(nn.Module):
 
     def forward(
         self,
-        tokens: Tensor,
+        tokens: torch.Tensor,
         *,
-        mask: Optional[Tensor] = None,
+        mask: Optional[torch.Tensor] = None,
         encoder_input: Optional[Dict[str, Dict[str, Any]]] = None,
-        input_pos: Optional[Tensor] = None,
+        input_pos: Optional[torch.Tensor] = None,
         **kwargs: Dict[str, Any],  # no need for encoder_mask
-    ) -> Tensor:
+    ) -> torch.Tensor:
         """
-        For the token IDs associated with each encoder, we are assuming that the number of tokens have already
-        been expanded to the number of tokens encoded for the given media. For example, if an image is tiled/patched
-        and tokenized to 100 tokens, we assume the text sequence already has 100 "image" tokens as placeholders.
+        Args:
+            tokens (torch.Tensor): input tensor with shape ``[b x s]``
+            mask (Optional[torch.Tensor]): Optional boolean tensor which contains the attention mask
+                with shape ``[b x s x s]``. This is applied after the query-key multiplication and
+                before the softmax. A value of True in row i and column j means token i attends
+                to token j. A value of False means token i does not attend to token j. If no
+                mask is specified, a causal mask is used by default. Default is None.
+            encoder_input (Optional[Dict[str, Dict[str, Any]]]): Optional input kwargs for the encoders. Must be
+                keyed by encoder name and match the keys of ``encoders``
+            input_pos (Optional[torch.Tensor]): Optional tensor which contains the position ids
+                of each token. During training, this is used to indicate the positions
+                of each token relative to its sample when packed, shape ``[b x s]``.
+                During inference, this indicates the position of the current token.
+                If none, assume the index of the token is its position id. Default is None.
+            **kwargs (Dict[str, Any]): additional keyword arguments. This is solely used to match the
+                :class:`~torchtune.modules.TransformerDecoder` forward and does not have any effect.
+
+        Note: At the very first step of inference, when the model is provided with a prompt,
+        ``input_pos`` would contain the positions of all of the tokens in the prompt
+        (eg: ``torch.arange(prompt_length)``). This is because we will need to compute the
+        KV values for each position.
+
+        Returns:
+            torch.Tensor: output tensor with shape ``[b x s x v]`` or a list of layer \
+                output tensors defined by ``output_hidden_states`` with the \
+                final output tensor appended to the list.
+
+        Raises:
+            ValueError: if ``encoder_input`` keys do not match ``encoders`` keys
+
+        Notation used for tensor shapes:
+            - b: batch size
+            - s: token sequence length
+            - s_e: encoder sequence length
+            - v: vocab size
+            - d: token embed dim
+            - d_e: encoder embed dim
+            - m_s: max seq len
         """
+        if encoder_input is not None and encoder_input.keys() != self.encoders.keys():
+            raise ValueError(
+                f"Found mismatched keys in encoder_input and instantiated encoders. "
+                f"Got {encoder_input.keys()}, expected {self.encoders.keys()}."
+            )
+
         embeds = self.tok_embeddings(tokens)
         bsz, seq_len, embed_dim = embeds.shape
         for encoder, inp in (encoder_input or {}).items():
