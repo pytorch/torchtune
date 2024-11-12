1196f8a9227cc55267827335d910931c95161d0f
joecummings <jrcummings27@gmail.com>
2024-11-12 08:05:29 -0800

    Add QDoRA stuffs

--- torchtune/modules/peft/dora.py
+++ torchtune/modules/peft/dora.py
@@ -37,6 +37,13 @@ class DoRALinear(nn.Module, AdapterModule):
             Default: False
         quantize_base (bool): Whether to quantize base linear weight or not.
             Default: False
+        **quantization_kwargs: Keyword arguments to pass to `to_nf4` when quantizing the base linear weight.
+            Examples of valid arguments are `block_size` and `scaler_block_size`, which control the granularity of
+            weight quantization and scaler quantization respectively. This is only used if `quantize_base` is True.
+            Default None
+
+    Raises:
+        ValueError: If ``quantize_base`` is False, but quantization kwargs are provided.
 
     """
 
@@ -49,6 +56,7 @@ class DoRALinear(nn.Module, AdapterModule):
         dropout: float = 0.0,
         use_bias: bool = False,
         quantize_base: bool = False,
+        **quantization_kwargs,
     ):
         super().__init__()
         self.in_dim = in_dim
@@ -56,17 +64,29 @@ class DoRALinear(nn.Module, AdapterModule):
         self.scaling = alpha / rank
         self.use_bias = use_bias
         self._quantize_base = quantize_base
-        weight, bias = self._create_weight_and_bias()
-        self.register_parameter("weight", nn.Parameter(weight))
-        self.register_parameter(
-            "bias", nn.Parameter(bias) if bias is not None else None
+
+        if not self._quantize_base and quantization_kwargs:
+            raise ValueError(
+                f"``quantize_base`` is False, but received the following quantization arguments: {quantization_kwargs}"
+            )
+
+        # Setup weight and bias
+        linear = nn.Linear(in_features=in_dim, out_features=out_dim, bias=self.use_bias)
+        weight = (
+            linear.weight
+            if not self._quantize_base
+            else to_nf4(linear.weight, **quantization_kwargs)
         )
+        bias = linear.bias if self.use_bias else None
 
         # 'self.disabled' is a flag showing whether to turn off DoRA adapters,
         # this can be used in DPO for treating the dora adapters as the policy model
         # and disabling it to treat the base model as the reference model
         self.disabled = False
-
+        self.register_parameter("weight", nn.Parameter(weight))
+        self.register_parameter(
+            "bias", nn.Parameter(bias) if bias is not None else None
+        )
         self.dropout = nn.Dropout(p=dropout) if dropout > 0.0 else nn.Identity()
         self.lora_a = nn.Linear(in_features=in_dim, out_features=rank, bias=False)
         self.lora_b = nn.Linear(in_features=rank, out_features=out_dim, bias=False)
@@ -89,19 +109,6 @@ class DoRALinear(nn.Module, AdapterModule):
         lora_weight = self.lora_b.weight @ self.lora_a.weight
         weight_norm = self._get_weight_norm(base_weight, lora_weight)
         self.magnitude.copy_(weight_norm)
-
-    def _create_weight_and_bias(self):
-        """
-        Creates a linear weight and bias tensor, using NF4 dtype if we're quantizing
-        (indicated via quantize_base=True).
-        """
-        in_dim, out_dim, use_bias = self.in_dim, self.out_dim, self.use_bias
-        linear = nn.Linear(in_features=in_dim, out_features=out_dim, bias=use_bias)
-        weight = linear.weight if not self._quantize_base else to_nf4(linear.weight)
-        bias = None
-        if self.use_bias:
-            bias = linear.bias
-        return weight, bias
 
     def _get_weight_norm(self, weight, lora_weight):
         weight = weight + self.scaling * lora_weight
