b69f2f30c875871476a3099fe87c120d79b29f4c
Mostafa Elhoushi <melhoushi@meta.com>
2024-11-25 04:37:52 +0000

    refactor layer_dropout and add test cases on wrapper

--- tests/torchtune/modules/test_layer_dropout.py
+++ tests/torchtune/modules/test_layer_dropout.py
@@ -14,6 +14,7 @@ from tests.test_utils import assert_expected
 from torchtune.modules.layer_dropout import (
     get_scale,
     LayerDropout,
+    ModuleLayerDropoutWrapper,
     prepare_layer_dropout,
     ScaleType,
 )
@@ -22,15 +23,11 @@ from torchtune.modules.layer_dropout import (
 class TestLayerDropout:
     """Class for testing LayerDropout implementation."""
 
-    @pytest.fixture(autouse=True)
-    def random(self):
-        torch.manual_seed(0)
-
     @pytest.fixture
     def input_shape(self) -> Tuple[int, int]:
-        bsz = 32
-        seqlen = 1024
-        dim = 4096
+        bsz = 8
+        seqlen = 256
+        dim = 32
         return bsz, seqlen, dim
 
     @pytest.fixture
@@ -45,7 +42,7 @@ class TestLayerDropout:
 
     def test_forward_train_prob_1(
         self, layer_dropout: LayerDropout, input: torch.Tensor
-    ) -> None:
+    ):
         # With dropout probability = 1.0, we expect output to be the same as input
         layer_dropout.prob = 1.0
         output = layer_dropout.forward(lambda x: x**2, input)
@@ -53,15 +50,13 @@ class TestLayerDropout:
 
     def test_forward_train_prob_0(
         self, layer_dropout: LayerDropout, input: torch.Tensor
-    ) -> None:
+    ):
         # With dropout probability = 1.0, we expect the operation to be applied on all elements in the input
         layer_dropout.prob = 0.0
         output = layer_dropout.forward(lambda x: x**2, input)
         assert torch.allclose(output, input**2)
 
-    def test_forward_eval(
-        self, layer_dropout: LayerDropout, input: torch.Tensor
-    ) -> None:
+    def test_forward_eval(self, layer_dropout: LayerDropout, input: torch.Tensor):
         layer_dropout.prob = 1.0
         layer_dropout.eval()
 
@@ -74,7 +69,79 @@ class TestLayerDropout:
             output = layer_dropout.forward(lambda x: x**2, input)
         assert torch.allclose(output, input)
 
-    def test_get_scale_uniform(self) -> None:
+
+class TestLayerDropoutWrapper:
+    @pytest.fixture
+    def input_shape(self) -> Tuple[int, int]:
+        bsz = 4
+        dim = 8
+        return (bsz, dim)
+
+    @pytest.fixture
+    def input(self, input_shape: Tuple[int]) -> torch.Tensor:
+        return torch.randn(input_shape)
+
+    @pytest.fixture
+    def model(self, input_shape) -> torch.nn.Module:
+        _, dim = input_shape
+        return torch.nn.Sequential(
+            torch.nn.Linear(dim, 32), torch.nn.ReLU(), torch.nn.Linear(32, dim)
+        )
+
+    @pytest.fixture
+    def linear(self, input_shape) -> torch.nn.Module:
+        _, dim = input_shape
+        return torch.nn.Linear(dim, dim)
+
+    def test_linear(self, linear: torch.nn.Module, input: torch.Tensor):
+        wrapper = ModuleLayerDropoutWrapper(linear, LayerDropout(prob=0.5))
+        assert wrapper.module == linear
+
+        # Test output
+        wrapper.dropout.prob = 1
+        assert torch.allclose(wrapper(input), input)
+        wrapper.dropout.prob = 0
+        assert torch.allclose(wrapper(input), linear(input))
+
+        # Test getters
+        assert wrapper.in_features == linear.in_features
+        assert wrapper.out_features == linear.out_features
+        assert torch.equal(wrapper.weight, linear.weight)
+
+        # Test setters
+        wrapper.weight.data = wrapper.weight.data * 2
+        assert torch.equal(wrapper.weight, linear.weight)
+
+        # Test state_dict
+        for k in wrapper.state_dict().keys():
+            assert torch.equal(wrapper.state_dict()[k], linear.state_dict()[k])
+
+    def test_model(self, model: torch.nn.Module, input: torch.Tensor):
+        wrapper = ModuleLayerDropoutWrapper(model, LayerDropout(prob=0.5))
+        assert wrapper.module == model
+
+        # Test output
+        wrapper.dropout.prob = 1
+        assert torch.allclose(wrapper(input), input)
+        wrapper.dropout.prob = 0
+        assert torch.allclose(wrapper(input), model(input))
+
+        # Test getters
+        assert wrapper[0].in_features == model[0].in_features
+        assert wrapper[0].out_features == model[0].out_features
+        assert torch.equal(wrapper[0].weight, model[0].weight)
+
+        # Test setters
+        wrapper[2].weight.data = wrapper[2].weight.data * 2
+        assert torch.equal(wrapper[2].weight, model[2].weight)
+
+        # Test state_dict
+        for k in wrapper.state_dict().keys():
+            assert torch.equal(wrapper.state_dict()[k], model.state_dict()[k])
+
+
+class TestScales:
+    def test_get_scale_uniform(self):
         scale_type = ScaleType.UNIFORM
         scale_period = 10
 
@@ -83,7 +150,7 @@ class TestLayerDropout:
         assert_expected(get_scale(scale_type, scale_period, scale_period), 1.0)
         assert_expected(get_scale(scale_type, scale_period, scale_period * 2), 1.0)
 
-    def test_get_scale_linear(self) -> None:
+    def test_get_scale_linear(self):
         scale_type = ScaleType.LINEAR
         scale_period = 10
 
@@ -92,7 +159,7 @@ class TestLayerDropout:
         assert_expected(get_scale(scale_type, scale_period, scale_period), 1.0)
         assert_expected(get_scale(scale_type, scale_period, scale_period * 2), 1.0)
 
-    def test_get_scale_exp(self) -> None:
+    def test_get_scale_exp(self):
         scale_type = ScaleType.EXP
         scale_period = 10
 
@@ -104,7 +171,7 @@ class TestLayerDropout:
         assert_expected(get_scale(scale_type, scale_period, scale_period), 1.0)
         assert_expected(get_scale(scale_type, scale_period, scale_period * 2), 1.0)
 
-    def test_get_scale_log(self) -> None:
+    def test_get_scale_log(self):
         scale_type = ScaleType.LOG
         scale_period = 10
 
@@ -116,7 +183,7 @@ class TestLayerDropout:
         assert_expected(get_scale(scale_type, scale_period, scale_period), 1.0)
         assert_expected(get_scale(scale_type, scale_period, scale_period * 2), 1.0)
 
-    def test_get_scale_sin(self) -> None:
+    def test_get_scale_sin(self):
         scale_type = ScaleType.SIN
         scale_period = 10
         val = 5
@@ -124,7 +191,9 @@ class TestLayerDropout:
         actual_scale = get_scale(scale_type, scale_period, val)
         assert_expected(actual_scale, expected_scale, atol=1e-7, rtol=1e-3)
 
-    def test_prepare_layer_dropout_uniform(self) -> None:
+
+class TestLayerDopoutModel:
+    def test_prepare_layer_dropout_uniform(self):
         class MockModel(torch.nn.Module):
             def __init__(self):
                 super().__init__()
@@ -144,7 +213,7 @@ class TestLayerDropout:
             else:
                 assert layer.dropout.prob == 0
 
-    def test_prepare_layer_dropout_exp(self) -> None:
+    def test_prepare_layer_dropout_exp(self):
         class MockModel(torch.nn.Module):
             def __init__(self):
                 super().__init__()
@@ -166,7 +235,7 @@ class TestLayerDropout:
             else:
                 assert layer.dropout.prob > 0 and layer.dropout.prob < prob_max
 
-    def test_prepare_layer_dropout_linear(self) -> None:
+    def test_prepare_layer_dropout_linear(self):
         class MockModel(torch.nn.Module):
             def __init__(self):
                 super().__init__()
