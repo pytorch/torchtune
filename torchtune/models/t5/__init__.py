da5ce2d0f488003923d30967ccc13fcc45b8534e
Calvin Pelletier <calvin.pelletier@gmail.com>
2024-11-25 10:47:32 -0800

    t5 model builder

---
+++ torchtune/models/t5/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ._model_builders import t5_v1p1_xxl_encoder
+
+__all__ = [
+    "t5_v1p1_xxl_encoder",
+]
