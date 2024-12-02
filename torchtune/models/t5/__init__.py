c608ad8e4f84419a90c0c63e2df3953ebafef079
Calvin Pelletier <calvin.pelletier@gmail.com>
2024-12-02 12:38:39 -0800

    exposing component builder

--- torchtune/models/t5/__init__.py
+++ torchtune/models/t5/__init__.py
@@ -4,9 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from ._component_builders import t5_encoder
 from ._model_builders import t5_tokenizer, t5_v1p1_xxl_encoder
 
 __all__ = [
+    "t5_encoder",
     "t5_tokenizer",
     "t5_v1p1_xxl_encoder",
 ]
