beab96391069e2cf6c550e48701c295a1d3b98bd
andrewkho <andrewkh@meta.com>
2024-12-10 16:22:20 -0800

    address comments

--- torchtune/utils/_import_guard.py
+++ torchtune/utils/_import_guard.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import importlib
+
 import torch
 
 # We can only use flex attention / BlockMask if torch version >= 2.5.0 and GPU is Turing / SM75 and above
@@ -11,11 +13,8 @@ _SUPPORTS_FLEX_ATTENTION = (
     torch.cuda.is_available() and torch.cuda.get_device_capability() >= (7, 5)
 )
 
-
 _TORCHDATA_MIN_VERSION = "0.10.0"
-try:
-    from torchdata.nodes import BaseNode, Loader  # noqa
-
+if importlib.util.find_spec("torchdata.nodes") is not None:
     _TORCHDATA_INSTALLED = True
-except ImportError as e:
+else:
     _TORCHDATA_INSTALLED = False
