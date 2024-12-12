ba4f453c0d86462028366415ac8ba8342598682a
pytorchbot <pytorchbot@pytorch.com>
2024-12-12 11:35:32 +0000

    2024-12-12 nightly release (9cfa28835246a4c1ac4449e703eae8f49227db55)

--- recipes/lora_dpo_distributed.py
+++ recipes/lora_dpo_distributed.py
@@ -27,7 +27,6 @@ from torchtune.modules.peft import (
     get_adapter_params,
     get_adapter_state_dict,
     get_merged_lora_ckpt,
-    load_dora_magnitudes,
     LoRALinear,
     set_trainable_params,
     validate_missing_and_unexpected_for_lora,
@@ -400,8 +399,7 @@ class LoRADPORecipeDistributed(FTRecipeInterface):
                 ) and not lora_weights_state_dict:
                     # lora may not be covered in state dict
                     # if finetune for the 1st time
-                    m.lora_a.to_empty(device=lora_device)
-                    m.lora_b.to_empty(device=lora_device)
+                    m.to_empty(device=lora_device)
                     m.initialize_parameters()
                 # RoPE is not covered in state dict
                 if hasattr(m, "rope_init"):
@@ -420,7 +418,9 @@ class LoRADPORecipeDistributed(FTRecipeInterface):
                 is_dora = True
                 m.initialize_dora_magnitude()
         if is_dora:
-            load_dora_magnitudes(model)
+            for m in model.modules():
+                if hasattr(m, "initialize_dora_magnitude"):
+                    m.initialize_dora_magnitude()
         validate_missing_and_unexpected_for_lora(
             lora_attn_modules=self._lora_attn_modules,
             apply_lora_to_mlp=self._apply_lora_to_mlp,
