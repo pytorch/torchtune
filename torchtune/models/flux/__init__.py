91a0eeab22c469b0a3420d53833aa45863056e8c
pytorchbot <pytorchbot@pytorch.com>
2025-01-09 11:35:20 +0000

    2025-01-09 nightly release (cce8ef68145b8a1cdaa7b8a4f90f85edb827cf20)

---
+++ torchtune/models/flux/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+from ._model_builders import flux_1_autoencoder
+
+__all__ = [
+    "flux_1_autoencoder",
+]
