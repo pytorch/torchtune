3760196e6a422a9c531b9763cf441a9d93ee4f3c
Insop Song <insop.song@gmail.com>
2025-01-09 16:37:12 -0800

    Pull out to a variable, review feedback

--- torchtune/modules/loss/kd_losses.py
+++ torchtune/modules/loss/kd_losses.py
@@ -54,7 +54,9 @@ class ForwardKLLoss(torch.nn.Module):
         mask = (labels != self.ignore_index).int()
         if not normalize:
             return -torch.sum(x * mask.view(-1), dim=0)
-        if torch.sum(mask.view(-1), dim=0) == 0:
+
+        sum_masks = torch.sum(mask.view(-1), dim=0)
+        if sum_masks == 0:
             return torch.tensor(0.0, device=x.device)
         return -torch.sum(x * mask.view(-1), dim=0) / torch.sum(mask.view(-1), dim=0)
 
@@ -137,7 +139,8 @@ class ForwardKLWithChunkedOutputLoss(torch.nn.Module):
                 student_chunk, teacher_chunk, label_chunk, normalize=False
             )
 
-        if torch.sum(mask.view(-1), dim=0) == 0:
+        sum_masks = torch.sum(mask.view(-1), dim=0)
+        if sum_masks == 0:
             return torch.tensor(0.0, device=student_logits[0].device)
 
         return total_fkl_loss / torch.sum(mask.view(-1), dim=0)
