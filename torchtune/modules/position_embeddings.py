2a44aed7f79d0b433cf6917b0ca6a54d54fea73f
RdoubleA <rafiayub@fb.com>
2025-01-13 08:58:57 -0800

    missing import

--- torchtune/modules/position_embeddings.py
+++ torchtune/modules/position_embeddings.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional
+from typing import Any, Optional
 
 import torch
 from torch import nn
