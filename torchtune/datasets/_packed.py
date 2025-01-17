aa72a14c2a1ef95352fbb46fa413941f8d91f0c4
Evan Smothers <ebs@fb.com>
2025-01-17 13:56:17 -0800

    temp commit

--- torchtune/datasets/_packed.py
+++ torchtune/datasets/_packed.py
@@ -90,6 +90,9 @@ class PackedDataset(Dataset):
         split_across_pack: bool = False,
     ) -> None:
         self.ds = ds
+        import pdb
+
+        pdb.set_trace()
         self.max_seq_len = max_seq_len
         self.padding_idx = padding_idx
         self.max_packs = max_packs
