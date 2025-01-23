07d519db68430086ec8587efb68a9d9e63df21bf
Calvin Pelletier <calvin.pelletier@gmail.com>
2025-01-23 13:52:42 -0800

    flux flow model builders

--- torchtune/models/flux/__init__.py
+++ torchtune/models/flux/__init__.py
@@ -3,8 +3,18 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-from ._model_builders import flux_1_autoencoder
+from ._model_builders import (
+    flux_1_autoencoder,
+    flux_1_dev_flow_model,
+    flux_1_schnell_flow_model,
+    lora_flux_1_dev_flow_model,
+    lora_flux_1_schnell_flow_model,
+)
 
 __all__ = [
     "flux_1_autoencoder",
+    "flux_1_dev_flow_model",
+    "flux_1_schnell_flow_model",
+    "lora_flux_1_dev_flow_model",
+    "lora_flux_1_schnell_flow_model",
 ]
