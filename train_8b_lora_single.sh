3246cd8a284a527caf29d06d5eb947a35911cd15
vatsal0 <vatsalbaherwani@gmail.com>
2025-02-03 16:47:45 -0500

    Config update for chain of thought

--- train_8b_lora_single.sh
+++ train_8b_lora_single.sh
@@ -11,4 +11,6 @@ module load cuda/12.4.1
 conda init bash
 conda shell.bash activate torchtune
 
+rm /fs/class-projects/fall2024/cmsc473/c473g002/models/Meta-Llama-3.1-8B-Instruct/checkpoints/*.pt
 tune run lora_finetune_single_device --config llama3_1/8B_lora_single_device
+# tune run lora_generate --config lora_generation
