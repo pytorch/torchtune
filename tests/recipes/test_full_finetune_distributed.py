1a749bf06d4312cf74bb2184a69cd293baada211
JessicaZhong <zhengjesszhong@gmail.com>
2025-02-05 21:17:02 -0800

    update test

--- tests/recipes/test_full_finetune_distributed.py
+++ tests/recipes/test_full_finetune_distributed.py
@@ -198,69 +198,69 @@ class TestFullFinetuneDistributedRecipe:
             loss_values, expected_loss_values, rtol=1e-4, atol=1e-4
         )
 
-    # @pytest.mark.integration_test
-    # @pytest.mark.parametrize(
-    #     "config, model_type, ckpt_type, micro_batch_size, gradient_accumulation_steps, optim_in_bwd",
-    #     [
-    #         ("llama2/7B_full", "llama2", "hf", 1, 4, False),
-    #         ("llama3/8B_full", "llama3", "tune", 1, 4, False),
-    #         ("llama3/8B_full", "llama3", "tune", 4, 1, True),
-    #     ],
-    # )
-    # @gpu_test(gpu_count=1)
-    # def test_loss_single_rank(
-    #     self,
-    #     micro_batch_size,
-    #     gradient_accumulation_steps,
-    #     config,
-    #     model_type,
-    #     ckpt_type,
-    #     optim_in_bwd,
-    #     tmpdir,
-    #     monkeypatch,
-    # ):
-    #     ckpt_component = CKPT_COMPONENT_MAP[ckpt_type]
-    #     ckpt = model_type + "_" + ckpt_type
-    #     ckpt_path = Path(CKPT_MODEL_PATHS[ckpt])
-    #     tokenizer_path = Path(TOKENIZER_PATHS[model_type])
-    #     ckpt_dir = ckpt_path.parent
-    #     log_file = gen_log_file_name(tmpdir)
+    @pytest.mark.integration_test
+    @pytest.mark.parametrize(
+        "config, model_type, ckpt_type, micro_batch_size, gradient_accumulation_steps, optim_in_bwd",
+        [
+            ("llama2/7B_full", "llama2", "hf", 1, 4, False),
+            ("llama3/8B_full", "llama3", "tune", 1, 4, False),
+            ("llama3/8B_full", "llama3", "tune", 4, 1, True),
+        ],
+    )
+    @gpu_test(gpu_count=1)
+    def test_loss_single_rank(
+        self,
+        micro_batch_size,
+        gradient_accumulation_steps,
+        config,
+        model_type,
+        ckpt_type,
+        optim_in_bwd,
+        tmpdir,
+        monkeypatch,
+    ):
+        ckpt_component = CKPT_COMPONENT_MAP[ckpt_type]
+        ckpt = model_type + "_" + ckpt_type
+        ckpt_path = Path(CKPT_MODEL_PATHS[ckpt])
+        tokenizer_path = Path(TOKENIZER_PATHS[model_type])
+        ckpt_dir = ckpt_path.parent
+        log_file = gen_log_file_name(tmpdir)
 
-    #     # Config file needed for model conversion.
-    #     write_hf_ckpt_config(ckpt_dir)
+        # Config file needed for model conversion.
+        write_hf_ckpt_config(ckpt_dir)
 
-    #     cmd = f"""
-    #     tune run --nnodes 1 --nproc_per_node 1 full_finetune_distributed \
-    #         --config {config} \
-    #         batch_size={micro_batch_size} \
-    #         gradient_accumulation_steps={gradient_accumulation_steps} \
-    #         output_dir={tmpdir} \
-    #         checkpointer._component_={ckpt_component} \
-    #         checkpointer.checkpoint_dir='{ckpt_dir}' \
-    #         checkpointer.checkpoint_files=[{ckpt_path}]\
-    #         checkpointer.output_dir={tmpdir} \
-    #         checkpointer.model_type={model_type.upper()} \
-    #         tokenizer.path='{tokenizer_path}' \
-    #         tokenizer.prompt_template=null \
-    #         metric_logger.filename={log_file} \
-    #     """.split()
-    #     model_config = MODEL_TEST_CONFIGS[model_type]
-    #     cmd = cmd + self._get_test_config_overrides() + model_config
-    #     # "optimizer_in_bwd=True" would free gradient info before clip_grad, causing
-    #     # wrong grad_norm, so we only test one of them each time. But loss values
-    #     # should be the same.
-    #     if not optim_in_bwd:
-    #         cmd.append("clip_grad_norm=100")
-    #     else:
-    #         cmd.append("optimizer_in_bwd=True")
+        cmd = f"""
+        tune run --nnodes 1 --nproc_per_node 1 full_finetune_distributed \
+            --config {config} \
+            batch_size={micro_batch_size} \
+            gradient_accumulation_steps={gradient_accumulation_steps} \
+            output_dir={tmpdir} \
+            checkpointer._component_={ckpt_component} \
+            checkpointer.checkpoint_dir='{ckpt_dir}' \
+            checkpointer.checkpoint_files=[{ckpt_path}]\
+            checkpointer.output_dir={tmpdir} \
+            checkpointer.model_type={model_type.upper()} \
+            tokenizer.path='{tokenizer_path}' \
+            tokenizer.prompt_template=null \
+            metric_logger.filename={log_file} \
+        """.split()
+        model_config = MODEL_TEST_CONFIGS[model_type]
+        cmd = cmd + self._get_test_config_overrides() + model_config
+        # "optimizer_in_bwd=True" would free gradient info before clip_grad, causing
+        # wrong grad_norm, so we only test one of them each time. But loss values
+        # should be the same.
+        if not optim_in_bwd:
+            cmd.append("clip_grad_norm=100")
+        else:
+            cmd.append("optimizer_in_bwd=True")
 
-    #     monkeypatch.setattr(sys, "argv", cmd)
-    #     runpy.run_path(TUNE_PATH, run_name="__main__")
-    #     loss_values = get_loss_values_from_metric_logger(log_file)
-    #     expected_loss_values = self._fetch_expected_loss_values_single_rank(model_type)
-    #     torch.testing.assert_close(
-    #         loss_values, expected_loss_values, rtol=1e-4, atol=1e-4
-    #     )
+        monkeypatch.setattr(sys, "argv", cmd)
+        runpy.run_path(TUNE_PATH, run_name="__main__")
+        loss_values = get_loss_values_from_metric_logger(log_file)
+        expected_loss_values = self._fetch_expected_loss_values_single_rank(model_type)
+        torch.testing.assert_close(
+            loss_values, expected_loss_values, rtol=1e-4, atol=1e-4
+        )
 
     @pytest.mark.integration_test
     @pytest.mark.parametrize(
