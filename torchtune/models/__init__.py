d11ec4995381e4a8375987e2c0f4044bf5b76b97
Salman Mohammadi <salman.mohammadi@outlook.com>
2025-02-07 00:58:04 +0000

    whoops

--- torchtune/models/__init__.py
+++ torchtune/models/__init__.py
@@ -2,4 +2,4 @@
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this sou
+# LICENSE file in the root directory of this source tree.
