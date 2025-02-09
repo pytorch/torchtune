52e5709c03cc1d2c1c1e24dcc48f10441cd0bf2e
Bogdan Salyp <bogdansalyp@gmail.com>
2025-02-09 04:43:03 +0000

    fix: torch and torchvision check alongside torchao one

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,11 +4,21 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Check at the top-level that torchao is installed.
+# Check at the top-level that torch, torchao, and torchvision are installed.
 # This is better than doing it at every import site.
 # We have to do this because it is not currently possible to
 # properly support both nightly and stable installs of PyTorch + torchao
 # in pyproject.toml.
+try:
+    import torch  # noqa
+except ImportError as e:
+    raise ImportError(
+        """
+        torch not installed.
+        Please follow the instructions at https://pytorch.org/torchtune/main/install.html#pre-requisites
+        to install torch.
+        """
+    ) from e
 try:
     import torchao  # noqa
 except ImportError as e:
@@ -17,5 +27,15 @@ except ImportError as e:
         torchao not installed.
         Please follow the instructions at https://pytorch.org/torchtune/main/install.html#pre-requisites
         to install torchao.
+        """
+    ) from e
+try:
+    import torchvision  # noqa
+except ImportError as e:
+    raise ImportError(
+        """
+        torchvision not installed.
+        Please follow the instructions at https://pytorch.org/torchtune/main/install.html#pre-requisites
+        to install torchvision.
         """
     ) from e
