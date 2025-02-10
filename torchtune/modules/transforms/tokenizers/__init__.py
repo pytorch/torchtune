d39e717d94df5d18c7deb453af5982af5fcb500c
Mark Obozov <markobozov@MacBook-Pro-Mark.local>
2025-02-10 21:18:31 +0300

    update __init__

--- torchtune/modules/transforms/tokenizers/__init__.py
+++ torchtune/modules/transforms/tokenizers/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from ._gpt2 import GPT2BaseTokenizer
 from ._sentencepiece import SentencePieceBaseTokenizer
 from ._tiktoken import TikTokenBaseTokenizer
 from ._utils import (
@@ -16,6 +17,7 @@ from ._utils import (
 __all__ = [
     "SentencePieceBaseTokenizer",
     "TikTokenBaseTokenizer",
+    "GPT2BaseTokenizer",
     "ModelTokenizer",
     "BaseTokenizer",
     "tokenize_messages_no_special_tokens",
