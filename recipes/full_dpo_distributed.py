eef228c2d5b9c4ea9cd6f3e853f16114a2693633
Mark Obozov <markobozov@MacBook-Pro-Mark.local>
2025-02-25 22:42:15 +0300

    nit 2

--- recipes/full_dpo_distributed.py
+++ recipes/full_dpo_distributed.py
@@ -876,9 +876,10 @@ class FullDPORecipeDistributed(FTRecipeInterface):
 
             pbar = tqdm(total=self._steps_per_epoch, disable=not (self.rank == 0))
             for idx, batch in enumerate(self._dataloader):
+                
                 if not has_trainable_tokens(
                     labels=batch.get("labels"),
-                    ignore_index=self._loss_fn.ignore_index,
+                    ignore_index=self._loss_fn.ignore_index if hasattr(self._loss_fn, 'ignore_index') else -100,
                 ):
                     continue
 
