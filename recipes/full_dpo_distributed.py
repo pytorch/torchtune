302946a9883a1c3d968fb62c2f10d6c263e3aecc
Mark Obozov <krammnic@yandex.ru>
2025-02-27 02:19:56 +0300

    attempt to fix

--- recipes/full_dpo_distributed.py
+++ recipes/full_dpo_distributed.py
@@ -883,14 +883,20 @@ class FullDPORecipeDistributed(FTRecipeInterface):
                 ):
                     break
                 
-                if not has_trainable_tokens(
+                has_trainable = has_trainable_tokens(
                     labels=batch[1],
                     ignore_index=self._loss_fn.ignore_index
                     if hasattr(self._loss_fn, "ignore_index")
                     else -100,
-                ):
-                    continue
-
+                )
+    
+                mask = torch.tensor(has_trainable, dtype=torch.int, device=self._device)
+                # If 0, then no trainable everywhere
+                torch.distributed.all_reduce(mask, op=torch.distributed.ReduceOp.MIN)
+    
+                if mask.item() == 0:
+                    continue  
+            
                 # batch is input_ids, labels
                 num_tokens += torch.tensor(batch[0].numel())
                 (
