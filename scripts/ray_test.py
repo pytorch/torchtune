892b799928ed7d0e5ea9902153c8796454139f37
Philip Bontrager <pbontrager@gmail.com>
2025-03-04 15:46:56 -0800

    Ray test

--- scripts/ray_test.py
+++ scripts/ray_test.py
@@ -1,4 +1,5 @@
 import os
+import pdb
 
 import ray
 import torch
@@ -9,21 +10,15 @@ import torch.nn as nn
 @ray.remote(num_cpus=8, num_gpus=1)
 class Worker:
     def __init__(self):
-        self.model = nn.Linear(5, 3)
+        self.device = torch.device("cuda")
+        self.model = nn.Linear(5, 3, device="cuda")
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01)
 
-        # device
-        gpu_ids = ray.get_gpu_ids()
-        self.device = torch.device(f"cuda:{gpu_ids[0]}")
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_ids[0])
-        breakpoint()
-        # self.model.to(self.device)
-
-    # async
-    def update(self):
+    async def update(self):
         # Create dummy data
-        inputs = torch.randn(10, 5).to(self.device)
-        labels = torch.randn(10, 3).to(self.device)
+        inputs = torch.randn(10, 5, device=self.device)
+        labels = torch.randn(10, 3, device=self.device)
+
         # Run a single optimizer update
         self.optimizer.zero_grad()
         outputs = self.model(inputs)
@@ -31,12 +26,10 @@ class Worker:
         loss.backward()
         self.optimizer.step()
 
-    # async
-    def get_state_dict(self):
+    async def get_state_dict(self):
         return self.model.state_dict()
 
-    # async
-    def set_state_dict(self, state_dict):
+    async def set_state_dict(self, state_dict):
         self.model.load_state_dict(state_dict)
 
 
@@ -51,8 +44,8 @@ worker2 = Worker.remote()
 ray.get([worker1.update.remote(), worker2.update.remote()])
 
 # # Get the state dictionary from worker 1
-# state_dict = ray.get(worker1.get_state_dict.remote())
+state_dict = ray.get(worker1.get_state_dict.remote())
 
 # # Set the state dictionary on worker 2
-# ray.get(worker2.set_state_dict.remote(state_dict))
+ray.get(worker2.set_state_dict.remote(state_dict))
 ray.shutdown()
