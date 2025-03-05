ecf2e721834b6af1401897f63344c393ee6bd16b
Salman Mohammadi <salman.mohammadi@outlook.com>
2025-03-05 16:55:17 +0000

    adding tests

--- tests/torchtune/models/test_common.py
+++ tests/torchtune/models/test_common.py
@@ -7,9 +7,8 @@
 import pytest
 import torch
 from tests.test_utils import fixed_init_model
-from torchtune.training.seed import set_seed
 from torchtune.models.common import classifier_model
-import torchtune
+from torchtune.training.seed import set_seed
 
 NUM_LAYERS = 4
 NUM_HEADS = 16
@@ -42,11 +41,19 @@ class TestClassifierModelBuilder:
             (1, 256, 256, 1, 110.2561),
         ],
     )
-    def test_forward_component_builders(self, base_model: str, bsz: int, embed_dim: int, seq_len: int, n_classes: int, expected: float):
+    def test_forward_component_builders(
+        self,
+        base_model: str,
+        bsz: int,
+        embed_dim: int,
+        seq_len: int,
+        n_classes: int,
+        expected: float,
+    ):
         inputs = torch.randint(low=0, high=VOCAB_SIZE, size=(bsz, seq_len))
         model = classifier_model(
             num_classes=n_classes,
-            base_model=base_model,
+            base_model_path=base_model,
             vocab_size=VOCAB_SIZE,
             num_layers=n_classes,
             num_heads=NUM_HEADS,
@@ -60,31 +67,3 @@ class TestClassifierModelBuilder:
         expected = torch.tensor(expected)
         assert actual.shape == (bsz, seq_len, n_classes)
         torch.testing.assert_close(actual.mean(), expected, atol=1e-4, rtol=1e-4)
-
-    # @pytest.mark.parametrize(
-    #     "base_model",
-    #     [
-    #         "torchtune.models.llama3_2.llama3_2",
-    #         "torchtune.models.llama3_1.llama3_1",
-    #         "torchtune.models.qwen2.qwen2",
-    #     ],
-    # )
-    # def test_forward_component_builders(self, base_model: str, bsz: int, embed_dim: int, seq_len: int, n_classes: int, expected: float):
-    #     inputs = torch.randint(low=0, high=VOCAB_SIZE, size=(bsz, seq_len))
-    #     model = classifier_model(
-    #         num_classes=n_classes,
-    #         base_model=base_model,
-    #         vocab_size=VOCAB_SIZE,
-    #         num_layers=n_classes,
-    #         num_heads=NUM_HEADS,
-    #         num_kv_heads=NUM_KV_HEADS,
-    #         embed_dim=embed_dim,
-    #         intermediate_dim=INTERMEDIATE_DIM,
-    #         max_seq_len=MAX_SEQ_LEN,
-    #     )
-    #     fixed_init_model(model)
-    #     actual = model(inputs)
-    #     expected = torch.tensor(expected)
-    #     assert actual.shape == (bsz, seq_len, n_classes)
-    #     torch.testing.assert_close(actual.mean(), expected, atol=1e-4, rtol=1e-4)
-
--- torchtune/models/common.py
+++ torchtune/models/common.py
@@ -4,53 +4,37 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Any, Dict
+
 import torch.nn as nn
 from torchtune.config._utils import _get_component_from_path
-from typing import Dict, Any
-import torch
-
-from torchtune.modules.tied_linear import TiedLinear
 
 
-def classifier_model(num_classes: int, base_model: str, **base_model_kwargs: Dict[str, Any]) -> nn.Module:
+def classifier_model(
+    num_classes: int, base_model_path: str, **base_model_kwargs: Dict[str, Any]
+) -> nn.Module:
     """
     Create a classifier model from a base model by adapting the output layer.
 
     Args:
         num_classes (int): The number of classes for the classifier.
         base_model_path (str): The path to the base model.
-        base_model_kwargs (Dict[str, Any]): Keyword arguments for the base model.
+        **base_model_kwargs (Dict[str, Any]): Keyword arguments for the base model.
 
     Returns:
         nn.Module: The classifier model.
 
     Example:
+        >>> from torchtune.models.common import classifier_model
+        >>> model = classifier_model(num_classes=1, base_model_path="torchtune.models.llama3_2.llama3_2_1b")
+
 
     """
-    model = _get_component_from_path(base_model)(**base_model_kwargs)
+    model = _get_component_from_path(base_model_path)(**base_model_kwargs)
 
     if isinstance(model.output, nn.Linear):
         del model.output.weight
         if hasattr(model.output, "bias"):
             del model.output.bias
     model.output = nn.Linear(model.head_dim * model.num_heads, num_classes, bias=False)
-    print(model.output)
-    print(model.output.weight)
-    # import pdb
-    # pdb.set_trace()
-    return model
-    model.output = nn.Linear(model.head_dim * model.num_heads, num_classes, bias=False)
-
-    # if isinstance(model.output, TiedLinear):
-    #     del model.output.tied_module.weight
-    # else:
-    #     del model.output.weight
-    # model.output.register_parameter("weight", nn.Parameter(torch.empty(num_classes, model.head_dim * model.num_heads)))
-    # # init weight with kaiming uniform
-    # if hasattr(model.output, "bias"):
-    #     del model.output.bias
-    #     model.output.register_parameter("bias", nn.Parameter(torch.empty(num_classes)))
-    #     # init bias with zeros
-    # model.output.reset_parameters()
-    # # model.output = nn.Linear(model.head_dim * model.num_heads, num_classes, bias=False)
     return model
