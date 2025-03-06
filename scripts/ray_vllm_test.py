97ff311102ac6e015caf90d2aaf36af9cba9c1ea
joecummings <jrcummings27@gmail.com>
2025-03-06 09:15:57 -0800

    Initial skeleton of recipe

--- scripts/ray_vllm_test.py
+++ scripts/ray_vllm_test.py
@@ -18,7 +18,13 @@ from vllm import LLM, SamplingParams
 from vllm.worker.worker import Worker
 
 
-def stateless_init_process_group(master_address, master_port, rank, world_size, device):
+def stateless_init_process_group(
+    master_address: str,
+    master_port: int,
+    rank: int,
+    world_size: int,
+    device: torch.device,
+):
     """
     vLLM provides `StatelessProcessGroup` to create a process group
     without considering the global process group in torch.distributed.
@@ -29,35 +35,24 @@ def stateless_init_process_group(master_address, master_port, rank, world_size, 
     from vllm.distributed.device_communicators.pynccl import PyNcclCommunicator
     from vllm.distributed.utils import StatelessProcessGroup
 
-    # assert isinstance(master_port, int)
-
     pg = StatelessProcessGroup.create(
-        host=master_address, port=int(master_port), rank=rank, world_size=world_size
+        host=master_address, port=master_port, rank=rank, world_size=world_size
     )
     pynccl = PyNcclCommunicator(pg, device=device)
     return pynccl
 
 
-class MyWorker(Worker):
-    """
-    The `MyWorker` class inherits from `Worker` to provide custom functions.
-    For simplicity, we define the `MyWorker` class in this self-contained
-    script. Normally, we should define the `MyWorker` class in a separate
-    file and pass the qualified name of the class to the `worker_cls`
-    parameter.
-    """
+class vLLMRayWorker(Worker):
+    """vLLM worker for Ray."""
 
     def init_weight_update_group(self, master_address, master_port, rank, world_size):
         from vllm.distributed.parallel_state import get_world_group
 
-        print(f"{get_world_group().rank=}, {self.device=}")
-        # rank = get_world_group().rank + rank_offset
         self.model_update_group = stateless_init_process_group(
             master_address,
             master_port,
             rank,
             world_size,
-            # "nccl",
             self.device,
         )
 
@@ -66,19 +61,8 @@ class MyWorker(Worker):
         self.model_update_group.broadcast(
             weight, src=1, stream=torch.cuda.current_stream()
         )
-
         self.model_runner.model.load_weights(weights=[(name, weight)])
-
         del weight
-
-    def check_weights_changed(self):
-        """
-        Check if the weights are updated to 0.
-        """
-        weights_updated = True
-        for name, p in self.model_runner.model.named_parameters():
-            weights_updated = weights_updated and torch.allclose(p, torch.zeros_like(p))
-        return weights_updated
 
 
 # Define the worker class
@@ -113,13 +97,13 @@ class TrainWorker:
 
     def zero_weights(self):
         for name, p in self.model.named_parameters():
-            #.data to prevent leaf Variable in-place modification RuntimeError
+            # .data to prevent leaf Variable in-place modification RuntimeError
             p.data.zero_()
         return True
 
     def get_state_dict(self):
         return self.model.state_dict()
-    
+
     def init_model_update_group(self, master_address, master_port, rank, world_size):
         self.model_update_group = stateless_init_process_group(
             master_address,
@@ -129,10 +113,10 @@ class TrainWorker:
             # FIXME: hardcoding, not sure if this is right
             torch.device(f"cuda:0"),
         )
-    
+
     def get_metadata_for_broadcast(self):
-        vllm_format_sd = self.new_sd  
-        new_sd = {}  
+        vllm_format_sd = self.new_sd
+        new_sd = {}
         for k, v in vllm_format_sd.items():
             new_sd[k] = (v.shape, v.dtype)
         return new_sd
@@ -142,13 +126,17 @@ class TrainWorker:
         for i, (k, v) in enumerate(self.model.state_dict().items()):
             new_sd[k] = v.full_tensor()
             if i == 0:
-                print(f"DTensor.local shape {v._local_tensor.shape}, DTensor.full_tensor shape {new_sd[k].shape}")
+                print(
+                    f"DTensor.local shape {v._local_tensor.shape}, DTensor.full_tensor shape {new_sd[k].shape}"
+                )
         new_sd = qwen2_tune_to_hf(new_sd, num_heads=16, num_kv_heads=2, dim=2048)
         # FIXME: is this sus
         self.new_sd = new_sd
-    
+
     def broadcast_key_to_vllm(self, key):
-        self.model_update_group.broadcast(self.new_sd[key], src=1, stream=torch.cuda.current_stream())
+        self.model_update_group.broadcast(
+            self.new_sd[key], src=1, stream=torch.cuda.current_stream()
+        )
 
     def get_rank(self):
         gpu_ids = ray.get_gpu_ids()
@@ -213,71 +201,71 @@ ray.init(num_cpus=192, num_gpus=6)
 
 # ====== init vllm ==========
 
-vllm_tp_size = 1
+# vllm_tp_size = 1
 
-pg_inference = placement_group([{"GPU": 1, "CPU": 0}] * 1)
-ray.get(pg_inference.ready())
-scheduling_inference = PlacementGroupSchedulingStrategy(
-    placement_group=pg_inference,
-    placement_group_capture_child_tasks=True,
-    placement_group_bundle_index=0,
-)
+# pg_inference = placement_group([{"GPU": 1, "CPU": 0}] * vllm_tp_size)
+# ray.get(pg_inference.ready())
+# scheduling_inference = PlacementGroupSchedulingStrategy(
+#     placement_group=pg_inference,
+#     placement_group_capture_child_tasks=True,
+#     placement_group_bundle_index=0,
+# )
 
-llm = ray.remote(
-    num_cpus=0,
-    num_gpus=0,
-    scheduling_strategy=scheduling_inference,
-)(LLM).remote(
-    model="Qwen/Qwen2.5-3B",
-    enforce_eager=True,
-    worker_cls=MyWorker,
-    tensor_parallel_size=vllm_tp_size,
-    distributed_executor_backend="ray",
-)
+# llm = ray.remote(
+#     num_cpus=0,
+#     num_gpus=0,
+#     scheduling_strategy=scheduling_inference,
+# )(LLM).remote(
+#     model="Qwen/Qwen2.5-3B",
+#     enforce_eager=True,
+#     worker_cls=MyWorker,
+#     tensor_parallel_size=vllm_tp_size,
+#     distributed_executor_backend="ray",
+# )
 
 # Generate texts from the prompts.
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
+# prompts = [
+#     "Hello, my name is",
+#     "The president of the United States is",
+#     "The capital of France is",
+#     "The future of AI is",
+# ]
 
-sampling_params = SamplingParams(temperature=0)
+# sampling_params = SamplingParams(temperature=0)
 
-outputs = ray.get(llm.generate.remote(prompts, sampling_params))
+# outputs = ray.get(llm.generate.remote(prompts, sampling_params))
 
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, " f"Generated text: {generated_text!r}")
+# for output in outputs:
+#     prompt = output.prompt
+#     generated_text = output.outputs[0].text
+#     print(f"Prompt: {prompt!r}, " f"Generated text: {generated_text!r}")
 
 # === init fsdp ====
 
-fsdp_world_size = 4
+# fsdp_world_size = 4
 
 
-def _get_node_ip():
-    def get_node_ip_by_sdk():
-        return ray._private.services.get_node_ip_address()
+# def _get_node_ip():
+#     def get_node_ip_by_sdk():
+#         return ray._private.services.get_node_ip_address()
 
-    host_ipv4 = os.getenv("MY_HOST_IP", None)
-    host_ipv6 = os.getenv("MY_HOST_IPV6", None)
-    host_ip_by_env = host_ipv4 or host_ipv6
-    host_ip_by_sdk = get_node_ip_by_sdk()
+#     host_ipv4 = os.getenv("MY_HOST_IP", None)
+#     host_ipv6 = os.getenv("MY_HOST_IPV6", None)
+#     host_ip_by_env = host_ipv4 or host_ipv6
+#     host_ip_by_sdk = get_node_ip_by_sdk()
 
-    host_ip = host_ip_by_env or host_ip_by_sdk
-    return host_ip
-
-
-def _get_free_port():
-    with socket.socket() as sock:
-        sock.bind(("", 0))
-        return sock.getsockname()[1]
+#     host_ip = host_ip_by_env or host_ip_by_sdk
+#     return host_ip
 
 
-def get_available_master_addr_port():
-    return _get_node_ip(), str(_get_free_port())
+# def _get_free_port():
+#     with socket.socket() as sock:
+#         sock.bind(("", 0))
+#         return sock.getsockname()[1]
+
+
+# def get_available_master_addr_port():
+#     return _get_node_ip(), str(_get_free_port())
 
 
 master_address, master_port = get_available_master_addr_port()
@@ -312,7 +300,7 @@ chosen_fsdp_master_rank = fsdp_workers[fsdp_nodes[0]]
 
 
 # ensure there is not process group initialized in the main process
-assert not torch.distributed.is_initialized() 
+assert not torch.distributed.is_initialized()
 
 # === init stateless process group for comms between vllm and fsdp workers ===
 from vllm.utils import get_ip, get_open_port
@@ -327,10 +315,7 @@ handle = llm.collective_rpc.remote(
 )
 
 handle2 = chosen_fsdp_master_rank.init_model_update_group.remote(
-    weight_update_address,
-    weight_update_port,
-    vllm_tp_size,
-    vllm_tp_size + 1
+    weight_update_address, weight_update_port, vllm_tp_size, vllm_tp_size + 1
 )
 
 # only need to .get one of the two handles since this will block until all
@@ -361,9 +346,7 @@ print("got metadata")
 
 # do broadcast key by key
 for name, (shape, dtype) in metadata.items():
-    handle = llm.collective_rpc.remote(
-            "update_weight", args=(name, dtype, shape)
-        )
+    handle = llm.collective_rpc.remote("update_weight", args=(name, dtype, shape))
     chosen_fsdp_master_rank.broadcast_key_to_vllm.remote(name)
     ray.get(handle)
     print(f"updated {name=}")
@@ -380,3 +363,146 @@ for output in outputs:
     print(f"Prompt: {prompt!r}, " f"Generated text: {generated_text!r}")
 
 ray.shutdown()
+
+
+class RayGRPORecipe:
+    def __init__(self, cfg):
+        self.ray_resources = cfg.ray_resources
+        self.vllm_tensor_parallel_size = cfg.vllm_tensor_parallel_size
+        self.fsdp_world_size = cfg.fsdp_world_size
+
+    def start_ray(self):
+        ray.init(
+            num_cpus=self.ray_resources.num_cpus, num_gpus=self.ray_resources.num_gpus
+        )
+
+    def setup(self):
+        # ---- Create models ---- #
+        self.vllm_model = _create_vllm_rollout(
+            tensor_parallel_size=self.tensor_parallel_size
+        )
+        addr = get_ip()
+        train_workers_port = get_open_port()
+        self.pytorch_train_workers = _create_train_workers(
+            world_size=self.fsdp_world_size, addr=addr, port=train_workers_port
+        )
+        # ---- Create PG to sync weights to rollout model ---- #
+        # Ensure there is not process group initialized in the main process
+        assert not torch.distributed.is_initialized()
+        weight_update_port = get_open_port()
+        self.vllm_model.collective_rpc.remote(
+            "init_weight_update_group",
+            args=(addr, weight_update_port, 0, self.vllm_tensor_parallel_size + 1),
+        )
+        self.pytorch_train_workers[0].init_model_update_group.remote(
+            addr,
+            weight_update_port,
+            self.vllm_tensor_parallel_size,
+            self.fsdp_world_size,
+        )
+        ray.get(handle)  # Wait for the weight update group to be initialized
+        # ---- Create dataloader ---- #
+        collate_fn = partial(
+            _get_component_from_path(cfg.collate_fn),
+            padding_idx=self._tokenizer.pad_id,
+        )
+        self.dataloader = _create_dataloader(
+            collate_fn,
+            batch_size=cfg.batch_size,
+        )
+        # ---- Create optimizer ---- #
+
+    def _create_vllm_rollout(tensor_parallel_size: int):
+        # Create placement group (still kinda need to figure out what this does)
+        pg_inference = placement_group([{"GPU": 1, "CPU": 0}] * tensor_parallel_size)
+        ray.get(pg_inference.ready())
+        scheduling_inference = PlacementGroupSchedulingStrategy(
+            placement_group=pg_inference,
+            placement_group_capture_child_tasks=True,
+            placement_group_bundle_index=0,
+        )
+        # Initialize the remote model
+        llm = ray.remote(
+            num_cpus=0,
+            num_gpus=0,
+            scheduling_strategy=scheduling_inference,
+        )(LLM).remote(
+            model="Qwen/Qwen2.5-3B",
+            enforce_eager=True,
+            worker_cls=vLLMRayWorker,
+            tensor_parallel_size=tensor_parallel_size,
+            distributed_executor_backend="ray",
+        )
+        return llm
+
+    def _create_train_workers(
+        world_size: int, addr: str, port: int
+    ) -> List[PyTorchTrainWorker]:
+        workers = []
+        for i in range(world_size):
+            env_vars = {
+                "WORLD_SIZE": str(world_size),
+                "RANK": str(i),
+                "WG_BACKEND": "ray",
+                "MASTER_ADDR": addr,
+                "MASTER_PORT": str(port),
+            }
+            worker = PyTorchTrainWorker.remote(env_vars)
+            workers.append(worker)
+        return workers
+
+    def _create_dataloader(collate_fn, batch_size, ds, shuffle, rank, world_size):
+        sampler = StatefulDistributedSampler(
+            ds,
+            num_replicas=self.world_size,
+            rank=self.rank,
+            shuffle=shuffle,
+            seed=self.seed,
+        )
+        dataloader = StatefulDataLoader(
+            dataset=ds,
+            batch_size=batch_size,
+            sampler=sampler,
+            collate_fn=(),
+            # dropping last avoids shape issues with compile + flex attention
+            drop_last=True,
+        )
+        return dataloader
+
+    def _sync_weights():
+        handles = []
+        for worker in fsdp_workers:
+            handle = worker.all_gather.remote()
+            handles.append(handle)
+
+        [ray.get(handle) for handle in handles]
+        print("done all gather")
+
+        # get metadata for broadcast
+        metadata = ray.get(chosen_fsdp_master_rank.get_metadata_for_broadcast.remote())
+
+        # do broadcast key by key
+        for name, (shape, dtype) in metadata.items():
+            handle = llm.collective_rpc.remote(
+                "update_weight", args=(name, dtype, shape)
+            )
+            chosen_fsdp_master_rank.broadcast_key_to_vllm.remote(name)
+            ray.get(handle)
+
+    def stop_ray(self):
+        ray.shutdown()
+
+
+@config.parse
+def recipe_main(cfg: DictConfig) -> None:
+    recipe = RayGRPORecipe(cfg=cfg)
+    config.log_config(recipe_name="RayGRPORecipe", cfg=cfg)
+    recipe.start_ray()
+    recipe.setup()
+    recipe.train()
+    recipe.cleanup()
+    recipe.stop_ray()
+
+
+if __name__ == "__main__":
+    sys.exit(recipe_main())
