bc17a3e4edb14a19fe9e9fc8e80080d7e822c34c
Maxime <672982+maximegmd@users.noreply.github.com>
2025-03-10 13:55:02 +0100

    Ignore flux encode/forward on MPS

--- tests/torchtune/models/flux/test_flux_autoencoder.py
+++ tests/torchtune/models/flux/test_flux_autoencoder.py
@@ -9,6 +9,7 @@ import torch
 
 from torchtune.models.flux import flux_1_autoencoder
 from torchtune.training.seed import set_seed
+from tests.test_utils import mps_ignored_test
 
 BSZ = 32
 CH_IN = 3
@@ -51,6 +52,7 @@ class TestFluxAutoencoder:
     def z(self):
         return torch.randn(BSZ, CH_Z, RES_Z, RES_Z)
 
+    @mps_ignored_test()
     def test_forward(self, model, img):
         actual = model(img)
         assert actual.shape == (BSZ, CH_IN, RESOLUTION, RESOLUTION)
@@ -64,6 +66,7 @@ class TestFluxAutoencoder:
         loss = y.mean()
         loss.backward()
 
+    @mps_ignored_test()
     def test_encode(self, model, img):
         actual = model.encode(img)
         assert actual.shape == (BSZ, CH_Z, RES_Z, RES_Z)
