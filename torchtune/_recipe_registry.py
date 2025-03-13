c3e42da5afcd63b29eda4186dece4f17a9ceabd7
Philip Bontrager <pbontrager@gmail.com>
2025-03-13 08:42:07 -0700

    phi4 config and style fixes

--- torchtune/_recipe_registry.py
+++ torchtune/_recipe_registry.py
@@ -523,6 +523,10 @@ _ALL_RECIPES = [
                 file_path="gemma/evaluation.yaml",
             ),
             Config(
+                name="phi4/evaluation",
+                file_path="phi4/evaluation.yaml",
+            ),
+            Config(
                 name="phi3/evaluation",
                 file_path="phi3/evaluation.yaml",
             ),
