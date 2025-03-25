09aeae3708c76acd2aa29923d7868bb7ee52d014
Kailash Gogineni <kailash.gogineni@amd.com>
2025-03-24 17:08:13 -0700

    Update and rename run_llama_3_2.sh to run_llama_3_2_1B_full.sh

--- run_llama_3_2_1B_full.sh
+++ run_llama_3_2_1B_full.sh
@@ -4,8 +4,8 @@
 # sample usage: COMPILE=False MAX_STEPS=10 EPOCHS=1 SAVE_WEIGHTS=False bash aramco.sh
 # compile is currently known to be broken in our environment.
 
-CONFIG="${CONFIG:-/workspace/torchtune/recipes/configs/llama3_3/70B_full_alpaca.yaml}"
-MODEL_DIR="${MODEL_DIR:-./models/Llama-3.3-70B-Instruct}"
+CONFIG="${CONFIG:-/workspace/torchtune/recipes/configs/llama3_2/1B_full.yaml}"
+MODEL_DIR="${MODEL_DIR:-./models/Llama-3.2-1B-Instruct}"
 CHECKPOINT_DIR="${CHECKPOINT_DIR:-./checkpoints}"
 
 PACKED="${PACKED:-False}"
@@ -35,9 +35,9 @@ if [ ! -f "$CONFIG" ]; then
 fi
 
 if [ ! -d $MODEL_DIR ]; then
-    echo "70B model not found in $MODEL_DIR" >&2
+    echo "1B model not found in $MODEL_DIR" >&2
     echo "Download with: "
-    echo "  huggingface-cli download meta-llama/Llama-3.1-70B-Instruct --local-dir $MODEL_DIR --exclude 'original/*.pth'"
+    echo "  huggingface-cli download meta-llama/Llama-3.2-1B-Instruct --local-dir $MODEL_DIR --exclude 'original/*.pth'"
     exit 1
 fi
 
