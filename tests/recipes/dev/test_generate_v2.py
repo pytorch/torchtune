84aea68d90ff5f8e65af33fd004e7b84424bc712
Felipe Mello <fmellomascarenhas@gmail.com>
2025-04-04 09:23:05 -0400

    Replace exponential with torch.log in generation sampling (#2549)
    Co-authored-by: Felipe Mello <felipemello@fb.com>

--- tests/recipes/dev/test_generate_v2.py
+++ tests/recipes/dev/test_generate_v2.py
@@ -55,7 +55,9 @@ class TestGenerateV2:
         # this is gibberish b/c the model is random weights, but it's
         # the expected value for what we currently have in V2
         # this test should catch any changes to the generate recipe that affect output
-        expected_output = "Pietroместkap щotimes rivers cache НиtringindexPathNAME"
+        expected_output = (
+            "restricted chap Пав Frenchisto porqueноello ignor ambientQuery"
+        )
 
         logs = caplog.text
         assert expected_output in logs
