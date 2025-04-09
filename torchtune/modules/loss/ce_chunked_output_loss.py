1d4a0dd066c3e21a9bb4d2e170a8790d2796ed11
Bogdan Salyp <bogdansalyp@gmail.com>
2025-04-09 18:29:27 +0000

    fix: Typo

--- torchtune/modules/loss/ce_chunked_output_loss.py
+++ torchtune/modules/loss/ce_chunked_output_loss.py
@@ -57,7 +57,7 @@ class CEWithChunkedOutputLoss(torch.nn.Module):
             >>> loss_fn = ChunkedCrossEntropyLoss()
             >>>
             >>> h = torch.tensor([bsz, num_tokens, dim])
-            >>> output_chunks = [model.output(chunk) for chunk in h.tensor_split(hum_chunks, dim=1)]
+            >>> output_chunks = [model.output(chunk) for chunk in h.tensor_split(num_chunks, dim=1)]
             >>>
             >>> labels = torch.tensor([bsz, num_tokens])
             >>> loss = loss_fn(output_chunks, labels)
