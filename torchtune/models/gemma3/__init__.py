88a10fd7c6cf933166defe3672d6500ef5f54c34
Mark Obozov <markobozov@MacBook-Pro-Mark-2.local>
2025-04-16 16:46:37 +0300

    __init__.py

--- torchtune/models/gemma3/__init__.py
+++ torchtune/models/gemma3/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ._component_builders import gemma3, lora_gemma3  # noqa
+from ._component_builders import gemma3, lora_gemma3, qlora_gemma3  # noqa
 from ._model_builders import (  # noqa  # noqa
     gemma3_12b,
     gemma3_1b,
@@ -14,4 +14,26 @@ from ._model_builders import (  # noqa  # noqa
     lora_gemma3_1b,
     lora_gemma3_27b,
     lora_gemma3_4b,
+    qlora_gemma3_12b,
+    qlora_gemma3_1b,
+    qlora_gemma3_27b,
+    qlora_gemma3_4b
 )
+__all__ = [
+    "gemma3",
+    "lora_gemma3",
+    "qlora_gemma3",
+    "gemma3_12b",
+    "gemma3_1b",
+    "gemma3_27b",
+    "gemma3_4b",
+    "lora_gemma3_12b",
+    "lora_gemma3_1b",
+    "lora_gemma3_27b",
+    "lora_gemma3_4b",
+    "qlora_gemma3_12b",
+    "qlora_gemma3_1b",
+    "qlora_gemma3_27b",
+    "qlora_gemma3_4b",
+]
+
