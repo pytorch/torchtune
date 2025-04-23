d5d229508f440ce5f252806f812f7cda0332ac11
joecummings <jrcummings27@gmail.com>
2025-04-23 13:23:19 -0700

    Raise NotImplementedError instead

--- torchtune/dev/rl/workers/datacollectors/sync.py
+++ torchtune/dev/rl/workers/datacollectors/sync.py
@@ -369,11 +369,7 @@ class SyncLLMCollector(SyncDataCollector):
             drop_last=True,
         )
         if dataloader_state_dict is not None:
-            raise AssertionError("Haven't handled dataloader_state_dict yet")
-            dataloader.load_state_dict(dataloader_state_dict)
-            # B/c we currently only save at epoch boundaries, if we cut the previous epoch short
-            # we need to force the dataloader to finish the last iteration before it's actually used
-            list(dataloader)
+            raise NotImplementedError
         return dataloader
 
 
