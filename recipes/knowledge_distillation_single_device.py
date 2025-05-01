43e16b84e877297924e2382147318d29d1161d87
Ankita George <ankitageorge@meta.com>
2025-05-01 12:13:44 -0700

    squashed commit

--- recipes/knowledge_distillation_single_device.py
+++ recipes/knowledge_distillation_single_device.py
@@ -11,6 +11,8 @@ from functools import partial
 from typing import Any, Dict, Optional, Union
 from warnings import warn
 
+import omegaconf
+
 import torch
 import torchtune.modules.common_utils as common_utils
 from omegaconf import DictConfig, ListConfig, OmegaConf
