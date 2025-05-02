89713e366cd665b43a5135494dcce8ca43d2ba0e
Evan Smothers <ebs@fb.com>
2025-05-01 21:01:03 -0700

    add back missing test ckpt file

--- tests/cache_artifacts.sh
+++ tests/cache_artifacts.sh
@@ -19,6 +19,7 @@ SMALL_MODEL_URLS=(
     "https://ossci-datasets.s3.amazonaws.com/torchtune/small-ckpt-tune-llama3-05052024.pt"
     "https://ossci-datasets.s3.amazonaws.com/torchtune/small-ckpt-hf-reward-07122024.pt"
     "https://ossci-datasets.s3.amazonaws.com/torchtune/small-ckpt-meta-vision-10172024.pt"
+    "https://ossci-datasets.s3.amazonaws.com/torchtune/small-ckpt-hf-vision-10172024.pt"
     "https://ossci-datasets.s3.amazonaws.com/torchtune/llama3-hf-04232025/config.json"
     "https://ossci-datasets.s3.amazonaws.com/torchtune/llama3-hf-04232025/generation_config.json"
     "https://ossci-datasets.s3.amazonaws.com/torchtune/llama3-hf-04232025/model.safetensors"
