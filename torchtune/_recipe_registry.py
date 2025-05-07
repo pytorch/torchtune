d8b57367e8ddbeadfd2f8be9722960c91429302a
pytorchbot <pytorchbot@pytorch.com>
2025-05-07 11:35:49 +0000

    2025-05-07 nightly release (a6cefb49ecb12014c2169f26362d9d6abc736563)

--- torchtune/_recipe_registry.py
+++ torchtune/_recipe_registry.py
@@ -586,6 +586,8 @@ _ALL_RECIPES = [
         configs=[
             Config(name="llama2/7B_qat_full", file_path="llama2/7B_qat_full.yaml"),
             Config(name="llama3/8B_qat_full", file_path="llama3/8B_qat_full.yaml"),
+            Config(name="llama3_1/8B_qat_full", file_path="llama3_1/8B_qat_full.yaml"),
+            Config(name="llama3_2/3B_qat_full", file_path="llama3_2/3B_qat_full.yaml"),
         ],
         supports_distributed=True,
     ),
