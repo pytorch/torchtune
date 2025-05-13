0b2cfeca6cedba6efb7389d1cb18c7e17574f355
Philip Bontrager <pbontrager@gmail.com>
2025-05-13 10:33:21 -0700

    ray updates

--- recipes/dev/full_async_grpo_distributed.py
+++ recipes/dev/full_async_grpo_distributed.py
@@ -4,70 +4,94 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import ray
+import functools
+from OmegaConf import DictConfig
+
+from torchtune import config
+from torchtune.dev.rl.ray import (
+    launch_actors,
+    update_state_dict_from_store,
+    update_store_from_state_dict,
+    get_kvstore,
+    init_ray,
+    get_actors,
+)
+
 # TODO: step by step walkthrough of vllm
 @ray.remote
 class GRPOGeneratorDistributed():
-	# Data collector
-	# envs from configs (include model transform)
-	# policy
-	# reward workers?
+    # Data collector
+    # envs from configs (include model transform)
+    # policy
+    # reward workers?
 
-	def prefetch(self, state_dict):
-	# check version of all keys, if len(version) == 1 and version != self.version then fetch
-	# new_weights = {k: self.store(k) for k,v in state_dict} # this owned by generator, can't be invalidated
-	# self.model.sync_weights
+    def prefetch(self, state_dict):
+    # check version of all keys, if len(version) == 1 and version != self.version then fetch
+    # new_weights = {k: self.store(k) for k,v in state_dict} # this owned by generator, can't be invalidated
+    # self.model.sync_weights
 
 
 
 # TODO: step by step walkthrough of trainer worker
 @ray.remote
 class GRPOTrainerDistributed:
-	# match SFTDistributed except:
-	# init -> ParameterStore
-	# setup_data -> ReplayBuffer
-	# train -> offload weights
-	# logging
+    # match SFTDistributed except:
+    # init -> ParameterStore
+    # setup_data -> ReplayBuffer
+    # train -> offload weights
+    # logging
 
-	def setup(self)
-		# add store actor
-		# find metric logger line
-		# find setup data
-		# add logging method
+    def setup(self)
+        # add store actor
+        # find metric logger line
+        # find setup data
+        # add logging method
 
-    def opt_step(self):
-    	# self.version
-    	if self.offload_handle is not None:
-    		await self.offload_handle
-    	self.opt.step()
-    	self.offload_handle = async offload_state_dict(self.model.state_dict())
-    	# offload + convert state dict
-    	# move to store
+    def sync_weights(self, model, step):
+        state_dict = {}
+        for k,v in model.state_dict():
+            # async offload to cpu
+            cpu_weights = torch.cuda.Stream()  # Create a stream
+            with torch.no_grad():
+                async_copy = v.to('cpu', non_blocking=True, stream=cpu_weights)
+                state_dict[k] = async_copy
+        # figure out how to have update_store_from_state_dict work with the streams
+        return update_store_from_state_dict(self.kvstore, state_dict, step)
+
+
+    def opt_step(self, step):
+        # lost of half broken code here
+        if self.offload_handle is not None:
+            await self.sync_handles
+        self.opt.step()
+        self.sync_handles = async sync_weights(self.model, step)
 
     def train(...):
 
-    	for ... in self.replay_buffer:
-    		...
-    		loss = self.loss_step(...)
-    		loss.backward()
-    		...
+        for ... in self.replay_buffer:
+            ...
+            loss = self.loss_step(...)
+            loss.backward()
+            ...
 
 
-    		self.opt.step()
+            self.opt.step(step)
 
 # TODO
 # show train offload -> save to store
 #   - only save to store if vllm updated
 #   - don't call opt_step while still offloading
 #   - start offload -> move to store -> store marks new version (per offset) (does store propagate copies to past copies or invalidate?) -> block update unitl offloaded and no new offload until vllm pulls
-# 		- do you want two copies to be able to exist if vllm and trainer are on the same machine (i.e. read always creates a copy (write location and read location, reads get invalidated with new write))
+#       - do you want two copies to be able to exist if vllm and trainer are on the same machine (i.e. read always creates a copy (write location and read location, reads get invalidated with new write))
 #
 # actors get other actors from ray.state
 
 
 def launch_replay_buffer(cfg):
-	ray_buffer = ray.remote(**cfg.replay_buffer.orchestration)(ReplayBuffer)
+    ray_buffer = ray.remote(**cfg.replay_buffer.orchestration)(ReplayBuffer)
 
-	storage = functools.partial(LazyStackStorage, max_size=cfg.replay_buffer.size)
+    storage = functools.partial(LazyStackStorage, max_size=cfg.replay_buffer.size)
     batch_size = cfg.training.batch_size
     # bs and collate_fn should come from inside trainer
     # ideal setting: ReplayBuffer exists in trainer, storage on object store for other actors to update
@@ -75,9 +99,9 @@ def launch_replay_buffer(cfg):
 
 
 def launch_metric_logger(cfg):
-	logger_cls = cfg.metric_logger.pop("_component_")
-	ray_logger = ray.remote(**cfg.metric_logger.orchestration)(logger_cls)
-	return ray_logger.options(name="MetricLogger").remote(**cfg.metric_logger)
+    logger_cls = cfg.metric_logger.pop("_component_")
+    ray_logger = ray.remote(**cfg.metric_logger.orchestration)(logger_cls)
+    return ray_logger.options(name="MetricLogger").remote(**cfg.metric_logger)
 
 
 @config.parse
@@ -97,15 +121,15 @@ def recipe_main(cfg: DictConfig) -> None:
     collectors = launch_actors(GRPOGeneratorDistributed, base_cfg + cfg.collector, **cfg.collector.resources)
     postprocessing = []
     for proccess_cfg in cfg.postprocessing:
-    	processes = launch_actors(RewardActor, base_cfg + process_cfg, **proccess_cfg.resources)
-    	postprocessing.append(processes)
+        processes = launch_actors(RewardActor, base_cfg + process_cfg, **proccess_cfg.resources)
+        postprocessing.append(processes)
     trainers = launch_actors(GRPOTrainerDistributed, base_cfg + cfg.training, **cfg.training.resources)
 
-    buffer = launch_replay_buffer(cfg)
-    metric_logger = launch_metric_logger(cfg)
-    post_process_queue = Queue() # can this be moved to the data collector?
+    launch_replay_buffer(cfg)
+    launch_metric_logger(cfg)
+    ray.util.queue.Queue(actor_options={"name": "PostprocessQueue"})
 
-	collector_handles = [collector.run.remote() for collector in collectors]
+    collector_handles = [collector.run.remote() for collector in collectors]
     process_handles = [process.run.remote() for process in postprocessing]
     trainer_handles = [trainer.train.remote() for trainer in trainers]
     ray.get(trainer_handles)
@@ -115,5 +139,5 @@ def recipe_main(cfg: DictConfig) -> None:
     ray.shutdown()
 
 
-if __name__ == "__main__":
+    if __name__ == "__main__":
     sys.exit(recipe_main())
