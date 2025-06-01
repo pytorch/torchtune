dc6bc469a6126792caac005cfbffbc4382db801d
Vasist10 <155972527+Vasist10@users.noreply.github.com>
2025-06-01 10:34:02 +0530

    Improve torch.compile log message in compile_model and add unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,9 +18,11 @@ import pytest
 
 import torch
 from torch import nn
+import logging
 from torchtune.data import Message, PromptTemplate, truncate
 from torchtune.modules.transforms import Transform
 from torchtune.modules.transforms.tokenizers import ModelTokenizer
+from torchtune.training._compile import compile_model
 
 
 CKPT_MODEL_PATHS = {
@@ -385,4 +387,23 @@ def rl_test() -> bool:
     return pytest.mark.skipif(
         "not config.getoption('--run-rl-tests')",
         reason="Only run when --run-rl-tests is given",
+    )
+
+class DummyTransformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.Linear(10, 10)
+
+    def modules(self):
+        return [self.layer]
+
+def test_compile_model_logs_message(caplog):
+    model = DummyTransformer()
+
+    with caplog.at_level(logging.INFO):
+        compile_model(model, verbose=True)
+
+    assert any(
+        "Compiling model layers with torch.compile. Expect a relatively slower first step."
+        in message for message in caplog.messages
     )
