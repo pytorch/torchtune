371bb0bc8c62a8a000838f25a528de1fd12db0e7
John Glover <j@johnglover.net>
2025-07-01 00:04:16 +0100

    Fix bug in Gemma2 HuggingFace weight mapping (#2853)

--- torchtune/models/gemma2/_convert_weights.py
+++ torchtune/models/gemma2/_convert_weights.py
@@ -28,8 +28,8 @@ _GEMMA2_FROM_HF = {
     "model.layers.{}.mlp.down_proj.weight": "layers.{}.mlp.w2.weight",
     "model.layers.{}.input_layernorm.weight": "layers.{}.sa_norm.scale",
     "model.layers.{}.post_attention_layernorm.weight": "layers.{}.sa_scale.scale",
-    "model.layers.{}.post_feedforward_layernorm.weight": "layers.{}.mlp_norm.scale",
-    "model.layers.{}.pre_feedforward_layernorm.weight": "layers.{}.mlp_scale.scale",
+    "model.layers.{}.pre_feedforward_layernorm.weight": "layers.{}.mlp_norm.scale",
+    "model.layers.{}.post_feedforward_layernorm.weight": "layers.{}.mlp_scale.scale",
     "model.norm.weight": "norm.rms_norm.scale",
     "lm_head.weight": "output.weight",
 }
