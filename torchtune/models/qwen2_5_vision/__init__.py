a8b00df47ef5e2ff3baddf6087ba149a0f72acc9
Albert <albert@inflection.ai>
2025-07-02 18:40:57 +0000

    custom collation + init edits

--- torchtune/models/qwen2_5_vision/__init__.py
+++ torchtune/models/qwen2_5_vision/__init__.py
@@ -1,6 +1,7 @@
 from ._model_builders import (
-    qwen2_5_vl_7b, 
-    qwen2_5_vl_transform 
+    qwen2_5_vl_72b,
+    qwen2_5_vl_7b,
+    qwen2_5_vl_3b,
 )
 
 from ._component_builders import (
@@ -13,16 +14,20 @@ from ._positional_embeddings import (
     Qwen2_5_VisionRotaryEmbedding,
 )
 
+from ._transform import Qwen2_5_VLTransform
+from ._collate import qwen2_5_vl_padded_collate_images
+
 from ._convert_weights import qwen2_5_vl_hf_to_tune
 
 __all__ = [
-    "qwen2_5_vl_7b",
-    "qwen2_5_vl_transform",
-    "Qwen2_5_VLTransform",
     "qwen2_5_vl_text_decoder",
     "qwen2_5_vision_encoder",
+    "qwen2_5_vl_72b",
+    "qwen2_5_vl_7b",
+    "qwen2_5_vl_3b",
     "Qwen25VLRotaryPositionalEmbeddings",
     "Qwen2_5_VisionRotaryEmbedding",
     "Qwen2_5_VLTransform",
+    "qwen2_5_vl_padded_collate_images",
     "qwen2_5_vl_hf_to_tune",
 ]
