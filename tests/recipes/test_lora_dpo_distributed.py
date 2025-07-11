0bcb5e1ee3602c8c221ff3b013ac9869bba3f020
Bogdan Salyp <bogdansalyp@gmail.com>
2025-07-11 10:51:52 -0700

    fix: Import statements

--- tests/recipes/test_lora_dpo_distributed.py
+++ tests/recipes/test_lora_dpo_distributed.py
@@ -13,6 +13,17 @@ import pytest
 import torch
 from omegaconf import OmegaConf
 from tests.common import TUNE_PATH
+from tests.recipes.utils import (
+    dummy_stack_exchange_dataset_config,
+    MODEL_TEST_CONFIGS,
+    write_hf_ckpt_config,
+)
+from tests.test_utils import (
+    CKPT_MODEL_PATHS,
+    gen_log_file_name,
+    get_loss_values_from_metric_logger,
+    gpu_test,
+)
 from tests.recipes.utils import dummy_stack_exchange_dataset_config, MODEL_TEST_CONFIGS
 from tests.test_utils import CKPT_MODEL_PATHS, gpu_test
 from torchtune import config
