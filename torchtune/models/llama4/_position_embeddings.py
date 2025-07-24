4bb03ed1a42da702ce7204eefee3d68a649b23de
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-07-24 11:39:42 -0700

    [compile] workaround for compile error FakeTensor no op for builtin.-
    [ghstack-poisoned]

--- torchtune/models/llama4/_position_embeddings.py
+++ torchtune/models/llama4/_position_embeddings.py
@@ -179,10 +179,14 @@ class Llama4ScaledRoPE(nn.Module):
         # tensor has shape [b, s, n_h, h_d // 2, 2]
         x_out = torch.stack(
             [
-                xshaped[..., 0] * rope_cache[..., 0]
-                - xshaped[..., 1] * rope_cache[..., 1],
-                xshaped[..., 1] * rope_cache[..., 0]
-                + xshaped[..., 0] * rope_cache[..., 1],
+                torch.sub(
+                    xshaped[..., 0] * rope_cache[..., 0],
+                    xshaped[..., 1] * rope_cache[..., 1],
+                ),
+                torch.add(
+                    xshaped[..., 1] * rope_cache[..., 0],
+                    xshaped[..., 0] * rope_cache[..., 1],
+                ),
             ],
             -1,
         )
