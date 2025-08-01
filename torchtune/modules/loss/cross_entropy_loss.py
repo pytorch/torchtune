b22a3ae68c9e254e7ec6afb6ae64b1b32e0f0cbe
ebsmothers <ebs@meta.com>
2025-08-01 12:56:07 -0700

    actually patch tp plan (#2897)

--- torchtune/modules/loss/cross_entropy_loss.py
+++ torchtune/modules/loss/cross_entropy_loss.py
@@ -72,8 +72,8 @@ class LinearCrossEntropyLoss(SFTLoss, nn.Module):
     def patch_tp_plan(self, tp_plan) -> dict:
         if "output" not in tp_plan and "decoder.output" not in tp_plan:
             raise KeyError("`tp_plan` requires `output` key")
-
-        tp_plan["output"] = ColwiseParallel(
+        key = "output" if "output" in tp_plan else "decoder.output"
+        tp_plan[key] = ColwiseParallel(
             input_layouts=Shard(1),
             output_layouts=Shard(-1),
             use_local_output=False,
